0e9347780d8758cf9e85c83e07f19ed140999fa0
Oli Larkin <olilarkin@googlemail.com>
2011-11-10 13:51:21 +0000

    [IPLUG/IOS] fix for detecting IOS when compiling against 10.6 sdk

--- WDL/IPlug/IPlugOSDetect.h
+++ WDL/IPlug/IPlugOSDetect.h
@@ -5,7 +5,7 @@
   #define OS_WIN
 #elif defined __APPLE__
   #include "TargetConditionals.h"
-  #ifdef TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
+  #if TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
     #define OS_IOS
   #elif TARGET_OS_MAC
     #define OS_OSX
