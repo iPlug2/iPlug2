5b84df4c03a6d9af6f9cf74051e7e6cf78365895
Oli Larkin <olilarkin@googlemail.com>
2012-01-03 12:23:32 +0000

    [IPLUG] IRect macros

--- WDL/IPlug/IPlugStructs.h
+++ WDL/IPlug/IPlugStructs.h
@@ -97,7 +97,12 @@ struct IText
   //	bool operator<(const IText& rhs) const;	// For sorting.
 };
 
+// these are macros to shorten the instantiation of IControls
+// for a paramater ID MyParam, define constants named MyParam_X, MyParam_Y, MyParam_W, MyParam_H to specify the Control's IRect
+// then when instantiating a Control you can just call MakeIRect(MyParam) to specify the IRect
 #define MakeIRect(a) IRECT(a##_X, a##_Y, a##_X + a##_W, a##_Y + a##_H)
+#define MakeIRectHOffset(a, xoffs) IRECT(a##_X + xoffs, a##_Y, a##_X + a##_W + xoffs, a##_Y + a##_H)
+#define MakeIRectVOffset(a, yoffs) IRECT(a##_X, a##_Y + yoffs, a##_X + a##_W, a##_Y + a##_H + yoffs)
 
 struct IRECT 
 { 
