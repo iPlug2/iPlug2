dc4685a0528d349df0200c4f27e2ec834b13278d
Oli Larkin <olilarkin@googlemail.com>
2012-02-19 22:37:20 +0000

    [IPLUG/EXAMPLES] made instrument examples work in PT

--- IPlugExamples/IPlugPolySynth/resource.h
+++ IPlugExamples/IPlugPolySynth/resource.h
@@ -27,55 +27,14 @@
 
 // ProTools stuff
 #define PLUG_MFR_DIGI "AcmeInc\nAcmeInc\nAcme\n"
-#define PLUG_NAME_DIGI "IPlugPolySynth\nIPMT"
+#define PLUG_NAME_DIGI "IPlugPolySynth\nIPPS"
 #define EFFECT_TYPE_DIGI "Effect" // valid options "None" "EQ" "Dynamics" "PitchShift" "Reverb" "Delay" "Modulation" "Harmonic" "NoiseReduction" "Dither" "SoundField" "Effect" instrument determined by PLUG _IS _INST
 
+#ifdef RTAS_API
+#define PLUG_CHANNEL_IO "1-1 2-2"
+#else
 #define PLUG_CHANNEL_IO "0-1 0-2"
-
-// if you want to do anything unusual re i/o you need to #ifdef PLUG_CHANNEL_IO and PLUG_SC_CHANS depending on the api because they all do it differently...
-
-// PLUGINS WITH SIDE CHAIN INPUTS
-// ***************************
-//#ifdef RTAS_API
-// PLUG_SC_CHANS defines the number of inputs in the PLUG_CHANNEL_IO that should be considered sidechain inputs.
-// RTAS can only have one mono sidechain input, so for instance to make a mono->mono or stereo->stereo plugin with a side chain input you could do this.
-//#define PLUG_CHANNEL_IO "2-1 3-2"
-//#define PLUG_SC_CHANS 1
-//#else // AU & VST2
-// AU sidechains work with audiounit effects or midi controlled effects only... not instruments
-// this works for a mono plug with optional mono sidechain...
-//#define PLUG_CHANNEL_IO "1-1 2-1"
-//#define PLUG_SC_CHANS 1
-// this DOESN'T work (in aulab) for a stereo plug with optional mono sidechain...
-//#define PLUG_CHANNEL_IO "2-2 3-2"
-//#define PLUG_SC_CHANS 1
-// this works for a stereo plug with optional stereo sidechain...
-//#define PLUG_CHANNEL_IO "2-2 4-2"
-//#define PLUG_SC_CHANS 2
-// but a combination DOESN'T work right now (in aulab)
-//#define PLUG_CHANNEL_IO "1-1 2-1 2-2 4-2"
-//#define PLUG_SC_CHANS 1
-//#endif
-
-// PLUGIN INSTRUMENTS (WITH MULTIPLE OUTPUTS)
-// ***************************
-//#ifdef RTAS_API
-// rtas instruments have to say they have inputs
-// rtas multiple outputs will result in a multichannel bus
-//#define PLUG_CHANNEL_IO "1-1 2-2"
-//#else // AU & VST2
-// in AU these will be grouped as stereo pairs... that is fixed right now
-//#define PLUG_CHANNEL_IO "0-2 0-4 0-6 0-8"
-//#endif
-//#define PLUG_SC_CHANS 0
-
-// MULTI-CHANNEL EFFECT PLUGINS (I.E MONO->QUAD, QUAD->QUAD etc)
-// ***************************
-// seems to be ok for au and rtas
-//#define PLUG_CHANNEL_IO "1-4 4-4"
-//#define PLUG_SC_CHANS 0
-
-//
+#endif
 
 #define PLUG_LATENCY 0
 #define PLUG_IS_INST 1
