b2c93f8fdb4a417bb56a935cc8bb5b167490e8eb
Oli Larkin <olilarkin@googlemail.com>
2012-02-19 22:14:48 +0000

    [IPLUG] added IGraphicsMac::DesktopPath()

--- WDL/IPlug/IGraphicsMac.h
+++ WDL/IPlug/IGraphicsMac.h
@@ -77,8 +77,9 @@ public:
   
   const char* GetGUIAPI();
 
-	void HostPath(WDL_String* pPath); 
+  void HostPath(WDL_String* pPath); 
   void PluginPath(WDL_String* pPath);
+  void DesktopPath(WDL_String* pPath);
 
 	void PromptForFile(WDL_String* pFilename, EFileAction action = kFileOpen, WDL_String* pDir = 0, char* extensions = "");   // extensions = "txt wav" for example.
   bool PromptForColor(IColor* pColor, char* prompt = "");
