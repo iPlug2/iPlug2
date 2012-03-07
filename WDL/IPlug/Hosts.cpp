752215f293db3084e0cff9b4f0d68b510bbde061
Oli Larkin <olilarkin@googlemail.com>
2012-03-07 22:23:25 +0000

    [IPLUG] added vst3plugintesthost, garageband and protools host identifiers. Also added a function GetHostNameStr for the odd occasion you might want to get the name of the identified host as a string

--- WDL/IPlug/Hosts.cpp
+++ WDL/IPlug/Hosts.cpp
@@ -6,11 +6,7 @@ EHost LookUpHost(const char* inHost)
 {
   char host[256];
   ToLower(host, inHost);
-  
-  if (strstr(host, "standalone")) {
-		return kHostStandalone;
-	}
-  
+ 
 	// C4 is version >= 8.2
 	if (strstr(host, "cubase")) {
 		return kHostCubase;
@@ -42,6 +38,9 @@ EHost LookUpHost(const char* inHost)
   if (strstr(host, "aulab")) {
     return kHostAULab;
   }
+  if (strstr(host, "garageband")) {
+    return kHostGarageBand;
+  }
   if (strstr(host, "forte")) {
     return kHostForte;
   }
@@ -69,5 +68,93 @@ EHost LookUpHost(const char* inHost)
   if (strstr(host, "presonus")) {
 		return kHostStudioOne;
 	}
+  if (strstr(host, "vst3plugintesthost")) {
+    return kHostVST3TestHost;
+  }
+  if (strstr(host, "protools")) {
+		return kHostProTools;
+	}
+  if (strstr(host, "standalone")) {
+		return kHostStandalone;
+	}
 	return kHostUnknown;
+}
+
+void GetHostNameStr(EHost host, char* pHostName)
+{
+  switch (host) 
+  {
+    case kHostCubase:
+      strcpy(pHostName, "Cubase");
+      break;
+    case kHostNuendo:
+      strcpy(pHostName, "Nuendo");
+      break;
+    case kHostLogic:
+      strcpy(pHostName, "Logic");
+      break;
+    case kHostAULab:
+      strcpy(pHostName, "AULab");
+      break;
+    case kHostGarageBand:
+      strcpy(pHostName, "GarageBand");
+      break;
+    case kHostAbletonLive:
+      strcpy(pHostName, "Live");
+      break;
+    case kHostReaper:
+      strcpy(pHostName, "Reaper");
+      break;
+    case kHostSonar:
+      strcpy(pHostName, "Sonar");
+      break;
+    case kHostVST3TestHost:
+      strcpy(pHostName, "VST3PluginTestHost");
+      break;
+    case kHostStudioOne:
+      strcpy(pHostName, "StudioOne");
+      break;
+    case kHostSAWStudio:
+      strcpy(pHostName, "SAWStudio");
+      break;
+    case kHostSamplitude:
+      strcpy(pHostName, "Samplitude");
+      break;
+    case kHostOrion:
+      strcpy(pHostName, "Orion");
+      break;
+    case kHostAudition:
+      strcpy(pHostName, "Audition");
+      break;
+    case kHostChainer:
+      strcpy(pHostName, "Chainer");
+      break;
+    case kHostVSTScanner:
+      strcpy(pHostName, "VSTScanner"); // ??
+      break;
+    case kHostForte:
+      strcpy(pHostName, "Forte");
+      break;
+    case kHostVegas:
+      strcpy(pHostName, "Vegas");
+      break;
+    case kHostFL:
+      strcpy(pHostName, "FLStudio");
+      break;
+    case kHostProTools:
+      strcpy(pHostName, "ProTools");
+      break;
+    case kHostAudioMulch:
+      strcpy(pHostName, "AudioMulch");
+      break;
+    case kHostDigitalPerformer:
+      strcpy(pHostName, "DigitalPerformer");
+      break;
+    case kHostStandalone:
+      strcpy(pHostName, "Standalone");
+      break;
+    default:
+      strcpy(pHostName, "Unknown");
+      break;
+  }
 }
