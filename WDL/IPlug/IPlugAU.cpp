iPlug2/iPlug2

commit c467c6b3ed02ddc651ec2f0f7bfc223d01a0ded3

IPlug: Added IsRenderingOffline() for VST and AU.
Adapted from commit a31c3d8 by uucidl @ GitHub <https://github.com/uucidl/wdl-ce.git>.

Conflicts:

	WDL/IPlug/IPlugAU.cpp
	WDL/IPlug/IPlugAU.h

WDL/IPlug/IPlugAU.cpp
@@ -683,7 +683,10 @@ ComponentResult IPlugAU::GetProperty(AudioUnitPropertyID propID, AudioUnitScope 
       }
       return noErr;
     }
-    NO_OP(kAudioUnitProperty_OfflineRender);              // 37,
+    case kAudioUnitProperty_OfflineRender: {              // 37,
+      mIsOffline = (*((UInt32*) pData) != 0);
+      return noErr; 
+    } 
     case kAudioUnitProperty_ParameterStringFromValue: {   // 33,
       *pDataSize = sizeof(AudioUnitParameterStringFromValue);
       if (pData && scope == kAudioUnitScope_Global) {
@@ -1423,7 +1426,7 @@ IPlugAU::IPlugAU(IPlugInstanceInfo instanceInfo,
 : IPlugBase(nParams, channelIOStr, nPresets,
   effectName, productName, mfrName, vendorVersion, uniqueID, mfrID, latency,
   plugDoesMidi, plugDoesChunks, plugIsInst),
-  mCI(0), mBypassed(false), mRenderTimestamp(-1.0), mTempo(DEFAULT_TEMPO), mActive(false)
+  mCI(0), mBypassed(false), mIsOffline(false), mRenderTimestamp(-1.0), mTempo(DEFAULT_TEMPO), mActive(false)
 {
   Trace(TRACELOC, "%s", effectName);
 
@@ -1538,10 +1541,9 @@ void IPlugAU::EndInformHostOfParamChange(int idx)
   SendAUEvent(kAudioUnitEvent_EndParameterChangeGesture, mCI, idx);
 }
 
-void IPlugAU::InformHostOfProgramChange()
+bool IPlugAU::IsRenderingOffline()
 {
-	//InformListeners(kAudioUnitProperty_CurrentPreset, kAudioUnitScope_Global);
-	InformListeners(kAudioUnitProperty_PresentPreset, kAudioUnitScope_Global);
+  return mIsOffline;
 }
 
 // Samples since start of project.

WDL/IPlug/IPlugAU.h
@@ -56,6 +56,9 @@ public:
   // Should be called only by the graphics object when it resizes itself.
   void ResizeGraphics(int w, int h) {}
 
+  // Whether the plugin is being used for offline rendering
+  bool IsRenderingOffline();
+
   enum EAUInputType {
     eNotConnected = 0,
     eDirectFastProc,
@@ -75,7 +78,7 @@ private:
 
   WDL_String mOSXBundleID, mCocoaViewFactoryClassName;
   ComponentInstance mCI;
-  bool mActive, mBypassed;
+  bool mActive, mBypassed, mIsOffline;
   double mRenderTimestamp, mTempo;
   HostCallbackInfo mHostCallbacks;
 

WDL/IPlug/IPlugVST.cpp
@@ -205,6 +205,11 @@ void IPlugVST::ResizeGraphics(int w, int h)
   }
 }
 
+bool IPlugVST::IsRenderingOffline()
+{ 
+  return mHostCallback(&mAEffect, audioMasterGetCurrentProcessLevel, 0, 0, 0, 0.0f) == kVstProcessLevelOffline;
+}
+
 void IPlugVST::SetLatency(int samples)
 {
     mAEffect.initialDelay = samples;

WDL/IPlug/IPlugVST.h
@@ -40,6 +40,9 @@ public:
   // Should be called only by the graphics object when it resizes itself.
   void ResizeGraphics(int w, int h);
 
+  // Whether the plugin is being used for offline rendering
+  bool IsRenderingOffline();
+
 protected:
 
   void HostSpecificInit();

commit 45f459b0bced741510875094f7e8d2da7cea6a4b

[IPLUG/AU] IsRenderingOffline() - fixed an error introduced in c467c6b3, which was tale's adaption of a31c3d8 by uucidl @ GitHub. The kAudioUnitProperty_OfflineRender code was in GetProperty when it was originally in SetProperty. The fact it was in GetProperty() meant calling IsRenderingOffline() didn't work in AUs

WDL/IPlug/IPlugAU.cpp
@@ -814,10 +814,7 @@ ComponentResult IPlugAU::GetProperty(AudioUnitPropertyID propID, AudioUnitScope 
       }
       return noErr;
     }
-    case kAudioUnitProperty_OfflineRender: {              // 37,
-      mIsOffline = (*((UInt32*) pData) != 0);
-      return noErr; 
-    } 
+    NO_OP(kAudioUnitProperty_OfflineRender);             // 37,
     case kAudioUnitProperty_ParameterStringFromValue: {   // 33,
       *pDataSize = sizeof(AudioUnitParameterStringFromValue);
       if (pData && scope == kAudioUnitScope_Global) {
@@ -1028,7 +1025,10 @@ ComponentResult IPlugAU::SetProperty(AudioUnitPropertyID propID, AudioUnitScope 
       RestorePreset(presetIdx);
       return noErr;
     }
-    NO_OP(kAudioUnitProperty_OfflineRender);             // 37,
+    case kAudioUnitProperty_OfflineRender: {              // 37,
+      mIsOffline = (*((UInt32*) pData) != 0);
+      return noErr; 
+    } 
     NO_OP(kAudioUnitProperty_ParameterStringFromValue);  // 33,
     NO_OP(kAudioUnitProperty_ParameterValueFromString);  // 38,
     NO_OP(kAudioUnitProperty_IconLocation);              // 39,