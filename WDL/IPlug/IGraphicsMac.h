a6a467e18dc10453028e5fed5d466f31826838e9
Oli Larkin <olilarkin@googlemail.com>
2012-06-14 20:12:14 +0100

    [IPLUG/OSX] fix compiling AAX with the 10.6 SDK

--- WDL/IPlug/IGraphicsMac.h
+++ WDL/IPlug/IGraphicsMac.h
@@ -8,7 +8,10 @@
 #include "IGraphics.h"
 #include "../swell/swell.h"
 
-#define _UINT32 // this is necessary to get VST3 to compile with the 10.6 SDK due to a conflict
+#ifdef VST3_API
+  #define _UINT32 // this is necessary to get VST3 to compile with the 10.6 SDK due to a conflict
+#endif
+
 #include <Carbon/Carbon.h>
 
 #ifndef DEFAULT_PATH_OSX
