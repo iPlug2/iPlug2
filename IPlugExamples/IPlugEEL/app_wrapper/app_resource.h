5ef791f3a4cf716aae96a926553f0da6d19cde2d
Oli Larkin <olilarkin@googlemail.com>
2012-07-11 14:00:16 +0100

    [IPLUG/EXAMPLES] added IPlugEEL example

---
+++ IPlugExamples/IPlugEEL/app_wrapper/app_resource.h
@@ -0,0 +1,42 @@
+// RESOURCE IDS FOR STANDALONE APP
+
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_DIALOG_MAIN             40001
+#define IDD_DIALOG_PREF             40002
+#define IDI_ICON1                   40003
+#define IDR_MENU1                   40004
+
+#define ID_ABOUT                    40005
+#define ID_PREFERENCES              40006
+#define ID_QUIT                     40007
+
+#define IDC_COMBO_AUDIO_DRIVER      40008
+#define IDC_COMBO_AUDIO_IN_DEV      40009
+#define IDC_COMBO_AUDIO_OUT_DEV     40010
+#define IDC_COMBO_AUDIO_IOVS        40011
+#define IDC_COMBO_AUDIO_SIGVS       40012
+#define IDC_COMBO_AUDIO_SR          40013
+#define IDC_COMBO_AUDIO_IN_L        40014
+#define IDC_COMBO_AUDIO_IN_R        40015
+#define IDC_COMBO_AUDIO_OUT_R       40016
+#define IDC_COMBO_AUDIO_OUT_L       40017
+#define IDC_COMBO_MIDI_IN_DEV       40018
+#define IDC_COMBO_MIDI_OUT_DEV      40019
+#define IDC_COMBO_MIDI_IN_CHAN      40020
+#define IDC_COMBO_MIDI_OUT_CHAN     40021
+#define IDC_BUTTON_ASIO             40022
+#define IDC_CB_MONO_INPUT           40023
+
+#define IDAPPLY                     40024
+
+// some options for the app
+
+#define ENABLE_SYSEX 0
+#define ENABLE_MIDICLOCK 0
+#define ENABLE_ACTIVE_SENSING 0
+#define NUM_CHANNELS 2
+#define N_VECTOR_WAIT 50
+#define APP_MULT 0.25
---
+++ IPlugExamples/IPlugEEL/resource.h
@@ -0,0 +1,75 @@
+#define PLUG_MFR "AcmeInc"
+#define PLUG_NAME "IPlugEEL"
+
+#define PLUG_CLASS_NAME IPlugEEL
+
+#define BUNDLE_MFR "AcmeInc"
+#define BUNDLE_NAME "IPlugEEL"
+
+#define PLUG_ENTRY IPlugEEL_Entry
+#define PLUG_VIEW_ENTRY IPlugEEL_ViewEntry
+
+#define PLUG_ENTRY_STR "IPlugEEL_Entry"
+#define PLUG_VIEW_ENTRY_STR "IPlugEEL_ViewEntry"
+
+#define VIEW_CLASS IPlugEEL_View
+#define VIEW_CLASS_STR "IPlugEEL_View"
+
+// Format        0xMAJR.MN.BG - in HEX! so version 10.1.5 would be 0x000A0105
+#define PLUG_VER 0x00010000
+#define VST3_VER_STR "1.0.0"
+
+// http://service.steinberg.de/databases/plugin.nsf/plugIn?openForm
+// 4 chars, single quotes. At least one capital letter
+#define PLUG_UNIQUE_ID 'Ipee'
+// make sure this is not the same as BUNDLE_MFR
+#define PLUG_MFR_ID 'Acme'
+
+// ProTools stuff
+#define PLUG_MFR_DIGI "AcmeInc\nAcmeInc\nAcme"
+#define PLUG_NAME_DIGI "IPlugEEL\nIPEE"
+#define EFFECT_TYPE_DIGI "Effect"
+
+/* "None", "EQ", "Dynamics", "PitchShift", "Reverb", "Delay", "Modulation", 
+"Harmonic" "NoiseReduction" "Dither" "SoundField" "Effect" 
+instrument determined by PLUG _IS _INST
+*/
+
+#define PLUG_CHANNEL_IO "1-1 2-2"
+
+#define PLUG_LATENCY 0
+#define PLUG_IS_INST 0
+
+// if this is 0 RTAS can't get tempo info
+#define PLUG_DOES_MIDI 0
+
+#define PLUG_DOES_STATE_CHUNKS 0
+
+// Unique IDs for each image resource.
+#define KNOB_ID 101
+
+// Image resource locations for this plug.
+#define KNOB_FN "resources/img/knob.png"
+
+// GUI default dimensions
+#define GUI_WIDTH 300
+#define GUI_HEIGHT 300
+
+// on MSVC, you must define SA_API in the resource editor preprocessor macros as well as the c++ ones
+#if defined(SA_API) && !defined(OS_IOS)
+#include "app_wrapper/app_resource.h"
+#endif
+
+// vst3 stuff
+#define MFR_URL "www.olilarkin.co.uk"
+#define MFR_EMAIL "spam@me.com"
+#define EFFECT_TYPE_VST3 "Fx"
+
+/* "Fx|Analyzer"", "Fx|Delay", "Fx|Distortion", "Fx|Dynamics", "Fx|EQ", "Fx|Filter",
+"Fx", "Fx|Instrument", "Fx|InstrumentExternal", "Fx|Spatial", "Fx|Generator",
+"Fx|Mastering", "Fx|Modulation", "Fx|PitchShift", "Fx|Restoration", "Fx|Reverb",
+"Fx|Surround", "Fx|Tools", "Instrument", "Instrument|Drum", "Instrument|Sampler",
+"Instrument|Synth", "Instrument|Synth|Sampler", "Instrument|External", "Spatial",
+"Spatial|Fx", "OnlyRT", "OnlyOfflineProcess", "Mono", "Stereo",
+"Surround"
+*/
