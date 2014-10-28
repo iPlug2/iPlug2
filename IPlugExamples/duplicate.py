11aa694a6c5a22f7c37c7ad9fec39734adf877b9
Oli Larkin <olilarkin@googlemail.com>
2014-10-28 10:37:59 +0000

    update duplicate.py

--- IPlugExamples/duplicate.py
+++ IPlugExamples/duplicate.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 
-# Python shell script for Duplicating IPlug Projects
-# Oli Larkin 2012 http://www.olilarkin.co.uk
+# Python shell script for Duplicating WDL-OL IPlug Projects
+# Oli Larkin 2012-2014 http://www.olilarkin.co.uk
 # License: WTFPL http://sam.zoy.org/wtfpl/COPYING
 # Modified from this script by Bibha Tripathi http://code.activestate.com/recipes/435904-sedawk-python-script-to-rename-subdirectories-of-a/
 # Author accepts no responsibilty for wiping your hd
 
 # NOTES:
+# should work with Python2 or Python3
 # not designed to be fool proof- think carefully about what you choose for a project name
 # best to stick to standard characters in your project names - avoid spaces, numbers and dots
-# windows users need to install python 2 (not tested with 3) and set it up so you can run it from the command line
+# windows users need to install python and set it up so you can run it from the command line
 # see http://www.voidspace.org.uk/python/articles/command_line.shtml
 # this involves adding the python folder e.g. C:\Python27\ to your %PATH% environment variable
-# to get the debugging setup on mac, you have to rename IPlugEffect/IPlugEffect.xcodeproj/oli.pbxuser to match your OSX username,
 
 # USAGE:
 # duplicate.py [inputprojectname] [outputprojectname] [manufacturername]
@@ -24,23 +24,35 @@
 
 from __future__ import generators
 
-import fileinput, glob, string, sys, os, re
+import fileinput, glob, string, sys, os, re, uuid
 from shutil import copy, copytree, ignore_patterns, rmtree
 from os.path import join
 
-#tabs = 0
-#newsubfolder = False
-
-VERSION = "0.7.5"
+VERSION = "0.9"
 
 # binary files that we don't want to do find and replace inside
 FILTERED_FILE_EXTENSIONS = [".ico",".icns", ".pdf", ".png", ".zip", ".exe", ".wav", ".aif"]
+# files that we don't want to duplicate
+DONT_COPY = ("*.exe", "*.dmg", "*.pkg", "*.mpkg", "*.svn", "*.ncb", "*.suo", "*sdf", "ipch", "build-*", "*.layout", "*.depend", ".DS_Store")
+
+SUBFOLDERS_TO_SEARCH = [
+"app_wrapper",
+"resources",
+"installer",
+"scripts",
+"manual",
+"ios_wrapper",
+"xcschemes",
+"xcshareddata",
+"xcuserdata",
+"en-osx.lproj",
+"project.xcworkspace"
+]
 
 def checkdirname(name, searchproject):
   "check if directory name matches with the given pattern"
-  print ""
+  print("")
   if name == searchproject:
-# if name == searchproject + ".xcodeproj":
     return True
   else:
     return False
@@ -49,86 +61,61 @@ def replacestrs(filename, s, r):
   files = glob.glob(filename)
   
   for line in fileinput.input(files,inplace=1):
-    string.find(line, s)
+    line.find(s)
     line = line.replace(s, r)
+    sys.stdout.write(line)
+    
+def replacestrsChop(filename, s, r):
+  files = glob.glob(filename)
+  
+  for line in fileinput.input(files,inplace=1):
+    if(line.startswith(s)): 
+      line = r + "\n"
     sys.stdout.write(line)
 
 def dirwalk(dir, searchproject, replaceproject, searchman, replaceman):
-# global tabs, newsubfolder
   for f in os.listdir(dir):
     fullpath = os.path.join(dir, f)
     
     if os.path.isdir(fullpath) and not os.path.islink(fullpath):
-      #if(newsubfolder):
-      # newsubfolder = False
-      #else:
-      # tabs+=1
-      # newsubfolder = True
       if checkdirname(f, searchproject + ".xcodeproj"):
         os.rename(fullpath, os.path.join(dir, replaceproject + ".xcodeproj"))
         fullpath = os.path.join(dir, replaceproject + ".xcodeproj")
         
-        print 'recursing in main xcode project directory: '
+        print("recursing in main xcode project directory: ")
         for x in dirwalk(fullpath, searchproject, replaceproject, searchman, replaceman):
           yield x
       elif checkdirname(f, searchproject + "-ios.xcodeproj"):
         os.rename(fullpath, os.path.join(dir, replaceproject + "-ios.xcodeproj"))
         fullpath = os.path.join(dir, replaceproject + "-ios.xcodeproj")
         
-        print 'recursing in IOS xcode project directory: '
+        print("recursing in ios xcode project directory: ")
         for x in dirwalk(fullpath, searchproject, replaceproject, searchman, replaceman):
           yield x
-      elif (f ==  "ios_wrapper"):
-        print 'recursing in ios_wrapper directory: '
+      elif (f in SUBFOLDERS_TO_SEARCH):
+        print 'recursing in ' + f + ' directory: '
         for x in dirwalk(fullpath, searchproject, replaceproject, searchman, replaceman):
           yield x
-      elif (f ==  "app_wrapper"):
-          print 'recursing in app_wrapper directory: '
-          for x in dirwalk(fullpath, searchproject, replaceproject, searchman, replaceman):
-            yield x
-      elif (f ==  "resources"):
-          print 'recursing in resources directory: '
-          for x in dirwalk(fullpath, searchproject, replaceproject, searchman, replaceman):
-            yield x
-      elif (f ==  "installer"):
-          print 'recursing in installer directory: '
-          for x in dirwalk(fullpath, searchproject, replaceproject, searchman, replaceman):
-            yield x
-      elif (f ==  "manual"):
-          print 'recursing in manual directory: '
-          for x in dirwalk(fullpath, searchproject, replaceproject, searchman, replaceman):
-            yield x
-      elif (f ==  "English.lproj"):
-          print 'recursing in interface builder localised directory: '
-          for x in dirwalk(fullpath, searchproject, replaceproject, searchman, replaceman):
-            yield x
-    #elif (newsubfolder):
-      #newsubfolder = False
-      #tabs-=1
-      
+
     if os.path.isfile(fullpath):
-      tabstring = ""
-      #for tab in range(0,tabs):
-      # tabstring += "\t"
-      
       filename = os.path.basename(fullpath)
       newfilename = filename.replace(searchproject, replaceproject)
       base, extension = os.path.splitext(filename)
       
       if (not(extension in FILTERED_FILE_EXTENSIONS)):
-        print tabstring + 'Replacing project name strings in file', filename
+        print("Replacing project name strings in file " + filename)
         replacestrs(fullpath, searchproject, replaceproject)
         
-        print tabstring + 'Replacing captitalized project name strings in file', filename
+        print("Replacing captitalized project name strings in file " + filename)
         replacestrs(fullpath, searchproject.upper(), replaceproject.upper())
         
-        print tabstring + 'Replacing manufacturer name strings in file', filename
+        print("Replacing manufacturer name strings in file " + filename)
         replacestrs(fullpath, searchman, replaceman)
       else:
-        print tabstring + 'NOT replacing name strings in file', filename
+        print("NOT replacing name strings in file " + filename)
       
       if filename != newfilename:
-        print tabstring + 'Renaming file ' + filename + ' to ' + newfilename
+        print("Renaming file " + filename + " to " + newfilename)
         os.rename(fullpath, os.path.join(dir, newfilename))
   
       yield f, fullpath 
@@ -137,10 +124,10 @@ def dirwalk(dir, searchproject, replaceproject, searchman, replaceman):
 
 def main():
   global VERSION
-  print "\nIPlug Project Duplicator v" + VERSION + " by Oli Larkin ------------------------------\n"
+  print("\nIPlug Project Duplicator v" + VERSION + " by Oli Larkin ------------------------------\n")
   
   if len(sys.argv) != 4:
-    print "Usage: duplicate.py inputprojectname outputprojectname [manufacturername]",
+    print("Usage: duplicate.py inputprojectname outputprojectname [manufacturername]")
     sys.exit(1)
   else:
     input=sys.argv[1]
@@ -148,15 +135,15 @@ def main():
     manufacturer=sys.argv[3]
 
     if ' ' in input:
-      print "error: input project name has spaces",
+      print("error: input project name has spaces")
       sys.exit(1)
       
     if ' ' in output:
-      print "error: output project name has spaces",
+      print("error: output project name has spaces")
       sys.exit(1)
     
     if ' ' in manufacturer:
-      print "error: manufacturer name has spaces",
+      print("error: manufacturer name has spaces")
       sys.exit(1)
     
     # remove a trailing slash if it exists
@@ -168,46 +155,27 @@ def main():
       
     #check that the folders are OK
     if os.path.isdir(input) == False:
-      print "error: input project not found",
+      print("error: input project not found")
       sys.exit(1)
         
     if os.path.isdir(output):
-      print "error: output folder allready exists",
+      print("error: output folder allready exists")
       sys.exit(1)
     # rmtree(output)
         
-    print "copying " + input + " folder to " + output
-    copytree(input, output, ignore=ignore_patterns('*.exe', '*.dmg', '*.pkg', '*.mpkg', '*.svn', '*.ncb', '*.suo', '*sdf', 'ipch', 'build-*', '*.layout', '*.depend', '.DS_Store', '*xcuserdata*' ))
+    print("copying " + input + " folder to " + output)
+    copytree(input, output, ignore=ignore_patterns(*DONT_COPY))
     cpath = os.path.join(os.getcwd(), output)
 
     #replace manufacturer name strings
     for dir in dirwalk(cpath, input, output, "AcmeInc", manufacturer):
       pass
     
-    #xcuserfile = output + "/" + output + ".xcodeproj/oli.pbxuser"
-    #vsuserfile = + output + "/" + output + ".vcxproj.user
-    
-    #ans = raw_input("import default debug setups? y/n ...")
-
-    #if ans == "y":
-    # print "\n renaming the file " + xcuserfile
-    # osxun = raw_input("enter your osx username ...")
-    # if ' ' in osxun:
-    #   print "error: spaces in user name"
-    #   sys.exit(1)
-    # else:
-    #   nxcuserfile = xcuserfile.replace("wdlce", osxun);
-    #   os.rename(xcuserfile, nxcuserfile)
-      
-    #elif ans == "n":
-    # print "\n not renaming the file " + xcuserfile + " debugging setup will be lost"
-    # #print "\n not renaming the file " + vsuserfile
-    
-    print "\ncopying gitignore template into project folder"
+    print("\ncopying gitignore template into project folder")
 
     copy('gitignore_template', output + "/.gitignore")
 
-    print "\ndone - don't forget to change PLUG_UNIQUE_ID and PLUG_MFR_ID in resource.h"
+    print("\ndone - don't forget to change PLUG_UID and MFR_UID in config.h")
     
 if __name__ == '__main__':
   main()
