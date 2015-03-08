b11911deccf833e34fd43c9d7a43c73d23e6175c
Oli Larkin <olilarkin@googlemail.com>
2015-03-08 12:16:17 +0000

    Draw mouse coordinates when IGraphics::mShowControlBounds is true
    (only in debug builds)

--- WDL/IPlug/IGraphics.cpp
+++ WDL/IPlug/IGraphics.cpp
@@ -714,6 +714,14 @@ bool IGraphics::DrawRadialLine(const IColor* pColor, float cx, float cy, float a
 
 bool IGraphics::IsDirty(IRECT* pR)
 {
+#ifndef NDEBUG
+  if (mShowControlBounds)
+  {
+    *pR = mDrawRECT;
+    return true;
+  }
+#endif
+  
   bool dirty = false;
   int i, n = mControls.GetSize();
   IControl** ppControl = mControls.GetList();
@@ -824,6 +832,12 @@ bool IGraphics::Draw(IRECT* pR)
       IControl* pControl = mControls.Get(j);
       DrawRect(&CONTROL_BOUNDS_COLOR, pControl->GetRECT());
     }
+    
+    WDL_String str;
+    str.SetFormatted(32, "x: %i, y: %i", mMouseX, mMouseY);
+    IText txt(20, &CONTROL_BOUNDS_COLOR);
+    IRECT rect(Width() - 150, Height() - 20, Width(), Height());
+    DrawIText(&txt, str.Get(), &rect);
   }
 #endif
 
