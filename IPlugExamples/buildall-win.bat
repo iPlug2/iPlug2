16cef1577a40a1b1e1d7c306da418d9bc130be5c
Oli Larkin <olilarkin@googlemail.com>
2017-08-12 01:34:09 +0100

    update buildall bat for vs community 2017

--- IPlugExamples/buildall-win.bat
+++ IPlugExamples/buildall-win.bat
@@ -2,12 +2,12 @@ if exist "%ProgramFiles(x86)%" (goto 64-Bit) else (goto 32-Bit)
 
 :32-Bit
 echo 32-Bit O/S detected
-call "%ProgramFiles%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+call "%ProgramFiles%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_x64 8.1
 goto END
 
 :64-Bit
 echo 64-Bit Host O/S detected
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_x64 8.1
 goto END
 :END
 
@@ -15,8 +15,8 @@ if exist build_errors.log del build_errors.log
 
 for /r %%X in (/*.sln) do (
 echo Building %%X
-msbuild %%X /p:configuration=release /p:platform=win32 /nologo /noconsolelogger /fileLogger /v:quiet /flp:logfile=build_errors.log;errorsonly;append
-msbuild %%X /p:configuration=release /p:platform=x64 /nologo /noconsolelogger /fileLogger /v:quiet /flp:logfile=build_errors.log;errorsonly;append
+msbuild "%%X" /p:configuration=release /p:platform=win32 /nologo /noconsolelogger /fileLogger /v:quiet /flp:logfile=build_errors.log;errorsonly;append
+msbuild "%%X" /p:configuration=release /p:platform=x64 /nologo /noconsolelogger /fileLogger /v:quiet /flp:logfile=build_errors.log;errorsonly;append
 )
 
 echo ------------------------------------------------------------------
