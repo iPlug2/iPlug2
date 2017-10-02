219e1499e490a0996bfd0cccb0d8daafd4675696
Oli Larkin <olilarkin@googlemail.com>
2017-10-02 22:20:22 +0100

    detabification

--- IPlug/AAX/AAX_CIPlugTaperDelegate.h
+++ IPlug/AAX/AAX_CIPlugTaperDelegate.h
@@ -11,61 +11,60 @@ template <typename T>
 class AAX_CIPlugTaperDelegate : public AAX_ITaperDelegate<T>
 {
 public: 
-	AAX_CIPlugTaperDelegate(T minValue=0, T maxValue=1, double shape = 1.);
-	
-	//Virtual AAX_ITaperDelegate Overrides
-	AAX_CIPlugTaperDelegate<T>*	Clone() const;
-	T		GetMinimumValue()	const						{ return mMinValue; }
-	T		GetMaximumValue()	const						{ return mMaxValue; }
-	T		ConstrainRealValue(T value)	const;
-	T		NormalizedToReal(double normalizedValue) const;
-	double	RealToNormalized(T realValue) const;
+  AAX_CIPlugTaperDelegate(T minValue=0, T maxValue=1, double shape = 1.);
+  
+  //Virtual AAX_ITaperDelegate Overrides
+  AAX_CIPlugTaperDelegate<T>* Clone() const;
+  T GetMinimumValue() const { return mMinValue; }
+  T GetMaximumValue() const { return mMaxValue; }
+  T ConstrainRealValue(T value) const;
+  T NormalizedToReal(double normalizedValue) const;
+  double  RealToNormalized(T realValue) const;
 
 private:
-	T	mMinValue;
-	T	mMaxValue;
-	double mShape;
+  T mMinValue;
+  T mMaxValue;
+  double mShape;
 };
 
 template <typename T>
-AAX_CIPlugTaperDelegate<T>::AAX_CIPlugTaperDelegate(T minValue, T maxValue, double shape)  :  AAX_ITaperDelegate<T>(),
-	mMinValue(minValue),
-	mMaxValue(maxValue),
-	mShape(shape)
+AAX_CIPlugTaperDelegate<T>::AAX_CIPlugTaperDelegate(T minValue, T maxValue, double shape):AAX_ITaperDelegate<T>(),
+  mMinValue(minValue),
+  mMaxValue(maxValue),
+  mShape(shape)
 {
-
 }
 
 template <typename T>
-AAX_CIPlugTaperDelegate<T>*		AAX_CIPlugTaperDelegate<T>::Clone() const
+AAX_CIPlugTaperDelegate<T>*   AAX_CIPlugTaperDelegate<T>::Clone() const
 {
-	return new AAX_CIPlugTaperDelegate(*this);
+  return new AAX_CIPlugTaperDelegate(*this);
 }
 
 template <typename T>
-T		AAX_CIPlugTaperDelegate<T>::ConstrainRealValue(T value)	const
+T   AAX_CIPlugTaperDelegate<T>::ConstrainRealValue(T value) const
 {
-	if (value > mMaxValue)
-		return mMaxValue;
-	if (value < mMinValue)
-		return mMinValue;
-	return value;		
+  if (value > mMaxValue)
+    return mMaxValue;
+  if (value < mMinValue)
+    return mMinValue;
+  return value;
 }
 
 template <typename T>
-T		AAX_CIPlugTaperDelegate<T>::NormalizedToReal(double normalizedValue) const
+T   AAX_CIPlugTaperDelegate<T>::NormalizedToReal(double normalizedValue) const
 {  
   double doubleRealValue = FromNormalizedParam(normalizedValue, mMinValue, mMaxValue, mShape);
   
-	T realValue = (T)doubleRealValue;
-	
-	return ConstrainRealValue(realValue);
+  T realValue = (T)doubleRealValue;
+  
+  return ConstrainRealValue(realValue);
 }
 
 template <typename T>
-double	AAX_CIPlugTaperDelegate<T>::RealToNormalized(T realValue) const
+double  AAX_CIPlugTaperDelegate<T>::RealToNormalized(T realValue) const
 {
-	realValue = ConstrainRealValue(realValue);
+  realValue = ConstrainRealValue(realValue);
   
   return ToNormalizedParam(realValue, mMinValue, mMaxValue, mShape);
 }
