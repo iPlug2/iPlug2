798d93b678a5584a06d4b91010f99b762e0a6064
Oli Larkin <olilarkin@googlemail.com>
2017-10-03 01:28:21 +0100

    include guard

--- IPlugExamples/IPlugEffect/app_wrapper/app_main.h
+++ IPlugExamples/IPlugEffect/app_wrapper/app_main.h
@@ -1,5 +1,4 @@
-#ifndef _IPLUGAPP_APP_MAIN_H_
-#define _IPLUGAPP_APP_MAIN_H_
+#pragma once
 
 #include "IPlugOSDetect.h"
 
@@ -142,6 +141,3 @@ extern std::vector<unsigned int> gAudioInputDevs;
 extern std::vector<unsigned int> gAudioOutputDevs;
 extern std::vector<std::string> gMIDIInputDevNames;
 extern std::vector<std::string> gMIDIOutputDevNames;
-
-#endif //_IPLUGAPP_APP_MAIN_H_
-
