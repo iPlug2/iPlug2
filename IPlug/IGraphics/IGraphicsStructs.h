b0eb72024ac748cdc356effdd400df33204afb77
Oli Larkin <olilarkin@googlemail.com>
2017-12-06 21:37:16 +0000

    remove stuff that's in I graphics utilities

--- IPlug/IGraphics/IGraphicsStructs.h
+++ IPlug/IGraphics/IGraphicsStructs.h
@@ -149,15 +149,8 @@ struct IFontData
   {}
 };
 
-// these are macros to shorten the instantiation of IControls
-// for a paramater ID MyParam, define constants named MyParam_X, MyParam_Y, MyParam_W, MyParam_H to specify the Control's IRect
-// then when instantiating a Control you can just call MakeIRect(MyParam) to specify the IRect
-#define MakeIRect(a) IRECT(a##_X, a##_Y, a##_X + a##_W, a##_Y + a##_H)
-#define MakeIRectHOffset(a, xoffs) IRECT(a##_X + xoffs, a##_Y, a##_X + a##_W + xoffs, a##_Y + a##_H)
-#define MakeIRectVOffset(a, yoffs) IRECT(a##_X, a##_Y + yoffs, a##_X + a##_W, a##_Y + a##_H + yoffs)
-#define MakeIRectHVOffset(a, xoffs, yoffs) IRECT(a##_X + xoffs, a##_Y + yoffs, a##_X + a##_W + xoffs, a##_Y + a##_H + yoffs)
-
 // An IRECT is always specified in 1:1 pixels, any scaling for HiDPI happens in the drawing
+// IGraphics 0,0 is top left
 struct IRECT
 {
   int L, T, R, B;
