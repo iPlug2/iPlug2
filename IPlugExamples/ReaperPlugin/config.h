c2e17606527bc5041623993052606a83a6453613
Oli Larkin <olilarkin@googlemail.com>
2017-12-18 08:51:35 +0000

    some reaper plugin files

---
+++ IPlugExamples/ReaperPlugin/config.h
@@ -0,0 +1,35 @@
+#define PLUG_MFR "OliLarkin"
+#define PLUG_NAME "ReaperPlugin"
+
+#define PLUG_CLASS_NAME ReaperPlugin
+
+#define BUNDLE_MFR "OliLarkin"
+#define BUNDLE_NAME "ReaperPlugin"
+
+//#define PLUG_ENTRY ReaperPlugin_Entry
+//#define PLUG_VIEW_ENTRY ReaperPlugin_ViewEntry
+//#define PLUG_FACTORY ReaperPlugin_Factory
+
+//#define PLUG_ENTRY_STR "ReaperPlugin_Entry"
+//#define PLUG_VIEW_ENTRY_STR "ReaperPlugin_ViewEntry"
+
+//#define VIEW_CLASS ReaperPlugin_View
+//#define VIEW_CLASS_STR "ReaperPlugin_View"
+
+// Format        0xMAJR.MN.BG - in HEX! so version 10.1.5 would be 0x000A0105
+#define PLUG_VER 0x00010000
+
+// 4 chars, single quotes. At least one capital letter
+#define PLUG_UNIQUE_ID 'IpeR'
+// make sure this is not the same as BUNDLE_MFR
+#define PLUG_MFR_ID 'Acme'
+
+#define PLUG_COPYRIGHT  "Copyright 2017 Acme Inc"
+#define PLUG_CHANNEL_IO "1-1 2-2"
+
+#define PLUG_LATENCY 0
+#define PLUG_IS_INST 0
+
+#define PLUG_DOES_MIDI 0
+
+#define PLUG_DOES_STATE_CHUNKS 0
