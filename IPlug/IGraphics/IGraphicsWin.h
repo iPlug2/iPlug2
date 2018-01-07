2ffbbcef66630318485355c6aac5f18bb72d8c4c
Oli Larkin <olilarkin@googlemail.com>
2018-01-07 16:39:13 +0000

    overrides

--- IPlug/IGraphics/IGraphicsWin.h
+++ IPlug/IGraphics/IGraphicsWin.h
@@ -29,37 +29,37 @@ public:
 
   void SetHInstance(HINSTANCE hInstance) { mHInstance = hInstance; }
 
-  void ForceEndUserEdit();
+  void ForceEndUserEdit() override;
 
-  void Resize(int w, int h);
+  void Resize(int w, int h) override;
 
-  void HideMouseCursor();
-  void ShowMouseCursor();
-  int ShowMessageBox(const char* str, const char* caption, int type);
+  void HideMouseCursor() override;
+  void ShowMouseCursor() override;
+  int ShowMessageBox(const char* str, const char* caption, int type) override;
 
   void DrawScreen(const IRECT& rect) override;
 
-  void* OpenWindow(void* pParentWnd);
-  void CloseWindow();
-  bool WindowIsOpen() { return (mPlugWnd); }
+  void* OpenWindow(void* pParentWnd) override;
+  void CloseWindow() override;
+  bool WindowIsOpen() override { return (mPlugWnd); }
   
   void UpdateTooltips() {}
 
-  void HostPath(WDL_String& path);
-  void PluginPath(WDL_String& path);
-  void DesktopPath(WDL_String& path);
-  void VST3PresetsPath(WDL_String& path, bool isSystem = true);
-  void AppSupportPath(WDL_String& path, bool isSystem = false);
-  void SandboxSafeAppSupportPath(WDL_String& path) { AppSupportPath(path, false); }
-  bool RevealPathInExplorerOrFinder(WDL_String& path, bool select = false);
+  void HostPath(WDL_String& path) override;
+  void PluginPath(WDL_String& path) override;
+  void DesktopPath(WDL_String& path) override;
+  void AppSupportPath(WDL_String& path, bool isSystem = false) override;
+  void SandboxSafeAppSupportPath(WDL_String& path) override { AppSupportPath(path, false); }
+  bool RevealPathInExplorerOrFinder(WDL_String& path, bool select = false) override;
 
-  void PromptForFile(WDL_String& filename, EFileAction action = kFileOpen, WDL_String* pDir = 0, const char* ext = "");
-  bool PromptForColor(IColor& colour, const char* str);
+  void PromptForFile(WDL_String& filename, EFileAction action = kFileOpen, WDL_String* pDir = 0, const char* ext = "") override;
+  bool PromptForColor(IColor& colour, const char* str) override;
 
   IPopupMenu* GetItemMenu(long idx, long &idxInMenu, long &offsetIdx, IPopupMenu& baseMenu);
   HMENU CreateMenu(IPopupMenu& menu, long* offsetIdx);
-  IPopupMenu* CreateIPopupMenu(IPopupMenu& menu, IRECT& areaRect);
-  void CreateTextEntry(IControl* pControl, const IText& text, const IRECT& textRect, const char* str, IParam* pParam);
+
+  IPopupMenu* CreateIPopupMenu(IPopupMenu& menu, IRECT& areaRect) override;
+  void CreateTextEntry(IControl* pControl, const IText& text, const IRECT& textRect, const char* str, IParam* pParam) override;
 
   bool OpenURL(const char* url, const char* msgWindowTitle = 0, const char* confirmMsg = 0, const char* errMsgOnFailure = 0);
 
