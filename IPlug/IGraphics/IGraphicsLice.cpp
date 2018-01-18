d6c4ee3ab9df6e53d0b58aa39d5c1f58594d4dfc
Oli Larkin <olilarkin@googlemail.com>
2018-01-18 21:53:17 +0000

    revert win blitting changes

--- IPlug/IGraphics/IGraphicsLice.cpp
+++ IPlug/IGraphics/IGraphicsLice.cpp
@@ -496,11 +496,14 @@ void IGraphicsLice::RenderAPIBitmap(void *pContext)
 #endif
     
 #else // OS_WIN
-  HDC dc = (HDC) pContext;
+  PAINTSTRUCT ps;
+  HWND hWnd = (HWND)GetWindow();
+  HDC dc = BeginPaint(hWnd, &ps);
 
   if (Scale() == 1.0)
-    BitBlt(dc, 0, 0, Width(), Height(), (HDC) pContext, 0, 0, SRCCOPY); 
+ 	BitBlt(dc, 0, 0, Width(), Height(), mDrawBitmap->getDC(), 0, 0, SRCCOPY);
   else
-    StretchBlt(dc, 0, 0, WindowWidth(), WindowHeight(), (HDC) pContext, 0, 0, Width(), Height(), SRCCOPY);
+	StretchBlt(dc, 0, 0, WindowWidth(), WindowHeight(), mDrawBitmap->getDC(), 0, 0, Width(), Height(), SRCCOPY);
+  EndPaint(hWnd, &ps);
 #endif
 }
--- IPlug/IGraphics/IGraphicsWin.cpp
+++ IPlug/IGraphics/IGraphicsWin.cpp
@@ -277,14 +277,9 @@ LRESULT CALLBACK IGraphicsWin::WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARA
       RECT r;
       if (GetUpdateRect(hWnd, &r, FALSE))
       { 
-        PAINTSTRUCT ps;
-        pGraphics->SetPlatformContext(nullptr);
-        HDC dc = BeginPaint(hWnd, &ps);
-        pGraphics->SetPlatformContext(dc);
         IRECT ir(r.left, r.top, r.right, r.bottom);
         ir.ScaleBounds(1. / pGraphics->Scale());
         pGraphics->Draw(ir);
-        EndPaint(hWnd, &ps);
       }
       return 0;
     }
