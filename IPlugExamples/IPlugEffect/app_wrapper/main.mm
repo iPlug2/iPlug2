8485907e4b40172e9c79527e192a3c72e36f8841
Oli Larkin <olilarkin@googlemail.com>
2018-01-31 23:38:57 +0000

    appwrapper

--- IPlugExamples/IPlugEffect/app_wrapper/main.mm
+++ IPlugExamples/IPlugEffect/app_wrapper/main.mm
@@ -4,20 +4,6 @@
 #include "wdlstring.h"
 #include "swell.h"
 
-int main(int argc, char *argv[])
-{
-  //if invoked with an argument registerauv3 use plug-in kit to explicitly register auv3 app extension (doesn't happen from debugger)
-  if(strcmp(argv[2], "registerauv3"))
-  {
-    WDL_String appexPath(argv[0]);
-    appexPath.SetFormatted(1024, "pluginkit -a %s%s%s.appex", argv[0], "/../../Plugins/", appexPath.get_filepart());
-    if(system(appexPath.Get()) > -1)
-      printf("registered audiounit app extension\n");
-  }
-  
-  return NSApplicationMain(argc,  (const char **) argv);
-}
-
 void CenterWindow(HWND hwnd)
 {
   if (!hwnd) return;
@@ -34,3 +20,33 @@ void CenterWindow(HWND hwnd)
     [turd center];
   }
 }
+
+bool IsSandboxed()
+{
+  NSString* pHomeDir = NSHomeDirectory();
+  //  NSString* pBundleIdentifier = [[NSBundle mainBundle] bundleIdentifier];
+  
+  if ([pHomeDir containsString:@"Library/Containers/"])
+  {
+    return true;
+  }
+  return false;
+}
+
+int main(int argc, char *argv[])
+{
+  //if invoked with an argument registerauv3 use plug-in kit to explicitly register auv3 app extension (doesn't happen from debugger)
+  if(strcmp(argv[2], "registerauv3"))
+  {
+    WDL_String appexPath(argv[0]);
+    appexPath.SetFormatted(1024, "pluginkit -a %s%s%s.appex", argv[0], "/../../Plugins/", appexPath.get_filepart());
+    if(system(appexPath.Get()) > -1)
+      NSLog(@"Registered audiounit app extension\n");
+    
+    if(IsSandboxed())
+      NSLog(@"SANDBOXED\n");
+  }
+  
+  return NSApplicationMain(argc,  (const char **) argv);
+}
+
