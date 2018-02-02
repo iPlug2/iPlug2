a84723bfbf2e3b34edaa4fede3e3965a6c5cedd5
Alex Harker <ajharker@gmail.com>
2018-02-02 21:25:43 +0000

    More formatting (spaces before function parentheses in header)

--- IPlug/IPlugVST3.h
+++ IPlug/IPlugVST3.h
@@ -36,27 +36,27 @@ public:
   virtual ~IPlugVST3();
 
   // AudioEffect
-  Steinberg::tresult PLUGIN_API initialize (FUnknown* context) override;
+  Steinberg::tresult PLUGIN_API initialize(FUnknown* context) override;
   Steinberg::tresult PLUGIN_API terminate() override;
   Steinberg::tresult PLUGIN_API setBusArrangements(Steinberg::Vst::SpeakerArrangement* inputs, Steinberg::int32 numIns, Steinberg::Vst::SpeakerArrangement* outputs, Steinberg::int32 numOuts) override;
   Steinberg::tresult PLUGIN_API setActive(Steinberg::TBool state) override;
-  Steinberg::tresult PLUGIN_API setupProcessing (Steinberg::Vst::ProcessSetup& newSetup) override;
+  Steinberg::tresult PLUGIN_API setupProcessing(Steinberg::Vst::ProcessSetup& newSetup) override;
   Steinberg::tresult PLUGIN_API process(Steinberg::Vst::ProcessData& data) override;
 //  Steinberg::tresult PLUGIN_API setState(IBStream* state) override;
 //  Steinberg::tresult PLUGIN_API getState(IBStream* state) override;
 //  Steinberg::tresult PLUGIN_API setComponentState(IBStream *state) override;
   Steinberg::tresult PLUGIN_API canProcessSampleSize(Steinberg::int32 symbolicSampleSize) override;
-  Steinberg::uint32 PLUGIN_API getLatencySamples () override;
+  Steinberg::uint32 PLUGIN_API getLatencySamples() override;
   Steinberg::uint32 PLUGIN_API getTailSamples() override { return GetTailSize(); }
   // IEditController
-  Steinberg::IPlugView* PLUGIN_API createView (const char* name) override;
-  Steinberg::tresult PLUGIN_API setEditorState (Steinberg::IBStream* state) override;
-  Steinberg::tresult PLUGIN_API getEditorState (Steinberg::IBStream* state) override;
-  Steinberg::tresult PLUGIN_API setParamNormalized (Steinberg::Vst::ParamID tag, Steinberg::Vst::ParamValue value) override;
+  Steinberg::IPlugView* PLUGIN_API createView(const char* name) override;
+  Steinberg::tresult PLUGIN_API setEditorState(Steinberg::IBStream* state) override;
+  Steinberg::tresult PLUGIN_API getEditorState(Steinberg::IBStream* state) override;
+  Steinberg::tresult PLUGIN_API setParamNormalized(Steinberg::Vst::ParamID tag, Steinberg::Vst::ParamValue value) override;
   Steinberg::Vst::ParamValue PLUGIN_API getParamNormalized(Steinberg::Vst::ParamID tag) override;
   Steinberg::Vst::ParamValue PLUGIN_API plainParamToNormalized(Steinberg::Vst::ParamID tag, Steinberg::Vst::ParamValue plainValue) override;
-  Steinberg::tresult PLUGIN_API getParamStringByValue (Steinberg::Vst::ParamID tag, Steinberg::Vst::ParamValue valueNormalized, Steinberg::Vst::String128 string) override;
-  Steinberg::tresult PLUGIN_API getParamValueByString (Steinberg::Vst::ParamID tag, Steinberg::Vst::TChar* string, Steinberg::Vst::ParamValue& valueNormalized) override;
+  Steinberg::tresult PLUGIN_API getParamStringByValue(Steinberg::Vst::ParamID tag, Steinberg::Vst::ParamValue valueNormalized, Steinberg::Vst::String128 string) override;
+  Steinberg::tresult PLUGIN_API getParamValueByString(Steinberg::Vst::ParamID tag, Steinberg::Vst::TChar* string, Steinberg::Vst::ParamValue& valueNormalized) override;
 
   //IUnitInfo
   Steinberg::int32 PLUGIN_API getUnitCount() override;
@@ -68,7 +68,7 @@ public:
   virtual Steinberg::tresult PLUGIN_API getProgramInfo(Steinberg::Vst::ProgramListID listId, Steinberg::int32 programIndex, Steinberg::Vst::CString attributeId, Steinberg::Vst::String128 attributeValue) override {return Steinberg::kNotImplemented;}
   virtual Steinberg::tresult PLUGIN_API hasProgramPitchNames(Steinberg::Vst::ProgramListID listId, Steinberg::int32 programIndex) override {return Steinberg::kNotImplemented;}
   virtual Steinberg::tresult PLUGIN_API getProgramPitchName(Steinberg::Vst::ProgramListID listId, Steinberg::int32 programIndex, Steinberg::int16 midiPitch, Steinberg::Vst::String128 name) override {return Steinberg::kNotImplemented;}
-  virtual Steinberg::Vst::UnitID PLUGIN_API getSelectedUnit () override {return Steinberg::Vst::kRootUnitId;}
+  virtual Steinberg::Vst::UnitID PLUGIN_API getSelectedUnit() override {return Steinberg::Vst::kRootUnitId;}
   virtual Steinberg::tresult PLUGIN_API selectUnit(Steinberg::Vst::UnitID unitId) override {return Steinberg::kNotImplemented;}
   virtual Steinberg::tresult PLUGIN_API getUnitByBus(Steinberg::Vst::MediaType type, Steinberg::Vst::BusDirection dir, Steinberg::int32 busIndex, Steinberg::int32 channel, Steinberg::Vst::UnitID& unitId) override {return Steinberg::kNotImplemented;}
   virtual Steinberg::tresult PLUGIN_API setUnitProgramData(Steinberg::int32 listOrUnitId, Steinberg::int32 programIndex, Steinberg::IBStream* data) override {return Steinberg::kNotImplemented;}
@@ -79,7 +79,7 @@ public:
   virtual void EndInformHostOfParamChange(int idx) override;
   virtual void InformHostOfProgramChange() override {};
   
-  virtual bool IsRenderingOffline() override { return (processSetup.processMode == Steinberg::Vst::kOffline); }
+  virtual bool IsRenderingOffline() override { return(processSetup.processMode == Steinberg::Vst::kOffline); }
 
   virtual int GetSamplePos() override;
   virtual double GetTempo() override;
@@ -106,17 +106,17 @@ public:
 //    kAftertouchParam = 'aftt',
   };
 
-  OBJ_METHODS (IPlugVST3, SingleComponentEffect)
+  OBJ_METHODS(IPlugVST3, SingleComponentEffect)
   DEFINE_INTERFACES
-  END_DEFINE_INTERFACES (SingleComponentEffect)
+  END_DEFINE_INTERFACES(SingleComponentEffect)
   REFCOUNT_METHODS(SingleComponentEffect)
 
 protected:
   virtual bool SendMidiMsg(IMidiMsg& msg) override { return false; } //TODO: SendMidiMsg
 
 private:
-  void addDependentView (IPlugVST3View* view);
-  void removeDependentView (IPlugVST3View* view);
+  void addDependentView(IPlugVST3View* view);
+  void removeDependentView(IPlugVST3View* view);
   virtual Steinberg::tresult beginEdit(Steinberg::Vst::ParamID tag) override;
   virtual Steinberg::tresult performEdit(Steinberg::Vst::ParamID tag, Steinberg::Vst::ParamValue valueNormalized) override;
   virtual Steinberg::tresult endEdit(Steinberg::Vst::ParamID tag) override;
@@ -140,7 +140,7 @@ class IPlugVST3View : public Steinberg::CPluginView
 {
 public:
   IPlugVST3View(IPlugVST3* pPlug);
-  ~IPlugVST3View ();
+  ~IPlugVST3View();
 
   // CPluginView overides
   Steinberg::tresult PLUGIN_API attached(void* parent, Steinberg::FIDString type);
