c74dcfd54b9a5231ad3cdc169c354099e56df2a8
Oli Larkin <olilarkin@googlemail.com>
2018-02-07 14:54:35 +0000

    print out location of tracer log file in IGraphics tracer builds

--- IPlug/IPlugBaseGraphics.cpp
+++ IPlug/IPlugBaseGraphics.cpp
@@ -81,4 +81,11 @@ void IPlugBaseGraphics::PrintDebugInfo()
   WDL_String buildInfo;
   GetBuildInfoStr(buildInfo);
   DBGMSG("\n%s\n%s Graphics %i FPS\n--------------------------------------------------\n", buildInfo.Get(), mGraphics->GetDrawingAPIStr(), mGraphics->FPS());
+
+#if defined TRACER_BUILD && !defined TRACE_TO_STDOUT
+  WDL_String pHomePath;
+  mGraphics->UserHomePath(pHomePath);
+  DBGMSG("Location of the Tracer Build Log: \n%s/%s\n\n", pHomePath.Get(), LOGFILE);
+#endif
+  
 }
