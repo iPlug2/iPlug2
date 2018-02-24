b25b90018c07f287758812760ce8a47a3cd3c001
Oli Larkin <olilarkin@googlemail.com>
2018-02-24 11:01:39 +0000

    compile/register auv3 again

--- IPlug/APP/IPlugAPP_main.cpp
+++ IPlug/APP/IPlugAPP_main.cpp
@@ -101,6 +101,20 @@ extern HMENU SWELL_app_stocksysmenu;
 
 int main(int argc, char *argv[])
 {
+  //if invoked with an argument registerauv3 use plug-in kit to explicitly register auv3 app extension (doesn't happen from debugger)
+  if(strcmp(argv[2], "registerauv3"))
+  {
+    WDL_String appexPath(argv[0]);
+    appexPath.SetFormatted(1024, "pluginkit -a %s%s%s.appex", argv[0], "/../../Plugins/", appexPath.get_filepart());
+    if(system(appexPath.Get()) > -1)
+      NSLog(@"Registered audiounit app extension\n");
+    else
+      NSLog(@"Failed to register audiounit app extension\n");
+
+//    if(IsSandboxed())
+//      NSLog(@"SANDBOXED\n");
+  }
+  
   return NSApplicationMain(argc,  (const char **) argv);
 }
 
