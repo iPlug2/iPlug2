38fa1bbdf2616c14be9dff233e60d55dde8c4d44
Oli Larkin <olilarkin@googlemail.com>
2018-03-12 19:53:59 +0000

    Add MIDISynth: A polyphonic & monophonic synthesiser class
    Inherit from MIDISynth::SynthVoice in order to implement your custom synth voice

---
+++ IPlug/IPlugMIDISynth.cpp
@@ -0,0 +1,373 @@
+#include "IPlugMIDISynth.h"
+
+MIDISynth::MIDISynth(EPolyMode polyMode)
+{
+  //TODO: check this should stop any allocations
+  mSustainedNotes.reserve(128);
+  mHeldKeys.reserve(128);
+  
+  for (int i=0; i<128; i++)
+  {
+    mVelocityLUT[i] = i;
+    mAfterTouchLUT[i] = i;
+  }
+}
+
+MIDISynth::~MIDISynth()
+{
+  mVS.Empty(true);
+}
+
+bool MIDISynth::ProcessBlock(double** inputs, double** outputs, int nFrames)
+{
+  assert(NVoices());
+  
+  if (mVoicesAreActive | !mMidiQueue.Empty())
+  {
+    SynthVoice* pVoice;
+
+    for (int s = 0; s < nFrames; ++s)
+    {
+      mSampleTime++;
+
+      //TODO: here there should be a mechanism for updating "click safe" variables
+
+      while (!mMidiQueue.Empty())
+      {
+        IMidiMsg& msg = mMidiQueue.Peek();
+
+        if (msg.mOffset > s) break;
+
+        int status = msg.StatusMsg(); // get the MIDI status byte
+
+        switch (status)
+        {
+          case IMidiMsg::kNoteOn:
+          case IMidiMsg::kNoteOff:
+          {
+            if (mPolyMode == kPolyModePoly)
+              NoteOnOffPoly(msg);
+            else
+              NoteOnOffMono(msg);
+
+            break;
+          }
+          case IMidiMsg::kPolyAftertouch:
+          {
+            for(int v = 0; v < NVoices(); v++)
+            {
+              if(mATMode == kATModePoly && GetVoice(v)->mKey == msg.NoteNumber())
+              {
+                GetVoice(v)->mAftertouch = mAfterTouchLUT[msg.PolyAfterTouch()] / 127.;
+              }
+            }
+
+            break;
+          }
+          case IMidiMsg::kChannelAftertouch:
+          {
+            if(mATMode == kATModeChannel)
+            {
+              double val = mAfterTouchLUT[msg.ChannelAfterTouch()] / 127.;
+
+              for(int v = 0; v < NVoices(); v++)
+              {
+                GetVoice(v)->mAftertouch = val;
+              }
+            }
+            break;
+          }
+          case IMidiMsg::kPitchWheel:
+          {
+            mPitchBend = msg.PitchWheel() * mPitchBendRange;
+            break;
+          }
+          case IMidiMsg::kControlChange:
+          {
+            switch (msg.ControlChangeIdx())
+            {
+              case IMidiMsg::kModWheel:
+                mModWheel = msg.ControlChange(IMidiMsg::kModWheel);
+                break;
+              case IMidiMsg::kSustainOnOff:
+
+                mSustainPedalDown = (bool) (msg.ControlChange(IMidiMsg::kSustainOnOff) >= 0.5);
+
+                if (!mSustainPedalDown) // sustain pedal released
+                {
+                  // if notes are sustaining, check that they're not still held and if not then stop voice
+                  if (!mSustainedNotes.empty())
+                  {
+                    std::vector<KeyPressInfo>::iterator susNotesItr;
+
+                    for (susNotesItr = mSustainedNotes.begin(); susNotesItr != mSustainedNotes.end();)
+                    {
+                      const bool held = std::find(mHeldKeys.begin(), mHeldKeys.end(), *susNotesItr) != mHeldKeys.end();
+
+                      if (!held)
+                      {
+                        StopVoicesForKey(susNotesItr->mKey);
+                        susNotesItr = mSustainedNotes.erase(susNotesItr);
+                      }
+                      else
+                        susNotesItr++;
+                    }
+                  }
+                }
+
+                break;
+              case IMidiMsg::kAllNotesOff:
+                mHeldKeys.clear();
+                mSustainedNotes.clear();
+                mSustainPedalDown = false;
+                SoftKillAllVoices();
+                break;
+              default:
+                break;
+            }
+            break;
+          }
+        }
+
+        mMidiQueue.Remove();
+      }
+
+      for(int v = 0; v < NVoices(); v++) // for each vs
+      {
+        pVoice = GetVoice(v);
+
+        if (pVoice->GetBusy())
+        {
+          pVoice->ProcessSample(inputs, outputs, s, mPitchBend);
+        }
+      }
+    }
+
+    bool voicesbusy = false;
+    int activeCount = 0;
+
+    for(int v = 0; v < NVoices(); v++)
+    {
+      bool busy = GetVoice(v)->GetBusy();
+      voicesbusy |= busy;
+
+      activeCount += (busy==true);
+      #if DEBUG_VOICE_COUNT
+            if(GetVoice(v)->GetBusy()) printf("X");
+            else DBGMSG("_");
+          }
+          DBGMSG("\n");
+          DBGMSG("Num Voices busy %i\n", activeCount);
+      #else
+    }
+      #endif
+
+    mVoicesAreActive = voicesbusy;
+
+    mMidiQueue.Flush(nFrames);
+  }
+  else // empty block
+  {
+    //TODO zero outputs
+    return true;
+  }
+
+  return false; // made some noise
+}
+
+#pragma mark - NOTE TRIGGER METHODS
+
+void MIDISynth::NoteOnOffPoly(const IMidiMsg& msg)
+{
+  int status = msg.StatusMsg();
+  int velocity = msg.Velocity();
+  int note = msg.NoteNumber();
+  
+  if (status == IMidiMsg::kNoteOn && velocity)
+  {
+    velocity = BOUNDED(mVelocityLUT[velocity], 1, 127);
+    double velNorm = (double) velocity / 127.;
+    
+    int v = FindFreeVoice(); // or first one triggered
+    
+    if (v == -1) // shouldn't happen
+      return;
+    
+    SynthVoice* pVoice = GetVoice(v);
+    
+    int prevKey = note;
+    
+    if (!mHeldKeys.empty())
+      prevKey = mHeldKeys.back().mKey;
+    
+    KeyPressInfo theNote = KeyPressInfo(note, velNorm);
+    
+    if(std::find(mHeldKeys.begin(), mHeldKeys.end(), theNote) == mHeldKeys.end())
+      mHeldKeys.push_back(theNote);
+    
+    if(std::find(mSustainedNotes.begin(), mSustainedNotes.end(), theNote) == mSustainedNotes.end())
+      mSustainedNotes.push_back(theNote);
+    
+    pVoice->mStartTime = mSampleTime;
+    pVoice->mKey = note;
+    pVoice->mBasePitch = GetAdjustedPitch(note);
+    pVoice->mAftertouch = 0.;
+    pVoice->Trigger(theNote.mVelNorm, pVoice->GetBusy()); // if voice is busy it will retrigger
+
+    mVoicesAreActive = true;
+  }
+  else  // Note off
+  {
+    std::vector<KeyPressInfo>::iterator it;
+    bool erase = false;
+    
+    // REMOVE released key from held keys list. Do this even if the sustain pedal is down
+    for (it = mHeldKeys.begin(); it != mHeldKeys.end(); it++)
+    {
+      if (it->mKey == note)
+      {
+        erase = true;
+        break;
+      }
+    }
+    
+    if (erase)
+      mHeldKeys.erase(it);
+    
+    if (!mSustainPedalDown)
+    {
+      // REMOVE released key from sustained keys list
+      erase = false;
+      
+      for (it = mSustainedNotes.begin(); it != mSustainedNotes.end(); it++)
+      {
+        if (it->mKey == note)
+        {
+          erase = true;
+          break;
+        }
+      }
+      
+      if (erase)
+        mSustainedNotes.erase(it);
+      
+      // now stop voices associated with this key
+      StopVoicesForKey(note);
+    }
+  }
+}
+
+void MIDISynth::NoteOnOffMono(const IMidiMsg& msg)
+{
+  int status = msg.StatusMsg();
+  int velocity = msg.Velocity();
+  int note = msg.NoteNumber();
+  
+  if (status == IMidiMsg::kNoteOn && velocity)
+  {
+    velocity = BOUNDED(mVelocityLUT[velocity], 1, 127);
+    double velNorm = (double) velocity / 127.;
+    
+    KeyPressInfo theNote = KeyPressInfo(note, velNorm);
+    
+    if(std::find(mHeldKeys.begin(), mHeldKeys.end(), theNote) == mHeldKeys.end())
+      mHeldKeys.push_back(theNote);
+    
+    // kinda stupid in mono modes only ever 1 sustained note
+    mSustainedNotes.clear();
+    mSustainedNotes.push_back(theNote);
+    
+    TriggerMonoNote(theNote);
+    
+    mPrevVelNorm = velNorm;
+  }
+  else  // Note off
+  {
+    std::vector<KeyPressInfo>::iterator it;
+    bool erase = false;
+    
+    // REMOVE released key from held keys list. Do this even if the sustain pedal is down
+    for (it = mHeldKeys.begin(); it != mHeldKeys.end(); it++)
+    {
+      if (it->mKey == note)
+      {
+        erase = true;
+        break;
+      }
+    }
+    
+    if (erase)
+      mHeldKeys.erase(it);
+    
+    // if there are still held keys...
+    if(!mHeldKeys.empty())
+    {
+      KeyPressInfo queuedNote = mHeldKeys.back();
+      
+      if (queuedNote.mKey != GetVoice(0)->mKey)
+      {
+        // kinda stupid in mono modes only ever 1 sustained note
+        mSustainedNotes.clear();
+        mSustainedNotes.push_back(queuedNote);
+        TriggerMonoNote(queuedNote);
+      }
+    }
+    else
+    {
+      if (mSustainPedalDown)
+      {
+        KeyPressInfo queuedNote = mSustainedNotes.back();
+        
+        if (queuedNote.mKey != GetVoice(0)->mKey)
+        {
+          // don't need to add to sustained queue, allready in it
+          TriggerMonoNote(queuedNote);
+        }
+      }
+      else
+        StopVoicesForKey(note);
+    }
+  }
+}
+
+void MIDISynth::TriggerMonoNote(KeyPressInfo note)
+{
+  for (int v = 0; v < mUnisonVoices; v++)
+  {
+    SynthVoice* pVoice = GetVoice(v);
+    
+    pVoice->mKey = note.mKey;
+    pVoice->mStackIdx = v;
+    pVoice->mBasePitch = GetAdjustedPitch(note.mKey);
+    pVoice->mAftertouch = 0.;
+    
+    const bool voiceFree = !pVoice->GetBusy();
+    const bool voiceReleased = pVoice->GetReleased();
+
+    if (voiceFree)
+    {
+      pVoice->Trigger(note.mVelNorm, false);
+    }
+    else if(mPolyMode == kPolyModeMono || voiceReleased)
+    {
+      pVoice->Trigger(note.mVelNorm, true);
+    }
+  }
+  
+  mVoicesAreActive = true;
+}
+
+void MIDISynth::SetSampleRateAndBlockSize(double sampleRate, int blockSize)
+{
+  HardKillAllVoices();
+  
+  mHeldKeys.clear();
+  mSustainedNotes.clear();
+  mSampleRate = sampleRate;
+  mMidiQueue.Resize(blockSize);
+  
+  for(int v = 0; v < NVoices(); v++)
+  {
+    GetVoice(v)->SetSampleRate(sampleRate);
+  }
+}
---
+++ IPlug/IPlugMIDISynth.h
@@ -0,0 +1,301 @@
+#pragma once
+
+/**
+ * @file
+ * @copydoc MIDISynth
+ */
+
+#include <vector>
+#include <stdint.h>
+
+#include "ptrlist.h"
+
+#include "IPlugConstants.h"
+#include "IPlugMidi.h"
+
+#define DEBUG_VOICE_COUNT 0
+
+/** A monophonic/polyphonic synthesiser base class which can be supplied with a custom voice.
+ *  Supports different kinds of after touch, pitch bend, velocity and after touch curves, unison (currently monophonic mode only)
+ *  NOTE: This is not currently particularly efficient, and needs a bit more work to be more generalisable */
+class MIDISynth
+{
+public:
+  struct KeyPressInfo
+  {
+    int mKey;
+    double mVelNorm;
+
+    KeyPressInfo(int key, double velNorm)
+    : mKey(key)
+    , mVelNorm(velNorm)
+    {
+    }
+
+    friend bool operator==(const KeyPressInfo& lhs, const KeyPressInfo& rhs);
+  };
+
+  enum EATMode
+  {
+    kATModeChannel = 0,
+    kATModePoly,
+    kNumATModes
+  };
+
+  enum EPolyMode
+  {
+    kPolyModePoly = 0,
+    kPolyModeLegato,
+    kPolyModeMono,
+    kNumPolyModes
+  };
+
+#pragma mark - Voice class
+  class SynthVoice
+  {
+  public:
+    virtual bool GetBusy() const = 0;
+
+    /** @return true if voice is free or amp envs are in release stage */
+    virtual bool GetReleased() const = 0;
+
+    /** Trigger is called when a new voice should start, or if the voice limit has been hit and an existing voice needs to re-trigger
+     * @param level Normalised starting level for this voice, derived from the velocity of the keypress, or in the case of a re-trigger the existing level \todo check
+     * @param isRetrigger If this is \c true it means the voices being re-triggered, and you should accommodate for this in your algorithm */
+    virtual void Trigger(double level, bool isRetrigger) { DBGMSG("Voice Triggered\n"); }
+
+    /** Release envelopes on note off message */
+    virtual void Release() { DBGMSG("Voice Released\n"); }
+
+    /** Kill a playing voice. Hard kill should kill voice immediately (potentially causing glitch)
+     *  Soft kill should kill voice as quickly as possible with a fade out to avoid glitch */
+    virtual void Kill(bool isSoft) { DBGMSG("Voice Hard Killed\n"); }
+
+    /** Process a single sample of audio data for the voice
+     * @param inputs Pointer to input channel arrays (sometimes synthesisers have audio inputs)
+     * @param outputs Pointer to output channel arrays
+     * @param sampleIdx The index of the sample to process */
+    virtual void ProcessSample(sample** inputs, sample** outputs, int sampleIdx, double pitchBend)
+    {
+      outputs[0][sampleIdx] = 0.;
+    }
+
+    /** If you have members that need to update when the sample rate changes you can do that by overriding this method
+     * @param sampleRate The new sample rate */
+    virtual void SetSampleRate(double sampleRate) {};
+
+  private:
+    void RemovedFromKey()
+    {
+      mKey = -1;
+      mAftertouch = 0.;
+    }
+
+  public:
+    int64_t mStartTime = -1;
+    bool mLastBusy = false;
+    int mKey = -1;
+    double mBasePitch = 0.;
+    double mAftertouch = 0.;
+    int mStackIdx = -1;
+    
+    friend class MIDISynth;
+  };
+
+public:
+#pragma mark - Engine class
+  MIDISynth(EPolyMode polyMode = kPolyModePoly);
+  ~MIDISynth();
+
+  void NoteOnOffMono(const IMidiMsg& msg);
+
+  void NoteOnOffPoly(const IMidiMsg& msg);
+
+  inline void TriggerMonoNote(KeyPressInfo note);
+
+  inline void StopVoicesForKey(int note)
+  {
+    // now stop voices associated with this key
+    for (int v = 0; v < NVoices(); v++)
+    {
+      if (GetVoice(v)->mKey == note)
+      {
+        if (GetVoice(v)->GetBusy())
+        {
+          StopVoice(*GetVoice(v));
+        }
+      }
+    }
+  }
+
+  inline void StopVoice(SynthVoice& voice)
+  {
+    voice.Release();
+    voice.RemovedFromKey();
+  }
+
+  /** Override this method if you need to implement a tuning table for microtonal support
+   * @param key The input MIDI pitch of the key pressed
+   * @return The adjusted MIDI pitch */
+  virtual double GetAdjustedPitch(int key)
+  {
+    return key;
+  }
+
+  inline void ReleaseAllVoices()
+  {
+    for (int v = 0; v < NVoices(); v++)
+    {
+      if (GetVoice(v)->GetBusy())
+      {
+        SynthVoice* pVoice = GetVoice(v);
+        pVoice->Release();
+        pVoice->RemovedFromKey();
+      }
+    }
+  }
+
+  inline void SoftKillAllVoices()
+  {
+    for (int v = 0; v < NVoices(); v++)
+    {
+      SynthVoice* pVoice = GetVoice(v);
+      pVoice->Kill(true);
+      pVoice->RemovedFromKey();
+    }
+  }
+
+  inline void HardKillAllVoices()
+  {
+    for (int v = 0; v < NVoices(); v++)
+    {
+      SynthVoice* pVoice = GetVoice(v);
+      pVoice->Kill(false);
+      pVoice->RemovedFromKey();
+    }
+  }
+
+  inline int CheckKey(int key)
+  {
+    for(int v = 0; v < NVoices(); v++)
+    {
+      if(GetVoice(v)->mKey == key)
+        return v;
+    }
+
+    return -1;
+  }
+
+  inline bool VoicesAreBusy()
+  {
+    for(int v = 0; v < NVoices(); v++)
+    {
+      if(GetVoice(v)->GetBusy())
+        return true;
+    }
+
+    return false;
+  }
+
+  inline int FindFreeVoice()
+  {
+    for(int v = 0; v < NVoices(); v++)
+    {
+      if(!GetVoice(v)->GetBusy())
+        return v;
+    }
+
+    int64_t mostRecentTime = mSampleTime;
+    int longestPlayingVoice = -1;
+
+    for(int v = 0; v < NVoices(); v++)
+    {
+      if (GetVoice(v)->mStartTime < mostRecentTime)
+      {
+        longestPlayingVoice = v;
+        mostRecentTime = GetVoice(v)->mStartTime;
+      }
+    }
+
+    return longestPlayingVoice;
+  }
+
+  void ResetSampleTime()
+  {
+    mSampleTime = 0;
+  }
+
+  void SetSampleRateAndBlockSize(double sampleRate, int blockSize);
+
+  void SetPitchBendRange(double rangeInSemitones)
+  {
+    mPitchBendRange = rangeInSemitones;
+  }
+  
+  void SetPolyMode(EPolyMode mode)
+  {
+    mPolyMode = mode; //TODO: implement click safe solution
+  }
+  
+  void SetATMode(EATMode mode)
+  {
+    mATMode = mode; //TODO: implement click safe solution
+  }
+
+  inline SynthVoice* GetVoice(int voiceIdx)
+  {
+    return mVS.Get(voiceIdx);
+  }
+
+  int NVoices() const
+  {
+    return mVS.GetSize();
+  }
+
+  void AddVoice(SynthVoice* pVoice)
+  {
+    mVS.Add(pVoice);
+  }
+
+  void AddMidiMsgToQueue(const IMidiMsg& msg)
+  {
+    mMidiQueue.Add(msg);
+  }
+
+  // return \c true if the synth is silent
+
+  /** Processes a block of audio samples
+   * @param inputs Pointer to input Arrays
+   * @param outputs Pointer to output Arrays
+   * @param nFrames The number of sample frames to process
+   * @return \c true if the synth is silent */
+  bool ProcessBlock(double** inputs, double** outputs, int nFrames);
+
+private:
+  WDL_PtrList<SynthVoice> mVS;
+
+  int64_t mSampleTime = 0;
+  double mSampleRate = DEFAULT_SAMPLE_RATE;
+  double mPitchBendRange = 12.;
+  double mPitchBend = 0.;
+  double mModWheel = 0.; //TODO: not used
+  double mPrevVelNorm = 0.; //TODO: not used
+
+  bool mSustainPedalDown = false;
+  bool mVoicesAreActive = false;
+  uint16_t mUnisonVoices = 1;
+
+  EPolyMode mPolyMode = kPolyModePoly; // mono note priority / polyphony
+  EATMode mATMode = kATModeChannel;
+  std::vector<KeyPressInfo> mHeldKeys; // The currently physically held keys on the keyboard
+  std::vector<KeyPressInfo> mSustainedNotes; // Any notes that are sustained, including those that are physically held
+  IMidiQueue mMidiQueue;
+
+public: // these are public for state saving
+  int mVelocityLUT[128];
+  int mAfterTouchLUT[128];
+
+} WDL_FIXALIGN;
+
+inline bool operator==(const MIDISynth::KeyPressInfo& lhs, const MIDISynth::KeyPressInfo& rhs) { return lhs.mKey == rhs.mKey; }
+
