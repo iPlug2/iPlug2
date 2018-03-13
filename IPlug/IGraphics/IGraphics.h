adeb69e0a2091b9676a5c13c33d7edb364ae3c98
Oli Larkin <olilarkin@googlemail.com>
2018-03-13 10:51:02 +0000

    remove a stray PathRoundRect

--- IPlug/IGraphics/IGraphics.h
+++ IPlug/IGraphics/IGraphics.h
@@ -412,7 +412,6 @@ public:
   virtual void PathRect(const IRECT& bounds) {}
   virtual void PathRoundRect(const IRECT& bounds, float ctl, float ctr, float cbl, float cbr) {}
   virtual void PathRoundRect(const IRECT& bounds, float cr = 5.f) {}
-  virtual void PathRoundRect(const IRECT& bounds, float tl, float tr, float br, float bl) {}
   virtual void PathArc(float cx, float cy, float r, float aMin, float aMax) {}
   virtual void PathCircle(float cx, float cy, float r) {}
   virtual void PathEllipse(float x, float y, float r1, float r2, float angle = 0.0) {}
