1256d004c6b034d9a31668204f50217bc359a943
Oli Larkin <olilarkin@googlemail.com>
2018-03-15 11:07:06 +0000

    fix oversampler and simpilfy interface

--- IPlug/IPlugOversampler.h
+++ IPlug/IPlugOversampler.h
@@ -11,7 +11,7 @@ using namespace hiir;
 class OverSampler
 {
 public:
-  
+
   enum EFactor
   {
     kNone = 0,
@@ -21,7 +21,7 @@ public:
     k16x,
     kNumFactors
   };
-  
+
   OverSampler()
   : mWritePos(0)
   , mDownSamplerOutput(0.f)
@@ -84,157 +84,154 @@ public:
     mUpsampler16x.set_coefs(coeffs16x);
     mDownsampler16x.set_coefs(coeffs16x);
 
-    ClearBuffers();
+    Reset();
   }
 
   ~OverSampler()
   {
   }
 
-  inline void ProcessUp16x(double input)
+  void Reset()
   {
-    mUpsampler2x.process_sample(mUp2x[0], mUp2x[1], input);
-    mUpsampler4x.process_block(mUp2x, mUp4x, 2);
-    mUpsampler8x.process_block(mUp4x, mUp8x, 4);
-    mUpsampler16x.process_block(mUp8x, mUp16x, 8);
-  }
-
-  inline void ProcessUp8x(double input)
-  {
-    mUpsampler2x.process_sample(mUp2x[0], mUp2x[1], input);
-    mUpsampler4x.process_block(mUp2x, mUp4x, 2);
-    mUpsampler8x.process_block(mUp4x, mUp8x, 4);
-  }
-
-  inline void ProcessUp4x(double input)
-  {
-    mUpsampler2x.process_sample(mUp2x[0], mUp2x[1], input);
-    mUpsampler4x.process_block(mUp4x, mUp2x, 2);
-  }
-
-  inline void ProcessUp2x(double input)
-  {
-    mUpsampler2x.process_sample(mUp2x[0], mUp2x[1], input);
-  }
-
-  inline void ProcessDown16x(double input)
-  {
-    mDown16x[mWritePos] = (float) input;
-
-    mWritePos++;
-    mWritePos &= 15;
-
-    if(mWritePos == 0)
-    {
-      mDownsampler16x.process_block(mDown8x, mDown16x, 8);
-      mDownsampler8x.process_block(mDown4x, mDown8x, 4);
-      mDownsampler4x.process_block(mDown2x, mDown4x, 2);
-      mDownSamplerOutput = mDownsampler2x.process_sample(mDown2x);
-    }
-  }
-
-  inline void ProcessDown8x(double input)
-  {
-    mDown8x[mWritePos] = (float) input;
-
-    mWritePos++;
-    mWritePos &= 7;
-
-    if(mWritePos == 0)
-    {
-      mDownsampler8x.process_block(mDown4x, mDown8x, 4);
-      mDownsampler4x.process_block(mDown2x, mDown4x, 2);
-      mDownSamplerOutput = mDownsampler2x.process_sample(mDown2x);
-    }
-  }
-
-  inline void ProcessDown4x(double input)
-  {
-    mDown4x[mWritePos] = (float) input;
-
-    mWritePos++;
-    mWritePos &= 3;
-
-    if(mWritePos == 0)
-    {
-      mDownsampler4x.process_block(mDown2x, mDown4x, 2);
-      mDownSamplerOutput = mDownsampler2x.process_sample(mDown2x);
-    }
-  }
-
-  inline void ProcessDown2x(double input)
-  {
-    mDown2x[mWritePos] = (float) input;
-
-    mWritePos = !mWritePos;
-
-    if(mWritePos == 0)
-    {
-      mDownSamplerOutput = mDownsampler2x.process_sample(mDown2x);
-    }
-  }
-
-  double GetDownSamplerOutput()
-  {
-    return (double) mDownSamplerOutput;
-  }
-
-  void ClearBuffers()
-  {
+    mUpsampler2x.clear_buffers();
+    mUpsampler4x.clear_buffers();
+    mUpsampler8x.clear_buffers();
+    mUpsampler16x.clear_buffers();
     mDownsampler2x.clear_buffers();
     mDownsampler4x.clear_buffers();
     mDownsampler8x.clear_buffers();
     mDownsampler16x.clear_buffers();
   }
 
-  /**
-   Over sample an input function (up sample input -> process with function -> down sample)
-
-   @param input The audio sample to input
-   @param std::function<double(double)> The function that processes the audio sample at the higher sampling rate
-   @param mOverSamplingFactor A power of 2 oversampling factor or 0 for no oversampling
-   @return The audio sample output
-   */
+  /** Over sample an input sample with a per-sample function (up sample input -> process with function -> down sample)
+   * @param input The audio sample to input
+   * @param std::function<double(double)> The function that processes the audio sample at the higher sampling rate
+   * @param mOverSamplingFactor A power of 2 oversampling factor or 0 for no oversampling
+   * @return The audio sample output */
   double Process(double input, std::function<double(double)> func)
   {
-    double upSampledInput, output;
+    double output;
 
-    upSampledInput = input;
-
-    for (int j = 0; j < mOverSamplingFactor; j++)
+    if(mOverSamplingFactor == 16)
     {
-      switch(mOverSamplingFactor) // we are switching on mOverSamplingFactor, NOT j so calls to one of the functions will happen mOverSamplingFactor times
-      {
-        case 2: ProcessUp2x(upSampledInput); break;
-        case 4: ProcessUp4x(upSampledInput); break;
-        case 8: ProcessUp8x(upSampledInput); break;
-        case 16: ProcessUp16x(upSampledInput); break;
-        default: break;
-      }
-    }
+      mUpsampler2x.process_sample(mUp2x[0], mUp2x[1], input);
+      mUpsampler4x.process_block(mUp4x, mUp2x, 2);
+      mUpsampler8x.process_block(mUp8x, mUp4x, 4);
+      mUpsampler16x.process_block(mUp16x, mUp8x, 8);
 
-    for (int j = 0; j < mOverSamplingFactor; j++)
+      for (auto i = 0; i < 16; i++)
+      {
+        mDown16x[i] = func(mUp16x[i]);
+      }
+
+      mDownsampler16x.process_block(mDown8x, mDown16x, 8);
+      mDownsampler8x.process_block(mDown4x, mDown8x, 4);
+      mDownsampler4x.process_block(mDown2x, mDown4x, 2);
+      output = mDownsampler2x.process_sample(mDown2x);
+    }
+    else if (mOverSamplingFactor == 8)
     {
-      output = func(upSampledInput); // func gets executed mOverSamplingFactor times
-
-      switch(mOverSamplingFactor)
+      mUpsampler2x.process_sample(mUp2x[0], mUp2x[1], input);
+      mUpsampler4x.process_block(mUp4x, mUp2x, 2);
+      mUpsampler8x.process_block(mUp8x, mUp4x, 4);
+      
+      for (auto i = 0; i < 8; i++)
       {
-        case 2: ProcessDown2x(output); break;
-        case 4: ProcessDown4x(output); break;
-        case 8: ProcessDown8x(output); break;
-        case 16: ProcessDown16x(output); break;
-        default: break;
+        mDown8x[i] = func(mUp8x[i]);
       }
+      
+      mDownsampler8x.process_block(mDown4x, mDown8x, 4);
+      mDownsampler4x.process_block(mDown2x, mDown4x, 2);
+      output = mDownsampler2x.process_sample(mDown2x);
     }
-
-    if(mOverSamplingFactor > 1)
-      output = GetDownSamplerOutput();
-
+    else if (mOverSamplingFactor == 4)
+    {
+      mUpsampler2x.process_sample(mUp2x[0], mUp2x[1], input);
+      mUpsampler4x.process_block(mUp4x, mUp2x, 2);
+      
+      for (auto i = 0; i < 4; i++)
+      {
+        mDown4x[i] = func(mUp4x[i]);
+      }
+      
+      mDownsampler4x.process_block(mDown2x, mDown4x, 2);
+      output = mDownsampler2x.process_sample(mDown2x);
+    }
+    else if (mOverSamplingFactor == 2)
+    {
+      mUpsampler2x.process_sample(mUp2x[0], mUp2x[1], input);
+      
+      mDown2x[0] = func(mUp2x[0]);
+      mDown2x[1] = func(mUp2x[1]);
+      output = mDownsampler2x.process_sample(mDown2x);
+    }
+    else
+    {
+      output = func(input);
+    }
+    
     return output;
   }
 
   double ProcessGen(std::function<double()> genFunc)
   {
+    auto ProcessDown16x = [&](double input)
+    {
+      mDown16x[mWritePos] = (float) input;
+      
+      mWritePos++;
+      mWritePos &= 15;
+      
+      if(mWritePos == 0)
+      {
+        mDownsampler16x.process_block(mDown8x, mDown16x, 8);
+        mDownsampler8x.process_block(mDown4x, mDown8x, 4);
+        mDownsampler4x.process_block(mDown2x, mDown4x, 2);
+        mDownSamplerOutput = mDownsampler2x.process_sample(mDown2x);
+      }
+    };
+    
+    auto ProcessDown8x = [&](double input)
+    {
+      mDown8x[mWritePos] = (float) input;
+      
+      mWritePos++;
+      mWritePos &= 7;
+      
+      if(mWritePos == 0)
+      {
+        mDownsampler8x.process_block(mDown4x, mDown8x, 4);
+        mDownsampler4x.process_block(mDown2x, mDown4x, 2);
+        mDownSamplerOutput = mDownsampler2x.process_sample(mDown2x);
+      }
+    };
+    
+    auto ProcessDown4x = [&](double input)
+    {
+      mDown4x[mWritePos] = (float) input;
+      
+      mWritePos++;
+      mWritePos &= 3;
+      
+      if(mWritePos == 0)
+      {
+        mDownsampler4x.process_block(mDown2x, mDown4x, 2);
+        mDownSamplerOutput = mDownsampler2x.process_sample(mDown2x);
+      }
+    };
+    
+    auto ProcessDown2x = [&](double input)
+    {
+      mDown2x[mWritePos] = (float) input;
+      
+      mWritePos = !mWritePos;
+      
+      if(mWritePos == 0)
+      {
+        mDownSamplerOutput = mDownsampler2x.process_sample(mDown2x);
+      }
+    };
+    
     double output;
 
     for (int j = 0; j < mOverSamplingFactor; j++)
@@ -252,14 +249,15 @@ public:
     }
 
     if(mOverSamplingFactor > 1)
-      output = GetDownSamplerOutput();
+      output = mDownSamplerOutput;
 
     return output;
   }
-  
+
   void SetOverSampling(EFactor factor)
   {
     mOverSamplingFactor = pow(2, (int) factor);
+    Reset();
   }
 
 private:
@@ -278,10 +276,13 @@ private:
   float mDown2x[2];
 
   Upsampler2xFPU<12> mUpsampler2x; // for 1x to 2x SR
+  //TODO: these could be replaced by cheaper alternatives
   Upsampler2xFPU<4> mUpsampler4x;  // for 2x to 4x SR
   Upsampler2xFPU<3> mUpsampler8x;  // for 4x to 8x SR
   Upsampler2xFPU<2> mUpsampler16x; // for 8x to 16x SR
+  
   Downsampler2xFPU<12> mDownsampler2x; // decimator for 2x to 1x SR
+  //TODO: these could be replaced by cheaper alternatives
   Downsampler2xFPU<4> mDownsampler4x;  // decimator for 4x to 2x SR
   Downsampler2xFPU<3> mDownsampler8x;  // decimator for 8x to 4x SR
   Downsampler2xFPU<2> mDownsampler16x; // decimator for 16x to 8x SR
