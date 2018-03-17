24c14ff0eb7e59709739a1280ab11fdb16cf04e5
Alex Harker <ajharker@gmail.com>
2018-03-17 23:04:21 +0000

    Allow developers to directly specify units for IParam

--- IPlug/IPlugParameter.h
+++ IPlug/IPlugParameter.h
@@ -54,7 +54,7 @@ public:
   void InitBool(const char* name, bool defaultValue, const char* label = "", const char* group = "", const char* offText = "off", const char* onText = "on"); // // LABEL not used here TODO: so why have it?
   void InitEnum(const char* name, int defaultValue, int nEnums, const char* label = "", const char* group = "", const char* listItems = 0, ...); // LABEL not used here TODO: so why have it?
   void InitInt(const char* name, int defaultValue, int minVal, int maxVal, const char* label = "", const char* group = "");
-  void InitDouble(const char* name, double defaultVal, double minVal, double maxVal, double step, const char* label = "", const char* group = "", Shape* shape = nullptr);
+  void InitDouble(const char* name, double defaultVal, double minVal, double maxVal, double step, const char* label = "", const char* group = "", Shape* shape = nullptr, EParamUnit unit = kUnitCustom);
 
   void InitSeconds(const char* name, double defaultVal = 1., double minVal = 0., double maxVal = 10., double step = 0.1, const char* group = "");
   void InitFrequency(const char* name, double defaultVal = 1000., double minVal = 0.1, double maxVal = 10000., double step = 0.1, const char* group = "");
