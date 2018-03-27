1e77ad55c98fe8dcd69f31f2784e9ceeff8474b5
Oli Larkin <olilarkin@googlemail.com>
2018-03-27 23:56:13 +0100

    REAPER_PLUGIN def

--- IPlug/IPlug_include_in_plug_hdr.h
+++ IPlug/IPlug_include_in_plug_hdr.h
@@ -13,8 +13,13 @@
 
 #define API_EXT2
 #ifdef VST2_API
+#ifdef REAPER_PLUGIN
+  #include "IPlugReaper.h"
+  typedef IPlugReaper IPlug;
+#else
   #include "IPlugVST2.h"
   typedef IPlugVST2 IPlug;
+#endif
   #define API_EXT "vst"
 #elif defined VST3_API
   #include "IPlugVST3.h"
