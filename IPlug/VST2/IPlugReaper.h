5701812040b208428c07b3fcc6b7543944e3a915
Oli Larkin <olilarkin@googlemail.com>
2018-03-28 11:36:21 +0100

    video stuff

--- IPlug/VST2/IPlugReaper.h
+++ IPlug/VST2/IPlugReaper.h
@@ -2,13 +2,10 @@
 
 #include "IPlugVST2.h"
 #include "reaper_plugin.h"
-
-#define IMPAPI(x) if (!((*((void **)&(x)) = (void *)rec->GetFunc(#x)))) errcnt++;
+#include "video_processor.h"
 
 #define REAPERAPI_IMPLEMENT
 #include "reaper_plugin_functions.h"
-
-//REAPER_PLUGIN_HINSTANCE ghInst;
 
 class IPlugReaper : public IPlugVST2
 {
@@ -16,76 +13,106 @@ public:
   IPlugReaper(IPlugInstanceInfo instanceInfo, IPlugConfig config)
   : IPlugVST2(instanceInfo, config)
   {
-    int errorCount = REAPERAPI_LoadAPI([this](const char* str)
-                                       {
+    int errorCount = REAPERAPI_LoadAPI([this](const char* str) {
                                          return (void*)mHostCallback(NULL, 0xdeadbeef, 0xdeadf00d, 0, (void*)str, 0.0);
                                        });
     if (errorCount > 0)
       LogToReaper("some errors when loading reaper api functions\n");
-    
-    LogToReaper("hello");
   }
-  
-//  double GetPlayPosition()
-//  {
-//  }
-//
-//  double GetPlayPosition2()
-//  {
-//  }
-//
-//  double GetCursorPosition()
-//  {
-//  }
-//
-//  int GetPlayState()
-//  {
-//  }
-//
-//  int GetSetRepeat(int parm)
-//  {
-//  }
-//
-//  void GetProjectPath(WDL_String& path)
-//  {
-//  }
-//
-//  bool IsInRealTimeAudio()
-//  {
-//  }
-//
-//  bool Audio_IsRunning()
-//  {
-//  }
-//
-//  void SetEditCurPos(double time, bool moveview, bool seekplay)
-//  {
-//  }
-  
-  void executeCallbackFunc(const char* str)
+
+  void SetTrackVolume(double gain)
   {
-    void (*func)();
-    *(long *)&func = mHostCallback(NULL, 0xdeadbeef, 0xdeadf00d, 0, (void*) str, 0.);
-    func();
+    MediaTrack* tr = GetReaperTrack();
+
+    if (tr != nullptr)
+      SetMediaTrackInfo_Value(tr, "D_VOL", gain);
   }
   
-  void OnPlayButton() { executeCallbackFunc("OnPlayButton"); }
-  void OnPauseButton() { executeCallbackFunc("OnPauseButton"); }
-  void OnStopButton() { executeCallbackFunc("OnStopButton"); }
-  
-//  void setTrackVolume(double gain);
-//  String getTakeName();
-//  void setTakeName(String name);
-//  String getTrackName();
-//  void setTrackName(String name);
-//  MediaTrack* getReaperTrack();
-//  MediaItem_Take* getReaperTake();
-//  void extendedStateHasChanged();
+  void GetTakeName(WDL_String& str)
+  {
+    const char* name = ::GetTakeName(GetReaperTake());
+    if (name != nullptr)
+      str.Set(name);
+  }
+
+  void SetTakeName(const char* name)
+  {
+    MediaItem_Take* tk = GetReaperTake();
+    if (tk != nullptr)
+    {
+      GetSetMediaItemTakeInfo_String(tk, "P_NAME", const_cast<char*>(name), true);
+      UpdateArrange();
+    }
+    else
+    {
+      LogToReaper("Plugin is not loaded into a Reaper take\n");
+    }
+  }
+
+  void GetTrackName(WDL_String& str)
+  {
+    char buf[2048];
+    if (GetSetMediaTrackInfo_String(GetReaperTrack(), "P_NAME", buf, false) == true)
+    {
+      str.Set(buf);
+    }
+  }
+
+  void SetTrackName(const char* name)
+  {
+    GetSetMediaTrackInfo_String(GetReaperTrack(), "P_NAME", const_cast<char*>(name), true);
+    UpdateArrange();
+  }
+
+  MediaTrack* GetReaperTrack()
+  {
+    return (MediaTrack*)mHostCallback(&mAEffect, 0xdeadbeef, 0xdeadf00e, 1, 0, 0.0);
+  }
+
+  MediaItem_Take* GetReaperTake()
+  {
+    return (MediaItem_Take*)mHostCallback(&mAEffect, 0xdeadbeef, 0xdeadf00e, 2, 0, 0.0);
+  }
   
   void LogToReaper(const char* str)
   {
     if(ShowConsoleMsg != nullptr)
       ShowConsoleMsg(str);
   }
+  
+  void InformHostOfAddedParams(int index, int numAddedParams)
+  {
+    int listadj[2] = {index, numAddedParams};
+    mHostCallback(&mAEffect, audioMasterVendorSpecific, 0xdeadbeef, audioMasterAutomate, listadj, 0.0);
+  }
+  
+  void InformHostOfRemovedParams(int index, int numRemovedParams)
+  {
+    int listadj[2] = {index, -numRemovedParams};
+    mHostCallback(&mAEffect, audioMasterVendorSpecific, 0xdeadbeef, audioMasterAutomate, listadj, 0.0);
+  }
+  
+  void InitializeVideo(void* staticProcessVideoFrame, void* staticGetVideoParam)
+  {
+    void* pCtx = (void*) mHostCallback(&mAEffect, 0xdeadbeef, 0xdeadf00e, 4, NULL, 0.0f);
+    
+    if (pCtx)
+    {
+      IREAPERVideoProcessor *(*video_CreateVideoProcessor)(void *fxctx, int version);
+      *(void**)&video_CreateVideoProcessor = (void *) mHostCallback(&mAEffect, 0xdeadbeef, 0xdeadf00d, 0, (void *) "video_CreateVideoProcessor", 0.0f);
+      if (video_CreateVideoProcessor)
+      {
+        mVideoProc = video_CreateVideoProcessor(pCtx, IREAPERVideoProcessor::REAPER_VIDEO_PROCESSOR_VERSION);
+        if (mVideoProc)
+        {
+          mVideoProc->userdata = this;
+          mVideoProc->process_frame = (IVideoFrame* (*)(IREAPERVideoProcessor*, const double*, int, double, double, int)) staticProcessVideoFrame;
+          mVideoProc->get_parameter_value = (bool (*)(IREAPERVideoProcessor*, int, double *)) staticGetVideoParam;
+        }
+      }
+    }
+  }
+  
+private:
+  IREAPERVideoProcessor* mVideoProc = nullptr;
 };
-
