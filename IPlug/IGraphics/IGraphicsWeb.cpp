abfaa13f53a87cfce5fe69212ec8413778c7a8bc
Oli Larkin <olilarkin@googlemail.com>
2018-03-28 21:44:12 +0100

    comment out non working embind

--- IPlug/IGraphics/IGraphicsWeb.cpp
+++ IPlug/IGraphics/IGraphicsWeb.cpp
@@ -8,9 +8,9 @@ IGraphicsWeb::IGraphicsWeb(IDelegate& dlg, int w, int h, int fps)
 {
   DBGMSG("HELLO IGraphics!\n");
 
-  val document = val::global("document");
-  document["body"]["style"].set("backgroundColor", "SteelBlue");
-  
+//  val document = val::global("document");
+//  document["body"]["style"].set("backgroundColor", "SteelBlue");
+//
 //  val canvas = val::global("document").call<val>("getElementById", std::string("canvas"));
 //  val ctx = canvas.call<val>("getContext", std::string("2d"));
 //  ctx.set("fillStyle", std::string("red"));
