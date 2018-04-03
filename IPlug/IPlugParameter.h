f9968b07526db6f7fb661614c14dac5ee1671dca
Alex Harker <ajharker@gmail.com>
2018-04-03 15:32:13 +0100

    IParam shape cloning

--- IPlug/IPlugParameter.h
+++ IPlug/IPlugParameter.h
@@ -33,16 +33,29 @@ public:
   struct Shape
   {
     virtual ~Shape() {}
+    virtual Shape* clone() const = 0;
     virtual void Init(const IParam& param) {}
-    virtual EDisplayType GetDisplayType() const { return kDisplayLinear; }
-    virtual double NormalizedToValue(double value, const IParam& param) const;
-    virtual double ValueToNormalized(double value, const IParam& param) const;
+    virtual EDisplayType GetDisplayType() const = 0;
+    virtual double NormalizedToValue(double value, const IParam& param) const = 0;
+    virtual double ValueToNormalized(double value, const IParam& param) const = 0;
   };
 
+  // Linear shape structs
+  struct ShapeLinear : public Shape
+  {
+      Shape* clone() const override { return new ShapeLinear(); };
+      IParam::EDisplayType GetDisplayType() const override { return kDisplayLinear; }
+      double NormalizedToValue(double value, const IParam& param) const override;
+      double ValueToNormalized(double value, const IParam& param) const override;
+    
+      double mShape;
+  };
+  
   // Non-linear shape structs
   struct ShapePowCurve : public Shape
   {
     ShapePowCurve(double shape);
+    Shape* clone() const override { return new ShapePowCurve(mShape); };
     IParam::EDisplayType GetDisplayType() const override;
     double NormalizedToValue(double value, const IParam& param) const override;
     double ValueToNormalized(double value, const IParam& param) const override;
@@ -53,7 +66,8 @@ public:
   struct ShapeExp : public Shape
   {
     void Init(const IParam& param) override;
-    IParam::EDisplayType GetDisplayType() const override { return IParam::kDisplayLog; }
+    Shape* clone() const override { return new ShapeExp(); };
+    IParam::EDisplayType GetDisplayType() const override { return kDisplayLog; }
     double NormalizedToValue(double value, const IParam& param) const override;
     double ValueToNormalized(double value, const IParam& param) const override;
     
