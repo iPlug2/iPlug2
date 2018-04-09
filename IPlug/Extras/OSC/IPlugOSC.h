19c1dc4042796eba779a981c6df85f5191392695
Oli Larkin <olilarkin@googlemail.com>
2018-04-09 11:10:00 +0100

    OSC progress. crashing when a receiver and sender exist

--- IPlug/Extras/OSC/IPlugOSC.h
+++ IPlug/Extras/OSC/IPlugOSC.h
@@ -10,6 +10,7 @@
 #include <cstring>
 #include <ctime>
 #include <unistd.h>
+#include <functional>
 
 #include "jnetlib/jnetlib.h"
 
@@ -25,11 +26,12 @@ protected:
     m_has_output = false;
     m_last_open_time = 0.0;
   }
+  
   struct rec
   {
-    void (*callback)(void *d1, void *d2, char type, int msglen, void *msg); // type=0 for MIDI, 1=osc
+    void (*callback)(void *d1, int dev_idx, char type, int msglen, void *msg); // type=0 for MIDI, 1=osc
     void *data1;
-    void *data2;
+    int dev_idx;
   };
   
   WDL_TypedBuf<rec> m_instances;
@@ -44,11 +46,11 @@ public:
   virtual const char *get_type()=0;
   
   virtual void oscSend(const char *src, int len) {}
-  virtual void midiSend(const unsigned char *buf, int len) {}
+//  virtual void midiSend(const unsigned char *buf, int len) {}
   
-  virtual void addinst(void (*callback)(void *d1, void *d2, char type, int msglen, void *msg), void *d1, void *d2)
+  virtual void addinst(void (*callback)(void *d1, int dev_idx, char type, int msglen, void *msg), void *d1, int dev_idx)
   {
-    const rec r={callback,d1,d2};
+    const rec r = {callback, d1, dev_idx};
     m_instances.Add(r);
   }
   
@@ -57,7 +59,7 @@ public:
     const int n=m_instances.GetSize();
     const rec *r = m_instances.Get();
     for (int x=0;x<n; x++)
-      if (r[x].callback) r[x].callback(r[x].data1,r[x].data2,type,len,(void*)msg);
+      if (r[x].callback) r[x].callback(r[x].data1,r[x].dev_idx, type, len, (void*)msg);
   }
 };
 
@@ -66,7 +68,10 @@ class OSCDevice : public IODevice
 public:
   OSCDevice(const char* dest, int maxpacket, int sendsleep, struct sockaddr_in* listen_addr)
   {
-    m_has_output = m_has_input = true;
+    m_has_input = true;
+    m_has_output = false;
+    
+    //    m_has_output = m_has_input = true;
     memset(&m_sendaddr, 0, sizeof(m_sendaddr));
     m_maxpacketsz = maxpacket > 0 ? maxpacket : 1024;
     m_sendsleep = sendsleep >= 0 ? sendsleep : 10;
@@ -236,22 +241,31 @@ public:
 
 WDL_PtrList<IODevice > g_devices;
 
-class OSCInterface
+class OSCReciever;
+
+class OSCInterface : public ITimerCallback
 {
 public:
-  OSCInterface()
+  OSCInterface(int updateRateMs = 100)
   {
     JNL::open_socketlib();
+    
+    if(mTimer == nullptr)
+      mTimer = Timer::Create(*this, updateRateMs);
   }
   
-  static void messageCallback(void *d1, void *d2, char type, int msglen, void *msg)
+  virtual ~OSCInterface()
   {
-    printf("got something\n");
+    if(mTimer != nullptr)
+      delete mTimer;
+    
+    mTimer = nullptr;
   }
   
-  void CreateReciever(WDL_String& results)
+  static void MessageCallback(void *d1, int dev_idx, char type, int msglen, void *msg);
+  
+  void CreateReciever(WDL_String& results, OSCReciever* pReceiver, int port = 8000)
   {
-    int port = 8000;
     const char buf[] = "127.0.0.1";
     
     struct sockaddr_in addr;
@@ -264,7 +278,7 @@ public:
     int x;
     bool is_reuse = false;
     OSCDevice *r = nullptr;
-    for (x=0; x < g_devices.GetSize(); x++)
+    for (x = 0; x < g_devices.GetSize(); x++)
     {
       IODevice  *dev = g_devices.Get(x);
       if (dev && !strcmp(dev->get_type(),"OSC") && dev->m_has_input)
@@ -281,13 +295,14 @@ public:
         }
       }
     }
+    
     if (!r)
     {
-      r = new OSCDevice(nullptr,0,-1,&addr);
+      r = new OSCDevice(nullptr, 0, -1, &addr);
       if (r->m_sendsock == INVALID_SOCKET)
       {
         delete r;
-        r=nullptr;
+        r = nullptr;
         results.AppendFormatted(1024,"\tError listening for '%s:%i'\r\n", buf, port);
       }
       else
@@ -295,27 +310,30 @@ public:
         results.AppendFormatted(1024,"\tListening on '%s:%i'\r\n", buf, port);
       }
     }
+    
     if (r)
     {
-      r->addinst(messageCallback,this, 0 /*TODO WHAT WITH*/);
-      
-      if (!is_reuse) g_devices.Add(r);
+      r->addinst(MessageCallback, this, m_devs.GetSize());
+      m_devs.Add(r);
+
+      if (!is_reuse)
+        g_devices.Add(r);
     }
   }
   
-  void CreateSender(WDL_String& results)
+  void CreateSender(WDL_String& results, const char* ip = "127.0.0.1", int port = 8000)
   {
-    const char dp[] = "127.0.0.1:8000";
+    WDL_String dp;
+    dp.SetFormatted(256, "%s:%i", ip, port);
     OSCDevice *r = nullptr;
-    int x;
     bool is_reuse = false;
-    for (x=0; x<g_devices.GetSize(); x++)
+    for (auto x=0; x<g_devices.GetSize(); x++)
     {
-      IODevice  *d = g_devices.Get(x);
+      IODevice *d = g_devices.Get(x);
       if (d && !strcmp(d->get_type(),"OSC") && d->m_has_output)
       {
         OSCDevice *p = (OSCDevice *)d;
-        if (!strcmp(p->m_dest.Get(), dp))
+        if (!strcmp(p->m_dest.Get(), dp.Get()))
         {
           is_reuse = true;
           r = p; // reuse!
@@ -327,113 +345,192 @@ public:
     if (!r)
     {
       is_reuse = false;
-      r = new OSCDevice(dp, 0, -1, nullptr);
+      r = new OSCDevice(dp.Get(), 0, -1, nullptr);
       if (r->m_sendsock == INVALID_SOCKET)
       {
-        results.AppendFormatted(1024,"\tWarning: failed creating destination for @output '%s' OSC '%s'\r\n",dp, dp);
+        results.AppendFormatted(1024,"\tWarning: failed creating destination for output '%s' OSC '%s'\r\n", dp.Get(), dp.Get());
         delete r;
-        r=nullptr;
+        r = nullptr;
       }
     }
     
     if (r)
     {
-      r->addinst(messageCallback,this, 0 /*TODO*/);
-
-      if (!is_reuse) g_devices.Add(r);
+      r->addinst(MessageCallback, this, m_devs.GetSize());
+      m_devs.Add(r);
+      
+      if (!is_reuse)
+        g_devices.Add(r);
     }
   }
   
 public:
   void SendMsg(const char* msg, int len)
   {
-    g_devices.Get(0)->oscSend(msg, len);
-  }
-};
-
-class IOSCHandler : public ITimerCallback
-{
-public:
-  IOSCHandler(int updateRateMs = 100)
-  {
-    if(mTimer != nullptr)
-      mTimer = Timer::Create(*this, updateRateMs);
-  }
-
-  virtual ~IOSCHandler()
-  {
-    if(mTimer != nullptr)
-      delete mTimer;
-    
-    mTimer = nullptr;
+    g_devices.Get(0)->oscSend(msg, len); // TODO: device 0?
   }
   
-  virtual void ProcessInput() {};
-  virtual void ProcessOutput() {};
-
+  struct incomingEvent
+  {
+    IODevice* dev_ptr;
+    int sz; // size of msg[], 1..3 for midi, anything for OSC
+    char type; // 0=midi, 1=OSC
+    unsigned char msg[3];
+  };
+  
+  virtual void OnOSCMessage(OscMessageRead& msg) {};
+  
+private:
   void OnTimer(Timer& timer)
   {
-    ProcessInput();
-    ProcessOutput();
+    if(mInputProc)
+      mInputProc();
+    
+    if (m_incoming_events.GetSize())
+    {
+      static WDL_HeapBuf tmp;
+      
+      m_incoming_events_mutex.Enter();
+      tmp.CopyFrom(&m_incoming_events,false);
+      m_incoming_events.Resize(0,false);
+      m_incoming_events_mutex.Leave();
+      
+      int pos=0;
+      const int endpos = tmp.GetSize();
+      while (pos < endpos+1 - sizeof(incomingEvent))
+      {
+        incomingEvent *evt = (incomingEvent*) ((char *)tmp.Get()+pos);
+        
+        const int this_sz = ((sizeof(incomingEvent) + (evt->sz-3)) + 7) & ~7;
+        
+        if (pos+this_sz > endpos) break;
+        pos += this_sz;
+        
+        int rd_pos = 0;
+        int rd_sz = evt->sz;
+        if (evt->sz > 20 && !strcmp((char*)evt->msg, "#bundle"))
+        {
+          rd_sz = *(int *)(evt->msg+16);
+          OSC_MAKEINTMEM4BE(&rd_sz);
+          rd_pos += 20;
+        }
+//        if (m_var_msgs[3]) m_var_msgs[3][0] = evt->dev_ptr ? *evt->dev_ptr : -1.0;
+        
+        while (rd_pos + rd_sz <= evt->sz && rd_sz>=0)
+        {
+          OscMessageRead rmsg((char*)evt->msg + rd_pos, rd_sz);
+          
+          const char *mstr = rmsg.GetMessage();
+          if (mstr && *mstr)
+          {
+            OnOSCMessage(rmsg);
+          }
+          
+          rd_pos += rd_sz+4;
+          if (rd_pos >= evt->sz) break;
+          
+          rd_sz = *(int *)(evt->msg+rd_pos-4);
+          OSC_MAKEINTMEM4BE(&rd_sz);
+        }
   }
-
-protected:
-  OSCInterface mOSCInterface;
-  static Timer* mTimer; // TODO: probably should be static
-  WDL_FastString results;
-};
-
-Timer* IOSCHandler::mTimer = nullptr;
-
-class OSCSender : public IOSCHandler
-{
-public:
-  OSCSender()
-  {
-    WDL_String str;
-    mOSCInterface.CreateSender(str);
-    DBGMSG("%s\n", str.Get());
+    }
+    
+    if(mOutputProc)
+      mOutputProc();
   }
   
-  virtual void ProcessOutput()
+  // these are non-owned refs
+  WDL_PtrList<IODevice> m_devs;
+protected:
+  static Timer* mTimer;
+  WDL_FastString results;
+  std::function<void()> mInputProc = nullptr;
+  std::function<void()> mOutputProc = nullptr;
+  WDL_HeapBuf m_incoming_events;  // incomingEvent list, each is 8-byte aligned
+  WDL_Mutex m_incoming_events_mutex;
+  static const int DEVICE_INDEX_BASE = 0x400000;
+};
+
+Timer* OSCInterface::mTimer = nullptr;
+
+class OSCSender : public OSCInterface
+{
+public:
+  OSCSender(const char* destIP = "127.0.0.1", int port = 8000)
   {
-    for (auto x = 0; x < g_devices.GetSize(); x++)
-      g_devices.Get(x)->run_output(results);  // send queued messages
+    WDL_String str;
+    CreateSender(str, destIP, port);
+    DBGMSG("%s\n", str.Get());
+    mOutputProc = [&]()
+    {
+      for (auto x = 0; x < g_devices.GetSize(); x++)
+        g_devices.Get(x)->run_output(results);  // send queued messages
+    };
   }
   
   void SendOSCMessage(OscMessageWrite& msg)
   {
     int msgLength;
     const char* msgStr = msg.GetBuffer(&msgLength);
-    mOSCInterface.SendMsg(msgStr, msgLength);
+    SendMsg(msgStr, msgLength);
   }
 };
 
-class OSCReciever : public IOSCHandler
+class OSCReciever : public OSCInterface
 {
 public:
-  OSCReciever()
+  OSCReciever(int port = 8000)
   {
     WDL_String str;
-    mOSCInterface.CreateReciever(str);
+    CreateReciever(str, this, port);
     DBGMSG("%s\n", str.Get());
-  }
-  
-  void ProcessInput() override
-  {
-    const int sizeOfData = results.GetLength();
     
-    for (auto x = 0; x < g_devices.GetSize(); x++)
-      g_devices.Get(x)->run_input(results);
-    
-    if (results.GetLength() != sizeOfData) // if some input device added results
+    mInputProc = [&]()
     {
-      OscMessageRead msg{mReadBuf, sizeOfData};
-      OnOSCMessage(msg);
-    }
+//      const int sizeOfData = results.GetLength();
+      
+      for (auto x = 0; x < g_devices.GetSize(); x++)
+        g_devices.Get(x)->run_input(results);
+      
+//      if (results.GetLength() != sizeOfData) // if some input device added results
+//      {
+//        OscMessageRead msg{mReadBuf, sizeOfData};
+//        OnOSCMessage(msg);
+//      }
+    };
   }
   
   virtual void OnOSCMessage(OscMessageRead& msg) = 0;
+
 private:
-  char mReadBuf[MAX_OSC_MSG_LEN];
+  const char* mTest = "TEST";
+  char mReadBuf[MAX_OSC_MSG_LEN] = {};
 };
+
+//static
+void OSCInterface::MessageCallback(void *d1, int dev_idx, char type, int len, void *msg)
+{
+  OSCInterface* _this  = (OSCInterface *) d1;
+
+  if (_this && msg)
+  {
+    if (_this->m_incoming_events.GetSize() < 65536*8)
+    {
+      const int this_sz = ((sizeof(incomingEvent) + (len-3)) + 7) & ~7;
+      
+      _this->m_incoming_events_mutex.Enter();
+      const int oldsz = _this->m_incoming_events.GetSize();
+      _this->m_incoming_events.Resize(oldsz + this_sz,false);
+      
+      if (_this->m_incoming_events.GetSize() == oldsz+this_sz)
+      {
+        incomingEvent *item=(incomingEvent *) ((char *)_this->m_incoming_events.Get() + oldsz);
+        item->dev_ptr = _this->m_devs.Get(dev_idx);
+        item->sz = len;
+        item->type = type;
+        memcpy(item->msg,msg,len);
+      }
+      _this->m_incoming_events_mutex.Leave();
+    }
+  }
+}
