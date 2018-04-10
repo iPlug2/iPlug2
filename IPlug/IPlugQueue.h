5df503b8cf773c61181a62bd0cf54fda2162c2a1
Oli Larkin <olilarkin@googlemail.com>
2018-04-10 14:54:28 +0100

    add IPlugQueue.h

---
+++ IPlug/IPlugQueue.h
@@ -0,0 +1,84 @@
+//
+// IPlugQueue based on MLQueue.h
+//
+//
+// A very simple SPSC Queue.
+// based on https://kjellkod.wordpress.com/2012/11/28/c-debt-paid-in-full-wait-free-lock-free-IPlugQueue/
+//
+
+#pragma once
+
+#include <atomic>
+#include <cstddef>
+
+template<typename T>
+class IPlugQueue final
+{
+public:
+  IPlugQueue(int size)
+  {
+    mData.Resize(size + 1);
+  }
+
+  ~IPlugQueue(){}
+
+  bool Push(const T& item)
+  {
+    const auto currentWriteIndex = mWriteIndex.load(std::memory_order_relaxed);
+    const auto nextWriteIndex = Increment(currentWriteIndex);
+    if(nextWriteIndex != mReadIndex.load(std::memory_order_acquire))
+    {
+      mData.Get()[currentWriteIndex] = item;
+      mWriteIndex.store(nextWriteIndex, std::memory_order_release);
+      return true;
+    }
+    return false;
+  }
+
+  bool Pop(T& item)
+  {
+    const auto currentReadIndex = mReadIndex.load(std::memory_order_relaxed);
+    if(currentReadIndex == mWriteIndex.load(std::memory_order_acquire))
+    {
+      return false; // empty the queue
+    }
+    item = mData.Get()[currentReadIndex];
+    mReadIndex.store(Increment(currentReadIndex), std::memory_order_release);
+    return true;
+  }
+
+  size_t ElementsAvailable() const
+  {
+    return (mWriteIndex.load(std::memory_order_acquire) - mReadIndex.load(std::memory_order_relaxed))%mData.GetSize();
+  }
+
+  // useful for reading elements while a criteria is met. Can be used like
+  // while IPlugQueue.ElementsAvailable() && q.peek().mTime < 100 { elem = q.pop() ... }
+  const T& Peek()
+  {
+    const auto currentReadIndex = mReadIndex.load(std::memory_order_relaxed);
+    return mData[currentReadIndex];
+  }
+
+  bool WasEmpty() const
+  {
+    return (mWriteIndex.load() == mReadIndex.load());
+  }
+
+  bool WasFull() const
+  {
+    const auto nextWriteIndex = increment(mWriteIndex.load());
+    return (nextWriteIndex == mReadIndex.load());
+  }
+
+private:
+  size_t Increment(size_t idx) const
+  {
+    return (idx + 1) % (mData.GetSize());
+  }
+
+  WDL_TypedBuf<T> mData;
+  std::atomic<size_t> mWriteIndex{0};
+  std::atomic<size_t> mReadIndex{0};
+};
+
