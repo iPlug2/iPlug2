9703451349dba8de2aa851cd4868e0cf56c6d5c3
Oli Larkin <olilarkin@googlemail.com>
2018-05-05 20:34:22 +0100

    move au GetAPIBusTypeForChannelIOConfig into separate header

--- IPlug/AUv2/IPlugAU.cpp
+++ IPlug/AUv2/IPlugAU.cpp
@@ -1,69 +1,8 @@
 #include <algorithm>
 
-#include "AUv2/dfx-au-utilities.h"
-
+#include "dfx-au-utilities.h"
 #include "IPlugAU.h"
-
-#ifndef CUSTOM_BUSTYPE_FUNC
-static uint64_t GetAPIBusTypeForChannelIOConfig(int configIdx, ERoute dir, int busIdx, IOConfig* pConfig, WDL_TypedBuf<uint64_t>* APIBusTypes = nullptr)
-{
-  assert(pConfig != nullptr);
-  assert(busIdx >= 0 && busIdx < pConfig->NBuses(dir));
-  
-  int numChans = pConfig->GetBusInfo(dir, busIdx)->mNChans;
-  
-  switch (numChans)
-  {
-    case 0: APIBusTypes->Add(kAudioChannelLayoutTag_UseChannelDescriptions | 0);
-      return kAudioChannelLayoutTag_UseChannelDescriptions | 0;
-    case 1:
-      APIBusTypes->Add(kAudioChannelLayoutTag_Mono);
-      break;
-    case 2:
-      APIBusTypes->Add(kAudioChannelLayoutTag_Binaural);
-      APIBusTypes->Add(kAudioChannelLayoutTag_Stereo);
-      break;
-    case 3:
-      APIBusTypes->Add(kAudioChannelLayoutTag_ITU_3_0 | 3);
-      break;
-    case 4:
-      // by default support any 4 channel layout
-      APIBusTypes->Add(kAudioChannelLayoutTag_AudioUnit_4);
-      APIBusTypes->Add(kAudioChannelLayoutTag_Ambisonic_B_Format);
-      APIBusTypes->Add(kAudioChannelLayoutTag_HOA_ACN_SN3D | 4);
-      break;
-    case 5:
-      APIBusTypes->Add(kAudioChannelLayoutTag_AudioUnit_5_0);
-      break;
-    case 6:
-      // by default support any 6 channel layout
-      APIBusTypes->Add(kAudioChannelLayoutTag_AudioUnit_5_1);
-      APIBusTypes->Add(kAudioChannelLayoutTag_AudioUnit_6_0);
-      break;
-    case 7:
-      // by default support any 7 channel layout
-      APIBusTypes->Add(kAudioChannelLayoutTag_AudioUnit_6_1);
-      APIBusTypes->Add(kAudioChannelLayoutTag_AudioUnit_7_0);
-      APIBusTypes->Add(kAudioChannelLayoutTag_AudioUnit_7_1_Front);
-      break;
-    case 8:
-      // by default support any 8 channel layout
-      APIBusTypes->Add(kAudioChannelLayoutTag_AudioUnit_7_1);
-      APIBusTypes->Add(kAudioChannelLayoutTag_AudioUnit_7_1_Front);
-      break;
-    case 9:
-    case 16: // 2nd and 3rd order ambisonics
-      APIBusTypes->Add(kAudioChannelLayoutTag_HOA_ACN_SN3D | numChans);
-      break;
-    default:
-      APIBusTypes->Add(kAudioChannelLayoutTag_DiscreteInOrder | numChans);
-      break;
-  }
-  return true;
-}
-#else
-extern uint64_t GetAPIBusTypeForChannelIOConfig(int configIdx, ERoutingDir dir, int busIdx, IOConfig* pConfig, WDL_TypedBuf<uint64_t>* APIBusTypes = nullptr);
-#endif //CUSTOM_BUSTYPE_FUNC
+#include "IPlugAU_ioconfig.h"
 
 inline CFStringRef MakeCFString(const char* cStr)
 {
