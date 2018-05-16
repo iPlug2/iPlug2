bcaff1954b036ab1a80401db0786a575336dc811
Oli Larkin <olilarkin@googlemail.com>
2018-05-16 18:43:24 +0200

    conf

--- Examples/IPlugEffect/scripts/prepare_resources-ios.py
+++ Examples/IPlugEffect/scripts/prepare_resources-ios.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# this script will update info plist files based on config.h and copy resources to the ~/Music/PLUG_NAME folder
+# this script will update info plist files based on config.h
 
 kAudioUnitType_MusicDevice      = "aumu"
 kAudioUnitType_MusicEffect      = "aumf"
