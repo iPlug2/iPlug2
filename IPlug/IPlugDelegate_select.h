fcaae37b822a5713676717cf03a93c874f2fe6d3
Oli Larkin <olilarkin@googlemail.com>
2018-05-28 01:16:07 +0100

    dlg select

--- IPlug/IPlugDelegate_select.h
+++ IPlug/IPlugDelegate_select.h
@@ -1,4 +1,7 @@
-#ifdef NO_IGRAPHICS
+#if defined UIKIT_EDITOR_DELEGATE
+#include "UIKitEditorDelegate.h"
+typedef UIKitEditorDelegate EDITOR_DELEGATE_CLASS;
+#elif defined NO_IGRAPHICS
   #include "IPlugEditorDelegate.h"
   typedef IEditorDelegate EDITOR_DELEGATE_CLASS;
 #else
