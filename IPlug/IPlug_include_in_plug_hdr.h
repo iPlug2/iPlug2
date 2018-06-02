ac6ad6b5cf91129aab53ce93cab835eb2b762458
Oli Larkin <olilarkin@googlemail.com>
2018-06-02 13:40:29 +0100

    IPlugReaperVST2

--- IPlug/IPlug_include_in_plug_hdr.h
+++ IPlug/IPlug_include_in_plug_hdr.h
@@ -31,8 +31,8 @@
 #ifdef VST2_API
 #ifdef REAPER_PLUGIN
   #define LICE_PROVIDED_BY_APP
-  #include "IPlugReaper.h"
-  typedef IPlugReaper IPlug;
+  #include "IPlugReaperVST2.h"
+  typedef IPlugReaperVST2 IPlug;
 #else
   #include "IPlugVST2.h"
   typedef IPlugVST2 IPlug;
