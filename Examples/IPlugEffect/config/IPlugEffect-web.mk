af4d8b1aa3bded8f6e2bc1081ded1370434f7bec
Oli Larkin <olilarkin@googlemail.com>
2018-06-03 20:44:17 +0100

    add MidiSynth.cpp

--- Examples/IPlugEffect/config/IPlugEffect-web.mk
+++ Examples/IPlugEffect/config/IPlugEffect-web.mk
@@ -13,6 +13,7 @@ IPLUG_SRC = $(IPLUG_PATH)/IPlugAPIBase.cpp \
 	$(IPLUG_PATH)/IPlugParameter.cpp \
 	$(IPLUG_PATH)/IPlugPluginBase.cpp \
 	$(IPLUG_PATH)/IPlugPaths.cpp \
+	$(IPLUG_EXTRAS_PATH)/MidiSynth.cpp \
 	# $(IPLUG_PATH)/IPlugTimer.cpp
 
 IGRAPHICS_SRC = $(IGRAPHICS_PATH)/IGraphics.cpp \
