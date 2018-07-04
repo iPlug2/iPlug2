b6afae2a2b1929a179cc0e5fdfbaaa0c4def1c74
Oli Larkin <olilarkin@googlemail.com>
2018-07-04 11:07:41 +0100

    Add Reaper Extension Example

---
+++ Examples/ReaperExtension/resources/resource.h
@@ -0,0 +1,39 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by res.rc
+//
+#define IDD_CFG                         101
+#define IDD_WAVEITEMINFO                120
+#define IDD_FLACITEMINFO                120
+#define IDD_FLACSINK_CFG                155
+#define IDD_SCAST_CFG                   155
+#define IDC_BUTTON1                     1000
+#define IDC_EDIT1                       1001
+#define IDC_BPS                         1002
+#define IDC_QUAL                        1003
+#define IDC_SRATE                       1015
+#define IDC_HOST                        1016
+#define IDC_PASS                        1017
+#define IDC_DESC                        1018
+#define IDC_GENRE                       1019
+#define IDC_PUB                         1020
+#define IDC_URL                         1021
+#define IDC_STATUS                      1022
+#define IDC_DISCONNECT                  1023
+#define IDC_NCH                         1024
+#define IDC_BITRATE                     1025
+#define IDC_TITLE                       1026
+#define IDC_NAME                        1037
+#define IDC_INFO                        1038
+#define IDC_VOL                         1077
+
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
