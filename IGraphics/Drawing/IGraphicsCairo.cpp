f39c5a16db2e26d8f2733a0dc55772c09f165e64
Alex Harker <ajharker@gmail.com>
2018-08-26 23:52:23 +0100

    Correct vertical alignment of text when it is at the bottom

--- IGraphics/Drawing/IGraphicsCairo.cpp
+++ IGraphics/Drawing/IGraphicsCairo.cpp
@@ -391,7 +391,7 @@ bool IGraphicsCairo::DrawText(const IText& text, const char* str, IRECT& bounds,
   {
     case IText::EVAlign::kVAlignTop: y = bounds.T + fontExtents.ascent; break;
     case IText::EVAlign::kVAlignMiddle: y = bounds.MH() + (fontExtents.ascent/2.); break;
-    case IText::EVAlign::kVAlignBottom: y = bounds.B - fontExtents.ascent; break;
+    case IText::EVAlign::kVAlignBottom: y = bounds.B - fontExtents.descent; break;
     default: break;
   }
   
