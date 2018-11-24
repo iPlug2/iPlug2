d2e8f1f41fb30aa1dd9a40c735bf51e24b797fd3
Oli Larkin <olilarkin@googlemail.com>
2018-11-24 00:44:25 +0000

    beginnings of IGraphicsSkia

---
+++ IGraphics/Drawing/IGraphicsSkia.cpp
@@ -0,0 +1,166 @@
+#include <cmath>
+
+#include "IGraphicsSkia.h"
+
+SkiaBitmap::SkiaBitmap(const char* path, double sourceScale)
+{
+
+//  SetBitmap(idx, w, h, sourceScale);
+}
+
+SkiaBitmap::~SkiaBitmap()
+{
+}
+
+#pragma mark -
+
+// Utility conversions
+
+inline SkColor SkiaColor(const IColor& color, const IBlend* pBlend = 0)
+{
+  return SkColorSetARGB(color.A, color.R, color.G, color.B);
+}
+
+//inline SkiaBlendMode(const IBlend* pBlend)
+//{
+//}
+
+//NVGpaint SkiaPaint(NVGcontext* context, const IPattern& pattern, const IBlend* pBlend)
+//{
+//  NVGcolor icol = SkiaColor(pattern.GetStop(0).mColor, pBlend);
+//  NVGcolor ocol = SkiaColor(pattern.GetStop(pattern.NStops() - 1).mColor, pBlend);
+//
+//  // Invert transform
+//
+//  float inverse[6];
+//  nvgTransformInverse(inverse, pattern.mTransform);
+//  float s[2];
+//
+//  nvgTransformPoint(&s[0], &s[1], inverse, 0, 0);
+//
+//  if (pattern.mType == kRadialPattern)
+//  {
+//    return nvgRadialGradient(context, s[0], s[1], 0.0, inverse[0], icol, ocol);
+//  }
+//  else
+//  {
+//    float e[2];
+//    nvgTransformPoint(&e[0], &e[1], inverse, 1, 0);
+//
+//    return nvgLinearGradient(context, s[0], s[1], e[0], e[1], icol, ocol);
+//  }
+//}
+
+#pragma mark -
+
+IGraphicsSkia::IGraphicsSkia(IGEditorDelegate& dlg, int w, int h, int fps, float scale)
+: IGraphicsPathBase(dlg, w, h, fps, scale)
+{
+  DBGMSG("IGraphics Skia @ %i FPS\n", fps);
+}
+
+IGraphicsSkia::~IGraphicsSkia()
+{
+}
+
+IBitmap IGraphicsSkia::LoadBitmap(const char* name, int nStates, bool framesAreHorizontal)
+{
+  const int targetScale = round(GetDisplayScale());
+  
+  APIBitmap* pAPIBitmap = mBitmapCache.Find(name, targetScale);
+  
+  // If the bitmap is not already cached at the targetScale
+  if (!pAPIBitmap)
+  {
+    WDL_String fullPath;
+    int sourceScale = 0;
+    bool resourceFound = SearchImageResource(name, "png", fullPath, targetScale, sourceScale);
+    assert(resourceFound);
+    
+    pAPIBitmap = LoadAPIBitmap(fullPath, sourceScale);
+    
+    mBitmapCache.Add(pAPIBitmap, name, sourceScale);
+
+    assert(pAPIBitmap);
+  }
+  
+  return IBitmap(pAPIBitmap, nStates, framesAreHorizontal, name);
+}
+
+APIBitmap* IGraphicsSkia::LoadAPIBitmap(const WDL_String& resourcePath, int scale)
+{
+  return new SkiaBitmap(mVG, resourcePath.Get(), scale, GetPlatformInstance());
+}
+
+void IGraphicsSkia::SetPlatformContext(void* pContext)
+{
+  mPlatformContext = pContext;
+}
+
+void IGraphicsSkia::OnViewInitialized(void* pContext)
+{
+}
+
+void IGraphicsSkia::OnViewDestroyed()
+{
+}
+
+void IGraphicsSkia::DrawResize()
+{
+//  if(mSurface != nullptr);
+  
+  mSurface = SkSurface::MakeRasterN32Premul(WindowWidth() * GetDisplayScale(), WindowHeight() * GetDisplayScale());
+}
+
+//void IGraphicsSkia::BeginFrame()
+//{
+//}
+
+void IGraphicsSkia::EndFrame()
+{
+}
+
+void IGraphicsSkia::DrawBitmap(IBitmap& bitmap, const IRECT& dest, int srcX, int srcY, const IBlend* pBlend)
+{
+}
+
+IColor IGraphicsSkia::GetPoint(int x, int y)
+{
+  return COLOR_BLACK; //TODO:
+}
+
+bool IGraphicsSkia::DrawText(const IText& text, const char* str, IRECT& bounds, const IBlend* pBlend, bool measure)
+{
+}
+
+bool IGraphicsSkia::MeasureText(const IText& text, const char* str, IRECT& bounds)
+{
+  return DrawText(text, str, bounds, 0, true);
+}
+
+void IGraphicsSkia::PathStroke(const IPattern& pattern, float thickness, const IStrokeOptions& options, const IBlend* pBlend)
+{
+  SkPaint p;
+  p.setAntiAlias(true);
+  mSurface->drawPath(path, paint);
+}
+
+void IGraphicsSkia::PathFill(const IPattern& pattern, const IFillOptions& options, const IBlend* pBlend)
+{
+}
+
+void IGraphicsSkia::LoadFont(const char* name)
+{
+}
+
+void IGraphicsSkia::DrawBoxShadow(const IRECT& bounds, float cr, float ydrop, float pad, const IBlend* pBlend)
+{
+}
+
+void IGraphicsSkia::PathTransformSetMatrix(const IMatrix& m)
+{
+}
+
+void IGraphicsSkia::SetClipRegion(const IRECT& r)
+{
+}
---
+++ IGraphics/Drawing/IGraphicsSkia.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "IPlugPlatform.h"
+#include "IGraphicsPathBase.h"
+
+#include "SkSurface.h"
+#include "SkPath.h"
+#include "SkCanvas.h"
+#include "SkBitmap.h"
+
+class SkiaBitmap : public APIBitmap
+{
+public:
+  SkiaBitmap(const char* path, double sourceScale);
+  virtual ~SkiaBitmap();
+private:
+  SkBitmap mBitmap;
+};
+
+/** IGraphics draw class using Skia
+*   @ingroup DrawClasses */
+class IGraphicsSkia : public IGraphicsPathBase
+{
+public:
+  const char* GetDrawingAPIStr() override { return "SKIA"; }
+
+  IGraphicsSkia(IGEditorDelegate& dlg, int w, int h, int fps, float scale);
+  ~IGraphicsSkia();
+
+  void BeginFrame() override;
+  void EndFrame() override;
+  void OnViewInitialized(void* pContext) override;
+  void OnViewDestroyed() override;
+  void DrawResize() override;
+
+  void DrawBitmap(IBitmap& bitmap, const IRECT& dest, int srcX, int srcY, const IBlend* pBlend) override;
+
+  void PathClear() override { mMainPath.reset(); }
+  void PathClose() override { mMainPath.close(); }
+
+  void PathArc(float cx, float cy, float r, float aMin, float aMax) override { //TODO;}
+
+  void PathMoveTo(float x, float y) override { mMainPath.moveTo(x, y); }
+  void PathLineTo(float x, float y) override { mMainPath.lineTo(x, y); }
+  void PathCurveTo(float x1, float y1, float x2, float y2, float x3, float y3) override { //TODO; }
+    
+  void PathStroke(const IPattern& pattern, float thickness, const IStrokeOptions& options, const IBlend* pBlend) override;
+  void PathFill(const IPattern& pattern, const IFillOptions& options, const IBlend* pBlend) override;
+  
+  IColor GetPoint(int x, int y) override;
+  void* GetDrawContext() override { return (void*) mSurface.getCanvas(); }
+
+  bool DrawText(const IText& text, const char* str, IRECT& bounds, const IBlend* pBlend, bool measure) override;
+  bool MeasureText(const IText& text, const char* str, IRECT& bounds) override;
+  
+  IBitmap LoadBitmap(const char* name, int nStates, bool framesAreHorizontal) override;
+  IBitmap ScaleBitmap(const IBitmap& bitmap, const char* name, int targetScale) override { return bitmap; } // NO-OP
+  void ReleaseBitmap(const IBitmap& bitmap) override { }; // NO-OP
+  void RetainBitmap(const IBitmap& bitmap, const char * cacheName) override { }; // NO-OP
+
+  void LoadFont(const char* name) override;
+  
+  void DrawBoxShadow(const IRECT& bounds, float cr, float ydrop, float pad, const IBlend* pBlend) override;
+  void SetPlatformContext(void* pContext) override;
+protected:
+  APIBitmap* LoadAPIBitmap(const WDL_String& resourcePath, int scale) override;
+  APIBitmap* ScaleAPIBitmap(const APIBitmap* pBitmap, int scale) override { return new APIBitmap(); } // NO-OP
+private:
+  void PathTransformSetMatrix(const IMatrix& m) override;
+  void SetClipRegion(const IRECT& r) override;
+  sk_sp<SkSurface> mSurface;
+  SkPath mMainPath;
+};
