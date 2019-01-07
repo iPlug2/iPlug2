2eb38f4cec71edafb385d5f0a5dc79fca1bd7ee1
Oli Larkin <olilarkin@googlemail.com>
2019-01-07 22:42:28 +0000

    begining app multichannel

--- IPlug/APP/IPlugAPP_host.h
+++ IPlug/APP/IPlugAPP_host.h
@@ -243,5 +243,8 @@ private:
   std::vector<std::string> mMidiInputDevNames;
   std::vector<std::string> mMidiOutputDevNames;
   
+  WDL_PtrList<double> mInputBufPtrs;
+  WDL_PtrList<double> mOutputBufPtrs;
+
   friend class IPlugAPP;
 };
