01be425c80cc7f396e7443f974e8d6ac4300c7e4
Oli Larkin <olilarkin@googlemail.com>
2019-01-15 14:36:07 +0000

    remove obsolete SWELL_NO_POSTMESSAGE code/decl

--- IPlug/IPlug_include_in_plug_src.h
+++ IPlug/IPlug_include_in_plug_src.h
@@ -402,21 +402,6 @@ extern "C"
   #error "No API defined!"
 #endif
 
-#if defined OS_MAC || defined OS_IOS
-#if defined SWELL_NO_POSTMESSAGE && !defined VST3P_API
-void Sleep(int ms)
-{
-  usleep(ms?ms*1000:100);
-}
-
-DWORD GetTickCount()
-{
-  struct timeval tm={0,};
-  gettimeofday(&tm,NULL);
-  return (DWORD) (tm.tv_sec*1000 + tm.tv_usec/1000);
-}
-#endif
-#endif
 /*
 #if defined _DEBUG
   #define PUBLIC_NAME APPEND_TIMESTAMP(PLUG_NAME " DEBUG")
