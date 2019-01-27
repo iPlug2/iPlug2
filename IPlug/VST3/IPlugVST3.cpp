ca50b32ea278f867e9841cae78f1cf01fc95aaed
Alex Harker <ajharker@gmail.com>
2019-01-27 13:49:47 +0000

    Make sure that OnUIOpen() is called consistently

--- IPlug/VST3/IPlugVST3.cpp
+++ IPlug/VST3/IPlugVST3.cpp
@@ -1003,16 +1003,18 @@ tresult PLUGIN_API IPlugVST3View::attached(void* parent, FIDString type)
 {
   if (mPlug->HasUI())
   {
+    void* pView = nullptr;
     #ifdef OS_WIN
     if (strcmp(type, kPlatformTypeHWND) == 0)
-      mPlug->OpenWindow(parent);
+      pView = mPlug->OpenWindow(parent);
     #elif defined OS_MAC
     if (strcmp (type, kPlatformTypeNSView) == 0)
-      mPlug->OpenWindow(parent);
+      pView = mPlug->OpenWindow(parent);
     else // Carbon
       return kResultFalse;
     #endif
-    mPlug->OnUIOpen();
+    if (pView)
+      mPlug->OnUIOpen();
 
     return kResultTrue;
   }
