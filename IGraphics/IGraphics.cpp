77d23fc3e5c7619533249fbd62a1bd6af585ab0e
Alex Harker <ajharker@gmail.com>
2019-04-05 20:02:10 +0100

    Fix windows compilation for NanoVG

--- IGraphics/IGraphics.cpp
+++ IGraphics/IGraphics.cpp
@@ -1159,7 +1159,7 @@ ISVG IGraphics::LoadSVG(const char* fileName, const char* units, float dpi)
   if(!pHolder)
   {
     WDL_String path;
-    EResourceLocation resourceFound = FindResource(fileName, "svg", path, GetBundleID());
+    EResourceLocation resourceFound = FindResource(fileName, "svg", path, GetBundleID(), GetWinModuleHandle());
 
     if (resourceFound == EResourceLocation::kNotFound)
       return ISVG(nullptr); // return invalid SVG
@@ -1312,7 +1312,7 @@ EResourceLocation IGraphics::SearchImageResource(const char* name, const char* t
       fullName.SetFormatted((int) (strlen(name) + strlen("@2x")), "%s@%dx%s", baseName.Get(), sourceScale, ext.Get());
     }
 
-    EResourceLocation found = FindResource(fullName.Get(), type, result, GetBundleID());
+    EResourceLocation found = FindResource(fullName.Get(), type, result, GetBundleID(), GetWinModuleHandle());
 
     if (found > EResourceLocation::kNotFound)
       return found;
