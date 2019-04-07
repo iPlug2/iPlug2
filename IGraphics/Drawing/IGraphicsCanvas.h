75d2ac0c43c403925ffe4baf13ec21adfd5885a4
Alex Harker <ajharker@gmail.com>
2019-04-07 17:45:41 +0100

    Fix method signature

--- IGraphics/Drawing/IGraphicsCanvas.h
+++ IGraphics/Drawing/IGraphicsCanvas.h
@@ -59,7 +59,7 @@ public:
   bool BitmapExtSupported(const char* ext) override;
 protected:
   APIBitmap* LoadAPIBitmap(const char* fileNameOrResID, int scale, EResourceLocation location, const char* ext) override;
-  APIBitmap* CreateAPIBitmap(int width, int height) override;
+  APIBitmap* CreateAPIBitmap(int width, int height, int scale, double drawScale) override;
 
   int AlphaChannel() const override { return 3; }
   bool FlippedBitmap() const override { return false; }
