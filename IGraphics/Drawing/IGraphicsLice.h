59ca0e84e674121a9f6806d1630b18c8ade41dc2
Alex Harker <ajharker@gmail.com>
2019-04-11 17:29:14 +0100

    Implement font loading on LICE

--- IGraphics/Drawing/IGraphicsLice.h
+++ IGraphics/Drawing/IGraphicsLice.h
@@ -128,7 +128,7 @@ protected:
   APIBitmap* LoadAPIBitmap(const char* fileNameOrResID, int scale, EResourceLocation location, const char* ext) override;
   APIBitmap* CreateAPIBitmap(int width, int height, int scale, double drawScale) override;
 
-  bool LoadAPIFont(const char* fontID, const PlatformFontPtr& font) override { return false; }
+  bool LoadAPIFont(const char* fontID, const PlatformFontPtr& font) override;
 
   int AlphaChannel() const override { return LICE_PIXEL_A; }
   bool FlippedBitmap() const override { return false; }
@@ -176,7 +176,7 @@ private:
     
   void UpdateLayer() override;
     
-  LICE_IFont* CacheFont(const IText& text, double scale);
+  LICE_IFont* CacheFont(const IText& text);
 
   IRECT mDrawRECT;
   IRECT mClipRECT;
