6b64346b40f2c1e4edf2690aec966c3adce5d943
Oli Larkin <olilarkin@googlemail.com>
2019-04-17 01:31:38 +0100

    update IPlugMidiEffect scripts for duplicate changes

--- Examples/IPlugMidiEffect/config/IPlugMidiEffect-web.mk
+++ Examples/IPlugMidiEffect/config/IPlugMidiEffect-web.mk
@@ -1,6 +1,7 @@
 # IPLUG2_ROOT should point to the top level IPLUG2 folder from the project folder
-# By default, that is two directories up from /Examples/IPlugEffect
+# By default, that is two directories up from /Examples/IPlugMidiEffect
 IPLUG2_ROOT = ./../../
+
 include ./../../common-web.mk
 
 SRC += IPlugMidiEffect.cpp
