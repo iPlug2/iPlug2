c9f582c40cd7b78bdb2e94ce3a7d460edd73ef0b
Oli Larkin <olilarkin@googlemail.com>
2019-04-23 13:16:48 +0100

    make sure MTL layer drawable size gets updated

--- IGraphics/Platforms/IGraphicsMac_view.mm
+++ IGraphics/Platforms/IGraphicsMac_view.mm
@@ -526,8 +526,17 @@ extern StaticStorage<CoreTextFontDescriptor> sFontDescriptorCache;
     [pWindow makeFirstResponder: self];
     [pWindow setAcceptsMouseMovedEvents: YES];
     
-    if (mGraphics && mGraphics->GetDrawContext())
-      mGraphics->SetScreenScale([pWindow backingScaleFactor]);
+    CGFloat newScale = [pWindow backingScaleFactor];
+
+    if (mGraphics)
+      mGraphics->SetScreenScale(newScale);
+    
+    #ifdef IGRAPHICS_METAL
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(frameDidChange:)
+                                                 name:NSViewFrameDidChangeNotification
+                                               object:self];
+    #endif
     
 //    [[NSNotificationCenter defaultCenter] addObserver:self
 //                                             selector:@selector(windowResized:) name:NSWindowDidEndLiveResizeNotification
@@ -555,8 +564,11 @@ extern StaticStorage<CoreTextFontDescriptor> sFontDescriptorCache;
   if (mGraphics->GetDrawContext() && newScale != mGraphics->GetScreenScale())
     mGraphics->SetScreenScale(newScale);
 
-#ifdef IGRAPHICS_GL
+#if defined IGRAPHICS_GL
   self.layer.contentsScale = 1./newScale;
+#else if defined IGRAPHICS_METAL
+  [(CAMetalLayer*)[self layer] setDrawableSize:CGSizeMake(self.frame.size.width * newScale,
+                                                          self.frame.size.height * newScale)];
 #endif
 }
 
@@ -1141,6 +1153,16 @@ static void MakeCursorFromName(NSCursor*& cursor, const char *name)
 
   return YES;
 }
+
+#ifdef IGRAPHICS_METAL
+- (void)frameDidChange:(NSNotification*)notification
+{
+  CGFloat scale = [[self window] backingScaleFactor];
+
+  [(CAMetalLayer*)[self layer] setDrawableSize:CGSizeMake(self.frame.size.width * scale,
+                                                          self.frame.size.height * scale)];
+}
+#endif
 
 //- (void)windowResized:(NSNotification *)notification;
 //{
