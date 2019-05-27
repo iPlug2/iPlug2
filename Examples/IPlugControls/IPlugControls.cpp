c76ff7cba5152b10d1056f4561387b90faccc404
Oli Larkin <olilarkin@googlemail.com>
2019-05-27 23:59:11 +0100

    don’t put IDirBrowseControlBase in web demo

--- Examples/IPlugControls/IPlugControls.cpp
+++ Examples/IPlugControls/IPlugControls.cpp
@@ -262,9 +262,12 @@ IPlugControls::IPlugControls(IPlugInstanceInfo instanceInfo)
     pGraphics->AttachControl(new IVScopeControl<2>(nextCell(), "IVScopeControl", style.WithColor(kFG, COLOR_BLACK)), kCtrlTagScope, "vcontrols");
     pGraphics->AttachControl(new IVCustomControl(nextCell(), "IVCustomControl", style), kNoTag, "vcontrols");
     
+#ifndef OS_WEB
     IRECT wideCell = nextCell().Union(nextCell());
     pGraphics->AttachControl(new ITextControl(wideCell.GetFromTop(20.f), "File Browser (IDirBrowseControlBase) demo", style.labelText));
     pGraphics->AttachControl(new FileBrowser(wideCell.GetReducedFromTop(20.f)));
+#endif
+    
 //
 //    auto button2action = [](IControl* pCaller) {
 //      SplashClickActionFunc(pCaller);
