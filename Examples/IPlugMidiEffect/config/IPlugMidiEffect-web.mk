a48b6279107ac9e2e2e942ac57f0a94ae2b2b045
Oli Larkin <olilarkin@googlemail.com>
2019-07-18 13:27:08 +0100

    fix IPlugMidiEffect

--- Examples/IPlugMidiEffect/config/IPlugMidiEffect-web.mk
+++ Examples/IPlugMidiEffect/config/IPlugMidiEffect-web.mk
@@ -1,7 +1,6 @@
 # IPLUG2_ROOT should point to the top level IPLUG2 folder from the project folder
 # By default, that is two directories up from /Examples/IPlugMidiEffect
-IPLUG2_ROOT = ./../../
-
+IPLUG2_ROOT = ../../..
 include ../../../common-web.mk
 
 SRC += $(PROJECT_ROOT)/IPlugMidiEffect.cpp
