499301878a48fd2e601d70e4252f5b2650eaa167
Saverio Vigni <s.vigni@hor-net.com>
2019-08-09 14:09:09 +0200

    prevent negative number in mPPQPos, sometimes sent by PT when playback is stop

--- IPlug/AAX/IPlugAAX.cpp
+++ IPlug/AAX/IPlugAAX.cpp
@@ -308,6 +308,9 @@ void IPlugAAX::RenderAudio(AAX_SIPlugRenderInfo* pRenderInfo)
     
     mTransport->GetCurrentTickPosition(&ppqPos);
     timeInfo.mPPQPos = (double) ppqPos / 960000.0;
+    if(timeInfo.mPPQPos < 0) {
+      timeInfo.mPPQPos = 0;
+    }
     
     mTransport->GetCurrentNativeSampleLocation(&samplePos);
     timeInfo.mSamplePos = (double) samplePos;
