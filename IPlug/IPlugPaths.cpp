64bc807e09c824d35d516e71396c0e151e0d57a8
Oli Larkin <olilarkin@googlemail.com>
2019-08-15 13:17:20 +0100

    rework IPlugPaths.cpp namespace

--- IPlug/IPlugPaths.cpp
+++ IPlug/IPlugPaths.cpp
@@ -17,12 +17,18 @@
 #include "IPlugConstants.h"
 #include "IPlugPaths.h"
 
-#ifdef OS_WIN
+#if defined OS_WEB
+#include <emscripten/val.h>
+#elif defined OS_WIN
 #include <windows.h>
 #include <Shlobj.h>
 #include <Shlwapi.h>
+#endif
 
-using namespace iplug;
+BEGIN_IPLUG_NAMESPACE
+
+#if defined OS_WIN
+#pragma mark - OS_WIN
 
 // Unicode helpers
 void UTF8ToUTF16(wchar_t* utf16Str, const char* utf8Str, int maxLen)
@@ -225,6 +231,7 @@ const void* LoadWinResource(const char* resid, const char* type, int& sizeInByte
 }
 
 #elif defined OS_WEB
+#pragma mark - OS_WEB
 
 void AppSupportPath(WDL_String& path, bool isSystem)
 {
@@ -246,12 +253,7 @@ void VST3PresetsPath(WDL_String& path, const char* mfrName, const char* pluginNa
   path.Set("Presets");
 }
 
-#include <emscripten/val.h>
-
-using namespace iplug;
-using namespace emscripten;
-
-EResourceLocation iplug::LocateResource(const char* name, const char* type, WDL_String& result, const char*, void*)
+EResourceLocation LocateResource(const char* name, const char* type, WDL_String& result, const char*, void*)
 {
   if (CStringHasContents(name))
   {
@@ -263,7 +265,7 @@ EResourceLocation iplug::LocateResource(const char* name, const char* type, WDL_
     
     if(strcmp(type, "png") == 0) { //TODO: lowercase/uppercase png
       plusSlash.SetFormatted(strlen("/resources/img/") + strlen(name) + 1, "/resources/img/%s", name);
-      foundResource = val::global("Module")["preloadedImages"].call<bool>("hasOwnProperty", std::string(plusSlash.Get()));
+      foundResource = emscripten::val::global("Module")["preloadedImages"].call<bool>("hasOwnProperty", std::string(plusSlash.Get()));
     }
     else if(strcmp(type, "ttf") == 0) { //TODO: lowercase/uppercase ttf
       plusSlash.SetFormatted(strlen("/resources/fonts/") + strlen(name) + 1, "/resources/fonts/%s", name);
@@ -284,3 +286,5 @@ EResourceLocation iplug::LocateResource(const char* name, const char* type, WDL_
 }
 
 #endif
+
+END_IPLUG_NAMESPACE
