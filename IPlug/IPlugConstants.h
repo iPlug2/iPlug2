9d517afbc46b6414beea758399d602e4b7cbaa39
Oli Larkin <olilarkin@googlemail.com>
2019-08-16 23:50:11 +0100

    move vst3preset constants

--- IPlug/IPlugConstants.h
+++ IPlug/IPlugConstants.h
@@ -240,6 +240,35 @@ enum EResourceLocation
   kPreloadedTexture
 };
 
+// These constants come from vstpreset.cpp, allowing saving of VST3 format presets without including the VST3 SDK
+typedef char ChunkID[4];
+
+enum ChunkType
+{
+  kHeader,
+  kComponentState,
+  kControllerState,
+  kProgramData,
+  kMetaInfo,
+  kChunkList,
+  kNumPresetChunks
+};
+
+static const ChunkID commonChunks[kNumPresetChunks] = {
+  {'V', 'S', 'T', '3'},  // kHeader
+  {'C', 'o', 'm', 'p'},  // kComponentState
+  {'C', 'o', 'n', 't'},  // kControllerState
+  {'P', 'r', 'o', 'g'},  // kProgramData
+  {'I', 'n', 'f', 'o'},  // kMetaInfo
+  {'L', 'i', 's', 't'}   // kChunkList
+};
+
+// Preset Header: header id + version + class id + list offset
+static const int32_t kFormatVersion = 1;
+static const int32_t kClassIDSize = 32; // ASCII-encoded FUID
+static const int32_t kHeaderSize = sizeof (ChunkID) + sizeof (int32_t) + kClassIDSize + sizeof (int64_t);
+//static const int32_t kListOffsetPos = kHeaderSize - sizeof (int64_t);
+
 END_IPLUG_NAMESPACE
 
 /**@}*/
