bf064d1f2e1c10a908182c148baf9848d7578efb
Oli Larkin <olilarkin@googlemail.com>
2019-08-18 17:05:51 +0100

    move MNVGtexture / MNVGbuffers / MNVGcontext to objc pch

--- IGraphics/Drawing/IGraphicsNanoVG_src.m
+++ IGraphics/Drawing/IGraphicsNanoVG_src.m
@@ -1,8 +1,5 @@
 #if defined IGRAPHICS_NANOVG
   #if defined IGRAPHICS_METAL
-    #define MNVGtexture CONCAT(MNVGtexture_)
-    #define MNVGbuffers CONCAT(MNVGbuffers_)
-    #define MNVGcontext CONCAT(MNVGcontext_)
     #include "nanovg_mtl.m"
   #endif
   #if defined IGRAPHICS_FREETYPE
