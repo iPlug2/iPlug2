58ff61f455342c35e0f9dab08e85b5d0dfbc5004
Oli Larkin <olilarkin@googlemail.com>
2019-09-11 21:35:52 +0100

    resvg capi

--- Dependencies/IGraphics/build-resvg-mac.sh
+++ Dependencies/IGraphics/build-resvg-mac.sh
@@ -4,3 +4,8 @@ git clone https://github.com/RazrFalcon/resvg.git ../Build/src/resvg
 
 cd ../Build/src/resvg
 cargo build --release --features "cairo-backend"
+cd capi
+cargo build --release --features "cairo-backend"
+cd ..
+mv capi/include/resvg.h ../../mac/include
+mv target/release/libresvg.a ../../mac/lib
