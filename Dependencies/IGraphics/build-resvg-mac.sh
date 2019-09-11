5990952358fc6d9ac208b92827391287ec150e6a
Oli Larkin <olilarkin@googlemail.com>
2019-09-11 23:15:09 +0100

    temp brew

--- Dependencies/IGraphics/build-resvg-mac.sh
+++ Dependencies/IGraphics/build-resvg-mac.sh
@@ -8,7 +8,9 @@ cd capi
 mkdir .cargo
 echo [build] > .cargo/config
 echo rustflags = [\"-C\", \"link-args=-L../../../mac/lib/\"] >> .cargo/config
-cargo build --release --features "cairo-backend"
+brew install cairo harfbuzz
+cargo build --verbose --release --features "cairo-backend"
 cd ..
+sed -i.bak s/"#include <cairo.h>"/"#include <cairo/cairo.h>"/g capi/include/resvg.h
 mv capi/include/resvg.h ../../mac/include
 mv target/release/libresvg.a ../../mac/lib
