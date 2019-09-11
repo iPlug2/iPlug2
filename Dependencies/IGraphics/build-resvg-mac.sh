69d28e7341992ba26ab232e9b20552ae65d831d9
Oli Larkin <olilarkin@googlemail.com>
2019-09-11 22:11:00 +0100

    link flags for cairo

--- Dependencies/IGraphics/build-resvg-mac.sh
+++ Dependencies/IGraphics/build-resvg-mac.sh
@@ -5,6 +5,9 @@ git clone https://github.com/RazrFalcon/resvg.git ../Build/src/resvg
 cd ../Build/src/resvg
 cargo build --release --features "cairo-backend"
 cd capi
+mkdir .cargo
+echo [build] > .cargo/config
+echo rustflags = ["-C", "link-args=-L../../../mac/lib/"] >> .cargo/config
 cargo build --release --features "cairo-backend"
 cd ..
 mv capi/include/resvg.h ../../mac/include
