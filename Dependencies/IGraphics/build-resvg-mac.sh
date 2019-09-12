d136b61c673a8358dcbc488232a9f05d74a0761e
Oli Larkin <olilarkin@googlemail.com>
2019-09-12 09:59:25 +0100

    please build

--- Dependencies/IGraphics/build-resvg-mac.sh
+++ Dependencies/IGraphics/build-resvg-mac.sh
@@ -7,6 +7,7 @@ cargo build --release --features "cairo-backend"
 cd capi
 mkdir .cargo
 echo [build] > .cargo/config
+ls -la $(AGENT_BUILDDIRECTORY)/Dependencies/Build/mac/lib/
 echo rustflags = [\"-C\", \"link-args=-L$(AGENT_BUILDDIRECTORY)/Dependencies/Build/mac/lib/\"] >> .cargo/config
 brew install cairo harfbuzz
 cargo build --verbose --release --features "cairo-backend"
