d2f833246f98f92c901e7fdc0c16e239a1c47096
Oli Larkin <olilarkin@googlemail.com>
2019-09-12 15:10:12 +0100

    Improve skia build scripts

--- Dependencies/IGraphics/build-skia-mac.sh
+++ Dependencies/IGraphics/build-skia-mac.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-# comment if you allready have depot tools!
-#git clone 'https://chromium.googlesource.com/chromium/tools/depot_tools.git' ../../tmp/depot_tools
-#export PATH="${PWD}/../../tmp/depot_tools:${PATH}"
+DEPOT_TOOLS_PATH=../Build/tmp/depot_tools
+
+if [ ! -d $DEPOT_TOOLS_PATH ]; then
+  git clone 'https://chromium.googlesource.com/chromium/tools/depot_tools.git' $DEPOT_TOOLS_PATH
+  export PATH="${PWD}/$DEPOT_TOOLS_PATH:${PATH}"
+fi
 
 cd ../Build/src/skia
 python tools/git-sync-deps
@@ -10,18 +13,18 @@ python tools/git-sync-deps
 is_official_build = true
 skia_use_system_libjpeg_turbo = false
 skia_use_system_libpng = false
-skia_use_libwebp = false
 skia_use_system_zlib = false
+skia_use_libwebp = false
 skia_use_xps = false
 skia_use_dng_sdk = false
 skia_use_expat = false
 skia_use_metal = true
 skia_use_icu = false
-skia_use_sfntly=false
+skia_use_sfntly = false
 skia_enable_skottie = false
 skia_enable_pdf = false
 skia_enable_particles = true
-skia_enable_gpu=true
+skia_enable_gpu = true
 cc = "clang"
 cxx = "clang++"
 target_os = "mac"
