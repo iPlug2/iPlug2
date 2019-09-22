0aa724f6e90af62b1b4b8624119ef7831c5da991
Oli Larkin <olilarkin@googlemail.com>
2019-09-22 22:11:11 +0100

    build RESVG windows with cairo backend

--- Dependencies/IGraphics/build-resvg-win.bat
+++ Dependencies/IGraphics/build-resvg-win.bat
@@ -1,28 +1,40 @@
-echo off
-
-if exist "%ProgramFiles(x86)%" (goto 64-Bit) else (goto 32-Bit)
-
-:32-Bit
-echo 32-Bit O/S detected
-call "%ProgramFiles%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_x64
-goto END
-
-:64-Bit
-echo 64-Bit Host O/S detected
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_x64
-goto END
-:END
 
+set LIBPATH=%CD:\=\\%\\..\\Build\\win\\x64\\Debug
+
+cd ..\Build\src\resvg
+cargo build --features="cairo-backend"
+
+cd capi
+if exist .cargo rmdir /q /s .cargo
+mkdir .cargo
+echo [build] >> .cargo/config
+echo rustflags = ["-C", "link-arg=/LIBPATH:%LIBPATH%", "-C", "link-arg=pixman.lib", "-C", "link-arg=freetype.lib", "-C", "link-arg=zlib.lib", "-C", "link-arg=Msimg32.lib", "-C", "link-arg=gdi32.lib", "-C", "link-arg=User32.lib", "-C", "target-feature=+crt-static"] >> .cargo/config
+cargo build --features="cairo-backend"
+cd .. 
+
+copy capi\include\resvg.h ..\resvg.h 
+copy target\debug\resvg.lib ..\..\win\x64\Debug
+
+cd ..\..\..\IGraphics
+
+REM ---------------------------------------------------------
+
+set LIBPATH=%CD:\=\\%\\..\\Build\\win\\x64\\Release
 
 cd ..\Build\src\resvg
 cargo build --release --features="cairo-backend"
 
 cd capi
+if exist .cargo rmdir /q /s .cargo
 mkdir .cargo
 echo [build] >> .cargo/config
-echo rustflags = ["-C", "link-arg=/LIBPATH:%cd%\\..\\..\\..\\Build\\win\\x64\\Release", "-C", "link-arg=pixman.lib", "-C", "link-arg=freetype.lib", "-C", "link-arg=Msimg32.lib", "-C", "link-arg=gdi32.lib", "-C", "link-arg=User32.lib"] >> .cargo/config
+echo rustflags = ["-C", "link-arg=/LIBPATH:%LIBPATH%", "-C", "link-arg=pixman.lib", "-C", "link-arg=freetype.lib", "-C", "link-arg=zlib.lib", "-C", "link-arg=Msimg32.lib", "-C", "link-arg=gdi32.lib", "-C", "link-arg=User32.lib", "-C", "target-feature=+crt-static"] >> .cargo/config
 cargo build --release --features="cairo-backend"
 cd .. 
 
-copy capi\include\resvg.h ..\..\win\include\resvg.h 
-copy target\release\libresvg.rlib ..\..\win\x64\Release
+copy capi\include\resvg.h ..\resvg.h 
+copy target\release\resvg.lib ..\..\win\x64\Release
+
+cd ..\..\..\IGraphics
+
