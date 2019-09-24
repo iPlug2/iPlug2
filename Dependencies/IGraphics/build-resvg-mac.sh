39de57b89037a2b95965f96641312e53d87fe44c
Oli Larkin <olilarkin@googlemail.com>
2019-09-24 20:28:58 +0100

    remove hardcoded paths from resvg build script

--- Dependencies/IGraphics/build-resvg-mac.sh
+++ Dependencies/IGraphics/build-resvg-mac.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cd ../Build/src/resvg
-export SKIA_DIR=/Users/oli/Dev/MyPlugins/Dependencies/Build/src/skia
-export SKIA_LIB_DIR=/Users/oli/Dev/MyPlugins/Dependencies/Build/mac/lib
+export SKIA_DIR=$(PWD)/../skia
+export SKIA_LIB_DIR=$(PWD)/../../mac/lib
 export MACOSX_DEPLOYMENT_TARGET=10.9
 cargo build --release --features "skia-backend cairo-backend"
 
@@ -15,10 +15,9 @@ fi
 echo ---------------------------------
 mkdir .cargo
 echo [build] > .cargo/config
-echo rustflags = [\"-L/Users/oli/Dev/MyPlugins/Dependencies/Build/mac/lib\", \"-ldylib=pixman-1\", \"-lframework=Metal\", \"-lframework=Foundation\"] >> .cargo/config
+echo rustflags = [\"-L$(SKIA_LIB_DIR)\", \"-ldylib=pixman-1\", \"-lframework=Metal\", \"-lframework=Foundation\"] >> .cargo/config
 
 cargo build --verbose --release --features "skia-backend cairo-backend"
 cd ..
-sed -i.bak s/"#include <cairo.h>"/"#include <cairo\/cairo.h>"/g capi/include/resvg.h
-cp capi/include/resvg.h ../../mac/include/resvg.h 
+
 mv target/release/libresvg.a ../../mac/lib
