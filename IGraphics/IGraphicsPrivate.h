db03cbbda31323072f56da60c4893fedb069b9de
Alex Harker <ajharker@gmail.com>
2019-10-10 23:38:33 +0100

    Implement support for both mac and windows font string encodings on both platforms

--- IGraphics/IGraphicsPrivate.h
+++ IGraphics/IGraphicsPrivate.h
@@ -65,6 +65,7 @@
 
 #if defined OS_MAC || defined OS_IOS
   #include <CoreText/CoreText.h>
+  #include <CoreFoundation/CoreFoundation.h>
   #define FONT_DESCRIPTOR_TYPE CTFontDescriptorRef
 #elif defined OS_WIN
   #include "wingdi.h"
@@ -175,8 +176,8 @@ public:
       {
         mUnitsPerEM = GetUInt16(mHeadLocation + 18);
         mMacStyle = GetUInt16(mHeadLocation + 44);
-        mFamily = GetFontString(1);
-        mStyle = GetFontString(2);
+        mFamily = SearchFontString(1);
+        mStyle = SearchFontString(2);
         mAscender = GetSInt16(mHheaLocation + 4);
         mDescender = GetSInt16(mHheaLocation + 6);
         mLineGap = GetSInt16(mHheaLocation + 8);
@@ -206,6 +207,9 @@ public:
   int16_t GetLineHeight() const  { return (mAscender - mDescender) + mLineGap; }
   
 private:
+    
+  enum class EStringID { Mac, Windows };
+    
   bool MatchTag(uint32_t loc, const char* tag)
   {
     return mData[loc+0] == tag[0] && mData[loc+1] == tag[1] && mData[loc+2] == tag[2] && mData[loc+3] == tag[3];
@@ -225,17 +229,35 @@ private:
     return 0;
   }
   
-  WDL_String GetFontString(int nameID)
+  WDL_String SearchFontString(int nameID)
   {
-#ifdef OS_WIN
+    WDL_String str = GetFontString(nameID, EStringID::Windows);
+    
+    if (str.GetLength())
+      return str;
+    
+    return GetFontString(nameID, EStringID::Mac);
+  }
+    
+  WDL_String GetFontString(int nameID, EStringID stringID)
+  {
+    // Default to windows values
+      
     int platformID = 3;
     int encodingID = 1;
     int languageID = 0x409;
-#else
-    int platformID = 1;
-    int encodingID = 0;
-    int languageID = 0;
-#endif
+      
+    switch (stringID)
+    {
+      case EStringID::Mac:
+        platformID = 1;
+        encodingID = 0;
+        languageID = 0;
+        break;
+            
+      case EStringID::Windows:
+        break;
+    }
     
     for (uint16_t i = 0; i < GetUInt16(mNameLocation + 2); ++i)
     {
@@ -247,22 +269,34 @@ private:
         uint32_t stringLocation = GetUInt16(mNameLocation + 4) + GetUInt16(loc + 10);
         uint16_t length = GetUInt16(loc + 8);
         
+        switch (stringID)
+        {
+          case EStringID::Windows:
+          {
+            WDL_TypedBuf<char> utf8;
+            WDL_TypedBuf<uint16_t> utf16;
+            utf16.Resize(length / sizeof(uint16_t));
+            
+            for (int j = 0; j < length; j++)
+              utf16.Get()[j] = GetUInt16(mNameLocation + stringLocation + j * 2);
+            
 #ifdef OS_WIN
-        WDL_TypedBuf<WCHAR> utf16;
-        WDL_TypedBuf<char> utf8;
-        
-        utf16.Resize(length / sizeof(WCHAR));
-        
-        for (int j = 0; j < length; j++)
-          utf16.Get()[j] = GetUInt16(mNameLocation + stringLocation + j * 2);
-        
-        int convertedLength = WideCharToMultiByte(CP_UTF8, 0, utf16.Get(), utf16.GetSize(), 0, 0, NULL, NULL);
-        utf8.Resize(convertedLength);
-        WideCharToMultiByte(CP_UTF8, 0, utf16.Get(), utf16.GetSize(), utf8.Get(), utf8.GetSize(), NULL, NULL);
-        return WDL_String(utf8.Get(), convertedLength);
-#else
-        return WDL_String((const char*)(mData + mNameLocation + stringLocation), length);
+            int convertedLength = WideCharToMultiByte(CP_UTF8, 0, utf16.Get(), utf16.GetSize(), 0, 0, NULL, NULL);
+            utf8.Resize(convertedLength);
+            WideCharToMultiByte(CP_UTF8, 0, utf16.Get(), utf16.GetSize(), utf8.Get(), utf8.GetSize(), NULL, NULL);
+#elif defined OS_MAC || defined OS_IOS
+            CFStringRef str = CFStringCreateWithBytes(NULL, (const UInt8 *) utf16.Get(), length, kCFStringEncodingUTF16, false);
+            utf8.Resize((int) CFStringGetMaximumSizeForEncoding(CFStringGetLength(str), kCFStringEncodingUTF8));
+            CFStringGetCString(str, utf8.Get(), utf8.GetSize(), kCFStringEncodingUTF8);
+            CFRelease(str);
+#elif defined OS_WEB
 #endif
+            return WDL_String(utf8.Get(), utf8.GetSize());
+          }
+            
+          case EStringID::Mac:
+             return WDL_String((const char*)(mData + mNameLocation + stringLocation), length);
+        }
       }
     }
     
