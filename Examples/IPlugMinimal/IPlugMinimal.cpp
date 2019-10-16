395e9a22c791ce548d0cfbff5d234072d29c28e8
Oli Larkin (rpi) <olilarkin@googlmail.com>
2019-10-16 22:27:10 +0000

    Add minimal project to get working without gui

---
+++ Examples/IPlugMinimal/IPlugMinimal.cpp
@@ -0,0 +1,20 @@
+#include "IPlugMinimal.h"
+#include "IPlug_include_in_plug_src.h"
+
+IPlugMinimal::IPlugMinimal(const InstanceInfo& info)
+: Plugin(info, MakeConfig(kNumParams, kNumPrograms))
+{
+  GetParam(kGain)->InitDouble("Gain", 0., 0., 100.0, 0.01, "%");
+}
+
+void IPlugMinimal::ProcessBlock(sample** inputs, sample** outputs, int nFrames)
+{
+  const double gain = GetParam(kGain)->Value() / 100.;
+  const int nChans = NOutChansConnected();
+  
+  for (int s = 0; s < nFrames; s++) {
+    for (int c = 0; c < nChans; c++) {
+      outputs[c][s] = inputs[c][s] * gain;
+    }
+  }
+}
