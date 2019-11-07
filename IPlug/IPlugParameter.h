a33d537d18d50001b6e4ae5d2b5a75e64ac5c077
Oliver Larkin <olilarkin@googlemail.com>
2019-11-07 00:35:37 +0000

    fix mShape

--- IPlug/IPlugParameter.h
+++ IPlug/IPlugParameter.h
@@ -93,7 +93,7 @@ public:
     double NormalizedToValue(double value, const IParam& param) const override;
     double ValueToNormalized(double value, const IParam& param) const override;
   
-    double mShape = 1.;
+    double mShape;
   };
   
   /** PowCurve parameter shaping */
