15c788932058b11866ee6d9097e07692bf253c77
Oli Larkin <olilarkin@googlemail.com>
2020-01-05 14:17:01 +0000

    Add IGraphics_IOS MultiTouch

--- IGraphics/Platforms/IGraphicsIOS_view.h
+++ IGraphics/Platforms/IGraphicsIOS_view.h
@@ -59,7 +59,6 @@ using namespace igraphics;
 - (void) createTextEntry: (int) paramIdx : (const IText&) text : (const char*) str : (int) length : (CGRect) areaRect;
 - (void) endUserInput;
 - (void) showMessageBox: (const char*) str : (const char*) caption : (EMsgBoxType) type : (IMsgBoxCompletionHanderFunc) completionHandler;
-- (void) getTouchXY: (CGPoint) pt x: (float*) pX y: (float*) pY;
 @property (readonly) CAMetalLayer* metalLayer;
 @property (nonatomic, strong) CADisplayLink *displayLink;
 
