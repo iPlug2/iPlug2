9a2954bade820e8aa84a39680214e071c421f259
oli <olilarkin@googlemail.com>
2020-01-14 11:08:49 +0000

    just build winx64

--- Dependencies/IGraphics/build-resvg-mac.sh
+++ Dependencies/IGraphics/build-resvg-mac.sh
@@ -4,7 +4,8 @@ cd ../Build/src/resvg
 export SKIA_DIR=$(PWD)/../skia
 export SKIA_LIB_DIR=$(PWD)/../../mac/lib
 export MACOSX_DEPLOYMENT_TARGET=10.9
-cargo build --release --features "skia-backend cairo-backend"
+cargo build --release --features "skia-backend"
+# cargo build --release --features "cairo-backend"
 
 cd capi
 
@@ -15,9 +16,11 @@ fi
 echo ---------------------------------
 mkdir .cargo
 echo [build] > .cargo/config
-echo rustflags = [\"-L$SKIA_LIB_DIR\", \"-ldylib=pixman-1\", \"-lframework=Metal\", \"-lframework=Foundation\"] >> .cargo/config
+# \"-ldylib=pixman-1\",
+echo rustflags = [\"-L$SKIA_LIB_DIR\", \"-lframework=Metal\", \"-lframework=Foundation\"] >> .cargo/config
 
-cargo build --verbose --release --features "skia-backend cairo-backend"
+# cargo build --verbose --release --features "skia-backend cairo-backend"
+cargo build --verbose --release --features "skia-backend"
 cd ..
 
 mv target/release/libresvg.a ../../mac/lib
--- Dependencies/IGraphics/build-resvg-win.bat
+++ Dependencies/IGraphics/build-resvg-win.bat
@@ -2,26 +2,26 @@ echo off
 
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_x64
 
-set BACKENDS="cairo-backend"
+set BACKENDS="skia-backend"
 
-set LIBPATH=%CD:\=\\%\\..\\Build\\win\\x64\\Debug
+REM set LIBPATH=%CD:\=\\%\\..\\Build\\win\\x64\\Debug
 set SKIA_DIR=%CD:\=\\%\\..\\Build\\src\\skia
-set SKIA_LIB_DIR=%LIBPATH%
+REM set SKIA_LIB_DIR=%LIBPATH%
 
-cd ..\Build\src\resvg
-cargo build --features=%BACKENDS%
+REM cd ..\Build\src\resvg
+REM cargo build --features=%BACKENDS%
 
-cd capi
-if exist .cargo rmdir /q /s .cargo
-mkdir .cargo
-echo [build] >> .cargo/config
-echo rustflags = ["-C", "link-arg=/LIBPATH:%LIBPATH%", "-C", "link-arg=pixman.lib", "-C", "link-arg=freetype.lib", "-C", "link-arg=zlib.lib", "-C", "link-arg=Msimg32.lib", "-C", "link-arg=gdi32.lib", "-C", "link-arg=User32.lib", "-C", "target-feature=+crt-static"] >> .cargo/config
-cargo build --features=%BACKENDS%
-cd .. 
+REM cd capi
+REM if exist .cargo rmdir /q /s .cargo
+REM mkdir .cargo
+REM echo [build] >> .cargo/config
+REM echo rustflags = ["-C", "link-arg=/LIBPATH:%LIBPATH%", "-C", "link-arg=pixman.lib", "-C", "link-arg=freetype.lib", "-C", "link-arg=zlib.lib", "-C", "link-arg=Msimg32.lib", "-C", "link-arg=gdi32.lib", "-C", "link-arg=User32.lib", "-C", "target-feature=+crt-static"] >> .cargo/config
+REM cargo build --features=%BACKENDS%
+REM cd .. 
 
-copy target\debug\resvg.lib ..\..\win\x64\Debug
+REM copy target\debug\resvg.lib ..\..\win\x64\Debug
 
-cd ..\..\..\IGraphics
+REM cd ..\..\..\IGraphics
 
 REM ---------------------------------------------------------
 
@@ -35,7 +35,9 @@ cd capi
 if exist .cargo rmdir /q /s .cargo
 mkdir .cargo
 echo [build] >> .cargo/config
-echo rustflags = ["-C", "link-arg=/LIBPATH:%LIBPATH%", "-C", "link-arg=pixman.lib", "-C", "link-arg=freetype.lib", "-C", "link-arg=zlib.lib", "-C", "link-arg=Msimg32.lib", "-C", "link-arg=gdi32.lib", "-C", "link-arg=User32.lib", "-C", "target-feature=+crt-static"] >> .cargo/config
+REM "link-arg=pixman.lib", "-C", "link-arg=freetype.lib", "-C", "link-arg=zlib.lib", "-C", "link-arg=Msimg32.lib", "-C", "link-arg=gdi32.lib", "-C", "link-arg=User32.lib",
+echo rustflags = ["-C", "link-arg=/LIBPATH:%LIBPATH%", "-C", "target-feature=+crt-static"] >> .cargo/config
+REM echo rustflags = ["-C", "link-arg=/LIBPATH:%LIBPATH%", "-C", "link-arg=pixman.lib", "-C", "link-arg=freetype.lib", "-C", "link-arg=zlib.lib", "-C", "link-arg=Msimg32.lib", "-C", "link-arg=gdi32.lib", "-C", "link-arg=User32.lib", "-C", "target-feature=+crt-static"] >> .cargo/config
 cargo build --release --features=%BACKENDS%
 cd .. 
 
