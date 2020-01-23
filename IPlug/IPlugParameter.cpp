7d802241f57ccde2eaf6c05651122e2292877d69
Oli Larkin <olilarkin@googlemail.com>
2020-01-23 19:29:02 +0000

    fix new IParam

--- IPlug/IPlugParameter.cpp
+++ IPlug/IPlugParameter.cpp
@@ -117,11 +117,11 @@ void IParam::InitEnum(const char* name, int defaultVal, int nEnums, const char* 
   }
 }
 
-void IParam::InitEnum(const char* name, int default, const std::initializer_list<const char*>& listItems, int flags, const char* group)
+void IParam::InitEnum(const char* name, int defaultVal, const std::initializer_list<const char*>& listItems, int flags, const char* group)
 {
   if (mType == kTypeNone) mType = kTypeEnum;
 
-  InitInt(name, default, 0, listItems.size() - 1, "", flags | kFlagStepped, group);
+  InitInt(name, defaultVal, 0, static_cast<int>(listItems.size()) - 1, "", flags | kFlagStepped, group);
 
   int idx = 0;
   for (auto& item : listItems)
