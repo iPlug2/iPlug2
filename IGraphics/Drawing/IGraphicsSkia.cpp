0d9e1561271a9b87b8b83d69f746e8a6d8fee5c2
Oli Larkin <olilarkin@googlemail.com>
2020-01-27 19:27:28 +0000

    Skia assert on invalid bitmap

--- IGraphics/Drawing/IGraphicsSkia.cpp
+++ IGraphics/Drawing/IGraphicsSkia.cpp
@@ -69,6 +69,9 @@ IGraphicsSkia::Bitmap::Bitmap(sk_sp<SkSurface> surface, int width, int height, i
 IGraphicsSkia::Bitmap::Bitmap(const char* path, double sourceScale)
 {
   auto data = SkData::MakeFromFileName(path);
+  
+  assert(!data->isEmpty());
+  
   mDrawable.mImage = SkImage::MakeFromEncoded(data);
   
   mDrawable.mIsSurface = false;
