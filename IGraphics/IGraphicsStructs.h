bf51e3c9a9a06aa16c27387a244c5b01975fd7fc
Alex Harker <ajharker@gmail.com>
2020-02-10 21:57:54 +0000

    Fix issue with constructor parameter name clashing with member variable

--- IGraphics/IGraphicsStructs.h
+++ IGraphics/IGraphicsStructs.h
@@ -1566,9 +1566,10 @@ struct IMouseMod
    * @param r /todo
    * @param s /todo
    * @param c /todo
-   * @param a /todo */
-  IMouseMod(bool l = false, bool r = false, bool s = false, bool c = false, bool a = false, ITouchID touchID = 0)
-    : L(l), R(r), S(s), C(c), A(a), touchID(touchID)
+   * @param a /todo
+   * @pararm touch /todo */
+  IMouseMod(bool l = false, bool r = false, bool s = false, bool c = false, bool a = false, ITouchID touch = 0)
+    : L(l), R(r), S(s), C(c), A(a), touchID(touch)
     {}
   
   /** /todo */
