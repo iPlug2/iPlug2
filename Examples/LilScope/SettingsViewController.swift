57548c041223557e91e18cfc7776ad066c538f33
Oli Larkin <olilarkin@googlemail.com>
2020-02-23 16:42:07 +0000

    iOS: support AUM size{} tags in AUv3 plist

---
+++ Examples/LilScope/SettingsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsViewController.swift
+//  LilScope-iOS
+//
+//  Created by Oliver Larkin on 22/02/2020.
+//
+
+import Foundation
---
+++ Examples/LilScope/projects/ColorPickerViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ColorPickerViewController.swift
+//  Shared
+//
+//  Created by Oliver Larkin on 22/02/2020.
+//
+
+import Foundation
