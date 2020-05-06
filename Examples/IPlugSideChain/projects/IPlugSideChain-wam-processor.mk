fc4f488304199da678c7c062fdfd14b9790c6779
Oli Larkin <olilarkin@googlemail.com>
2020-05-06 15:23:00 +0100

    Add missing projects folders

---
+++ Examples/IPlugSideChain/projects/IPlugSideChain-wam-processor.mk
@@ -0,0 +1,12 @@
+include ../config/IPlugSideChain-web.mk
+
+TARGET = ../build-web/scripts/IPlugSideChain-wam.js
+
+SRC += $(WAM_SRC)
+CFLAGS += $(WAM_CFLAGS)
+CFLAGS += $(EXTRA_CFLAGS)
+LDFLAGS += $(WAM_LDFLAGS) \
+-s EXPORTED_FUNCTIONS=$(WAM_EXPORTS)
+
+$(TARGET): $(OBJECTS)
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $(SRC)
