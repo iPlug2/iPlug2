e5006d51d0585a592481620b54318df5062309a7
Oli Larkin <olilarkin@googlemail.com>
2020-05-16 23:00:51 +0100

    Remove some stray semicolons

--- IPlug/IPlugAPIBase.h
+++ IPlug/IPlugAPIBase.h
@@ -117,19 +117,19 @@ public:
   void SetParameterValue(int paramIdx, double normalizedValue);
   
   /** Get the color of the track that the plug-in is inserted on */
-  virtual void GetTrackColor(int& r, int& g, int& b) { r = 0; g = 0; b = 0; };
+  virtual void GetTrackColor(int& r, int& g, int& b) { r = 0; g = 0; b = 0; }
 
   /** Get the name of the track that the plug-in is inserted on */
-  virtual void GetTrackName(WDL_String& str) {};
+  virtual void GetTrackName(WDL_String& str) {}
 
   /** Get the index of the track that the plug-in is inserted on */
-  virtual int GetTrackIndex() { return 0; };
+  virtual int GetTrackIndex() { return 0; }
 
   /** Get the namespace of the track that the plug-in is inserted on */
-  virtual void GetTrackNamespace(WDL_String& str) {};
+  virtual void GetTrackNamespace(WDL_String& str) {}
 
   /** Get the namespace index of the track that the plug-in is inserted on */
-  virtual int GetTrackNamespaceIndex() { return 0; };
+  virtual int GetTrackNamespaceIndex() { return 0; }
 
   /** /todo */
   virtual void DirtyParametersFromUI() override;
@@ -190,23 +190,23 @@ private:
   
   /** Implemented by the API class, called by the UI (or by a delegate) at the beginning of a parameter change gesture
    * @param paramIdx The parameter that is being changed */
-  virtual void BeginInformHostOfParamChange(int paramIdx) {};
+  virtual void BeginInformHostOfParamChange(int paramIdx) {}
 
   /** Implemented by the API class, called by the UI (or by a delegate) at the end of a parameter change gesture
    * @param paramIdx The parameter that is being changed */
-  virtual void EndInformHostOfParamChange(int paramIdx) {};
+  virtual void EndInformHostOfParamChange(int paramIdx) {}
 
   /** Implemented by the API class, called by the UI via SetParameterValue() with the value of a parameter change gesture
    * @param paramIdx The parameter that is being changed
    * @param normalizedValue The new normalised value of the parameter being changed */
-  virtual void InformHostOfParamChange(int paramIdx, double normalizedValue) {};
+  virtual void InformHostOfParamChange(int paramIdx, double normalizedValue) {}
   
   //DISTRIBUTED ONLY (Currently only VST3)
   /** /todo */
-  virtual void TransmitMidiMsgFromProcessor(const IMidiMsg& msg) {};
+  virtual void TransmitMidiMsgFromProcessor(const IMidiMsg& msg) {}
   
   /** /todo */
-  virtual void TransmitSysExDataFromProcessor(const SysExData& data) {};
+  virtual void TransmitSysExDataFromProcessor(const SysExData& data) {}
 
   void OnTimer(Timer& t);
 
