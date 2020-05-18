iPlug2/iPlug2

commit f7fb4579d3d103ad306ff56940a6dda57ed0eb32

Add IEditorDelegate::OnParentWindowResize() and associated changes

IPlug/AAX/IPlugAAX.cpp
@@ -535,8 +535,6 @@ bool IPlugAAX::EditorResize(int viewWidth, int viewHeight)
     
     if (pViewInterface && (viewWidth != GetEditorWidth() || viewHeight != GetEditorHeight()))
       pViewInterface->GetViewContainer()->SetViewSize(oEffectViewSize);
-
-    SetEditorSize(viewWidth, viewHeight);
   }
   
   return true;

IPlug/APP/IPlugAPP.cpp
@@ -38,7 +38,7 @@ IPlugAPP::IPlugAPP(const InstanceInfo& info, const Config& config)
 bool IPlugAPP::EditorResize(int viewWidth, int viewHeight)
 {
   bool parentResized = false;
-    
+  
   if (viewWidth != GetEditorWidth() || viewHeight != GetEditorHeight())
   {
     #ifdef OS_MAC
@@ -48,7 +48,6 @@ bool IPlugAPP::EditorResize(int viewWidth, int viewHeight)
     SetWindowPos(gHWND, 0, r.left, r.bottom - viewHeight - TITLEBAR_BODGE, viewWidth, viewHeight + TITLEBAR_BODGE, 0);
     parentResized = true;
     #endif
-    SetEditorSize(viewWidth, viewHeight);
   }
   
   return parentResized;

IPlug/IPlugAPIBase.cpp
@@ -85,10 +85,14 @@ bool IPlugAPIBase::CompareState(const uint8_t* pIncomingState, int startPos) con
   return isEqual;
 }
 
-bool IPlugAPIBase::EditorResize(int viewWidth, int viewHeight)
+bool IPlugAPIBase::EditorResizeFromUI(int viewWidth, int viewHeight, bool needsPlatformResize)
 {
   SetEditorSize(viewWidth, viewHeight);
-  return false;
+  
+  if (needsPlatformResize)
+    return EditorResize(viewWidth, viewHeight);
+  else
+    return true;
 }
 
 #pragma mark -

IPlug/VST2/IPlugVST2.cpp
@@ -212,8 +212,6 @@ bool IPlugVST2::EditorResize(int viewWidth, int viewHeight)
     
       resized = mHostCallback(&mAEffect, audioMasterSizeWindow, viewWidth, viewHeight, 0, 0.f);
     }
-    
-    SetEditorSize(viewWidth, viewHeight);
   }
 
   return resized;

IPlug/VST3/IPlugVST3.cpp
@@ -218,8 +218,6 @@ bool IPlugVST3::EditorResize(int viewWidth, int viewHeight)
   {
     if (viewWidth != GetEditorWidth() || viewHeight != GetEditorHeight())
       mView->resize(viewWidth, viewHeight);
-
-    SetEditorSize(viewWidth, viewHeight);
   }
   
   return true;

IPlug/VST3/IPlugVST3_Controller.cpp
@@ -143,9 +143,7 @@ bool IPlugVST3Controller::EditorResize(int viewWidth, int viewHeight)
   if (HasUI())
   {
     if (viewWidth != GetEditorWidth() || viewHeight != GetEditorHeight())
-      mView->resize(viewWidth, viewHeight);
- 
-    SetEditorSize(viewWidth, viewHeight);
+      mView->resize(viewWidth, viewHeight); 
   }
   
   return true;

IGraphics/IGraphics.cpp
@@ -79,14 +79,17 @@ IGraphics::~IGraphics()
 
 void IGraphics::SetScreenScale(int scale)
 {
+  int windowWidth = WindowWidth() * GetPlatformWindowScale();
+  int windowHeight = WindowHeight() * GetPlatformWindowScale();
   mScreenScale = scale;
-  PlatformResize(GetDelegate()->EditorResizeFromUI(WindowWidth() * GetPlatformWindowScale(), WindowHeight() * GetPlatformWindowScale()));
+    
+  PlatformResize(GetDelegate()->EditorResizeFromUI(windowWidth, windowHeight, true));
   ForAllControls(&IControl::OnRescale);
   SetAllControlsDirty();
   DrawResize();
 }
 
-void IGraphics::Resize(int w, int h, float scale)
+void IGraphics::Resize(int w, int h, float scale, bool needsPlatformResize)
 {
   w = Clip(w, mMinWidth, mMaxWidth);
   h = Clip(h, mMinHeight, mMaxHeight);
@@ -104,7 +107,10 @@ void IGraphics::Resize(int w, int h, float scale)
   if (mCornerResizer)
     mCornerResizer->OnRescale();
 
-  PlatformResize(GetDelegate()->EditorResizeFromUI(WindowWidth() * GetPlatformWindowScale(), WindowHeight() * GetPlatformWindowScale()));
+  int windowWidth = WindowWidth() * GetPlatformWindowScale();
+  int windowHeight = WindowHeight() * GetPlatformWindowScale();
+    
+  PlatformResize(GetDelegate()->EditorResizeFromUI(windowWidth, windowHeight, needsPlatformResize));
   ForAllControls(&IControl::OnResize);
   SetAllControlsDirty();
   DrawResize();

IGraphics/IGraphics.h
@@ -983,8 +983,10 @@ public:
   /** \todo detailed description of how this works
    * @param w New width in pixels
    * @param h New height in pixels
-   * @param scale New scale ratio */
-  void Resize(int w, int h, float scale);
+   * @param scale New scale ratio
+   * @param needsPlatformResize This should be true for a "manual" resize from the plug-in UI and false
+   * if being called from IEditorDelegate::OnParentWindowResize(), in order to avoid feedback */
+  void Resize(int w, int h, float scale, bool needsPlatformResize = true);
   
   /** Enables strict drawing mode. \todo explain strict drawing
    * @param strict Set /true to enable strict drawing mode */

commit 39c16c0fff76f04a7f932eadbfdba629a193243e

Fix corner resizer bug introduced in f7fb4579 "Add IEditorDelegate::OnParentWindowResize() and associated changes"

IPlug/AAX/IPlugAAX.cpp
@@ -535,6 +535,8 @@ bool IPlugAAX::EditorResize(int viewWidth, int viewHeight)
     
     if (pViewInterface && (viewWidth != GetEditorWidth() || viewHeight != GetEditorHeight()))
       pViewInterface->GetViewContainer()->SetViewSize(oEffectViewSize);
+
+    SetEditorSize(viewWidth, viewHeight);
   }
   
   return true;

IPlug/APP/IPlugAPP.cpp
@@ -38,7 +38,7 @@ IPlugAPP::IPlugAPP(const InstanceInfo& info, const Config& config)
 bool IPlugAPP::EditorResize(int viewWidth, int viewHeight)
 {
   bool parentResized = false;
-  
+    
   if (viewWidth != GetEditorWidth() || viewHeight != GetEditorHeight())
   {
     #ifdef OS_MAC
@@ -48,6 +48,7 @@ bool IPlugAPP::EditorResize(int viewWidth, int viewHeight)
     SetWindowPos(gHWND, 0, r.left, r.bottom - viewHeight - TITLEBAR_BODGE, viewWidth, viewHeight + TITLEBAR_BODGE, 0);
     parentResized = true;
     #endif
+    SetEditorSize(viewWidth, viewHeight);
   }
   
   return parentResized;

IPlug/IPlugAPIBase.cpp
@@ -86,9 +86,7 @@ bool IPlugAPIBase::CompareState(const uint8_t* pIncomingState, int startPos) con
 }
 
 bool IPlugAPIBase::EditorResizeFromUI(int viewWidth, int viewHeight, bool needsPlatformResize)
-{
-  SetEditorSize(viewWidth, viewHeight);
-  
+{  
   if (needsPlatformResize)
     return EditorResize(viewWidth, viewHeight);
   else

IPlug/VST2/IPlugVST2.cpp
@@ -212,6 +212,8 @@ bool IPlugVST2::EditorResize(int viewWidth, int viewHeight)
     
       resized = mHostCallback(&mAEffect, audioMasterSizeWindow, viewWidth, viewHeight, 0, 0.f);
     }
+    
+    SetEditorSize(viewWidth, viewHeight);
   }
 
   return resized;

IPlug/VST3/IPlugVST3.cpp
@@ -218,6 +218,8 @@ bool IPlugVST3::EditorResize(int viewWidth, int viewHeight)
   {
     if (viewWidth != GetEditorWidth() || viewHeight != GetEditorHeight())
       mView->resize(viewWidth, viewHeight);
+
+    SetEditorSize(viewWidth, viewHeight);
   }
   
   return true;

IPlug/VST3/IPlugVST3_Controller.cpp
@@ -143,7 +143,9 @@ bool IPlugVST3Controller::EditorResize(int viewWidth, int viewHeight)
   if (HasUI())
   {
     if (viewWidth != GetEditorWidth() || viewHeight != GetEditorHeight())
-      mView->resize(viewWidth, viewHeight); 
+      mView->resize(viewWidth, viewHeight);
+ 
+    SetEditorSize(viewWidth, viewHeight);
   }
   
   return true;