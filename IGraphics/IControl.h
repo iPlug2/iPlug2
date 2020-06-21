75b741788d094eff5e406276dfe6ff9fdbb3e888
Oli Larkin <olilarkin@googlemail.com>
2020-06-21 01:44:57 +0100

    Add ITrackControlBase::SetZeroValueStepHasBounds

--- IGraphics/IControl.h
+++ IGraphics/IControl.h
@@ -1397,6 +1397,12 @@ public:
     SetDirty(false);
   }
   
+  void SetZeroValueStepHasBounds(bool val)
+  {
+    mZeroValueStepHasBounds = val;
+    OnResize();
+  }
+  
   bool HasTrackNames() const
   {
     return mTrackNames.GetSize() > 0;
