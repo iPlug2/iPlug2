3e28f0a18a3c2e37de4067dbbd2a163fc5dd14cf
dmsplugins <dspicer64@gmail.com>
2020-06-23 22:36:27 -0400

    AUv2 preset persistence Preset name and chunk persistently restored at idx0 from Saved host project or .aupresetfile

--- IPlug/AUv2/IPlugAU.cpp
+++ IPlug/AUv2/IPlugAU.cpp
@@ -1460,8 +1460,6 @@ OSStatus IPlugAU::SetState(CFPropertyListRef pPropList)
   {
     return kAudioUnitErr_InvalidPropertyValue;
   }
-  
-  RestorePreset(presetName);
 
   IByteChunk chunk;
 
@@ -1478,7 +1476,9 @@ OSStatus IPlugAU::SetState(CFPropertyListRef pPropList)
   {
     return kAudioUnitErr_InvalidPropertyValue;
   }
-
+  
+  ModifyPreset(0, presetName);
+  RestorePreset(presetName);
   OnRestoreState();
   return noErr;
 }
