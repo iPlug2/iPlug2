894bad24f8bee6028a1f4052fa7dfb0635aa77f0
Andrew Heintz <bindernews@gmail.com>
2020-06-27 19:20:30 -0400

    Move VST3 SDK into its own static library to help simplify the iPlug2-specific VST3 code.

--- IPlug/VST3/VST3.cmake
+++ IPlug/VST3/VST3.cmake
@@ -1,4 +1,5 @@
 cmake_minimum_required(VERSION 3.11)
+find_package(VST3_SDK REQUIRED)
 
 find_file(VST3_32_PATH
   "VST3"
@@ -21,63 +22,27 @@ set(IPLUG2_VST_ICON
 ##########################
 
 add_library(iPlug2_VST3 INTERFACE)
-
-set(_inc "")
-set(_def "VST3_API" "IPLUG_EDITOR=1" "IPLUG_DSP=1")
-set(_src "")
-# iPlug2 stuff
 set(sdk ${IPLUG2_DIR}/IPlug/VST3)
 list(APPEND _inc ${sdk})
-target_sources(iPlug2_VST3 INTERFACE 
-  "${sdk}/IPlugVST3.h"
-  "${sdk}/IPlugVST3.cpp"
-  "${sdk}/IPlugVST3_Common.h"
-  "${sdk}/IPlugVST3_Controller.h"
-  "${sdk}/IPlugVST3_Controller.cpp"
-  "${sdk}/IPlugVST3_ControllerBase.h"
-  "${sdk}/IPlugVST3_Defs.h"
-  "${sdk}/IPlugVST3_Parameter.h"
-  "${sdk}/IPlugVST3_Processor.h"
-  #"${sdk}/IPlugVST3_Processor.cpp"
-  "${sdk}/IPlugVST3_ProcessorBase.h"
-  "${sdk}/IPlugVST3_ProcessorBase.cpp"
-  "${sdk}/IPlugVST3_View.h"
+iplug2_target_add(iPlug2_VST3 INTERFACE
+  SOURCE
+    "${sdk}/IPlugVST3.h"
+    "${sdk}/IPlugVST3.cpp"
+    "${sdk}/IPlugVST3_Common.h"
+    "${sdk}/IPlugVST3_Controller.h"
+    "${sdk}/IPlugVST3_Controller.cpp"
+    "${sdk}/IPlugVST3_ControllerBase.h"
+    "${sdk}/IPlugVST3_Defs.h"
+    "${sdk}/IPlugVST3_Parameter.h"
+    "${sdk}/IPlugVST3_Processor.h"
+    #"${sdk}/IPlugVST3_Processor.cpp"
+    "${sdk}/IPlugVST3_ProcessorBase.h"
+    "${sdk}/IPlugVST3_ProcessorBase.cpp"
+    "${sdk}/IPlugVST3_View.h"
+  INCLUDE "${sdk}"
+  DEFINE "VST3_API" "IPLUG_EDITOR=1" "IPLUG_DSP=1"
+  LINK VST3_SDK iPlug2_Core
 )
-# VST3 SDK files
-set(VST3_SDK ${IPLUG2_DIR}/Dependencies/IPlug/VST3_SDK)
-list(APPEND _inc "${VST3_SDK}")
-# Pick up all the VST3 source files, there are a LOT of them. 
-file(GLOB_RECURSE tmp ${VST3_SDK}/base/*.cpp)
-list(APPEND _src ${tmp})
-file(GLOB_RECURSE tmp ${VST3_SDK}/pluginterfaces/*.cpp)
-list(APPEND _src ${tmp})
-# In the public.sdk dir we only add specific sources.
-set(sdk ${VST3_SDK}/public.sdk/source)
-list(APPEND _src
-  "${sdk}/common/commoniids.cpp"
-  "${sdk}/common/memorystream.cpp"
-  "${sdk}/common/pluginview.cpp" "${sdk}/vst/vstaudioeffect.cpp"
-  "${sdk}/vst/vstbus.cpp" "${sdk}/vst/vstcomponent.cpp"
-  "${sdk}/vst/vstcomponentbase.cpp" "${sdk}/vst/vstinitiids.cpp"
-  "${sdk}/vst/vstparameters.cpp" "${sdk}/vst/vstsinglecomponenteffect.cpp"
-)
-list(APPEND _inc
-  "${sdk}/vst/vst2wrapper"
-)
-# Platform-dependent stuff
-if (WIN32)
-  list(APPEND _src "${sdk}/main/dllmain.cpp" "${sdk}/main/pluginfactory.cpp" "${sdk}/main/winexport.def")
-  list(APPEND _src "${sdk}/common/threadchecker_win32.cpp")
-
-elseif (CMAKE_SYSTEM_NAME MATCHES "Darwin")
-  list(APPEND _def "SWELL_CLEANUP_ON_UNLOAD")
-  list(APPEND _src "${sdk}/main/macmain.cpp" "${sdk}/main/pluginfactory.cpp" "${sdk}/main/macexport.exp")
-endif()
-
-source_group(TREE ${VST3_SDK} PREFIX "IPlug/VST3" FILES ${_src})
-iplug2_add_interface(iPlug2_VST3 INCLUDE ${_inc} SOURCE ${_src} DEFINE ${_def} LINK iPlug2_Core)
-
-
 
 function(iplug2_configure_vst3 target)
   if (WIN32)
