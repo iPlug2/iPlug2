0294911d4a4319533fd449e05a6fb64f6aefd351
Jonas Eriksson <wizzie@hotmail.se>
2020-09-02 14:48:27 +0200

    added vst3sdk recursive submodules cloning

--- Dependencies/IPlug/download-iplug-libs.sh
+++ Dependencies/IPlug/download-iplug-libs.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 rm -r WAM_AWP
-rm -r WAM_SDK
 git clone https://github.com/iplug2/audioworklet-polyfill WAM_AWP
+
+rm -r WAM_SDK
 git clone https://github.com/iplug2/api.git WAM_SDK
+
+rm -r VST3_SDK
+git clone -v --recurse-submodules https://github.com/steinbergmedia/vst3sdk.git VST3_SDK
