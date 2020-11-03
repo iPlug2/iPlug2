e55443e5b3759039cba8be718d3d5274d4b44722
Oli Larkin <olilarkin@googlemail.com>
2020-11-03 09:33:45 +0000

    normalize indentation in python scripts

--- Scripts/server.py
+++ Scripts/server.py
@@ -1,4 +1,5 @@
 import BaseHTTPServer, SimpleHTTPServer
+
 SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map['.wasm'] = 'application/wasm'
 port = 8000
 httpd = BaseHTTPServer.HTTPServer(('localhost', 8000), SimpleHTTPServer.SimpleHTTPRequestHandler)
