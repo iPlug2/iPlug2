95f958679de033c18267d6ba7b885454fff099b8
Oli Larkin <olilarkin@googlemail.com>
2020-11-03 10:41:10 +0000

    fix previous commit

--- Dependencies/IPlug/download-iplug-libs.sh
+++ Dependencies/IPlug/download-iplug-libs.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-rm -r VST3_SDK
-rm -r WAM_AWP
-rm -r WAM_SDK
+sudo rm -r VST3_SDK
+sudo rm -r WAM_AWP
+sudo rm -r WAM_SDK
 git clone https://github.com/iplug2/audioworklet-polyfill WAM_AWP
 git clone https://github.com/iplug2/api.git WAM_SDK
 
@@ -12,6 +12,7 @@ git submodule update --init pluginterfaces
 git submodule update --init base
 git submodule update --init public.sdk
 git submodule update --init doc
+cd ..
 git checkout ./VST3_SDK/README.md
 git checkout ./WAM_SDK/readme.txt
 git checkout ./WAM_AWP/readme.txt
