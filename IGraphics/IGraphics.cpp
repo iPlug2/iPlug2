b020d05e926c7b78ad2df6e834f590804a605b14
Oli Larkin <olilarkin@googlemail.com>
2021-01-17 00:57:36 +0000

    PopupHostContextMenuForParam() takes screen scale into account

--- IGraphics/IGraphics.cpp
+++ IGraphics/IGraphics.cpp
@@ -1446,8 +1446,8 @@ void IGraphics::PopupHostContextMenuForParam(IControl* pControl, int paramIdx, f
         pVST3ContextMenu->addItem(item, pControl);
       }
 
-      x *= GetDrawScale();
-      y *= GetDrawScale();
+      x *= GetTotalScale();
+      y *= GetTotalScale();
       pVST3ContextMenu->popup((Steinberg::UCoord) x, (Steinberg::UCoord) y);
       pVST3ContextMenu->release();
     }
