d1e947b63c5e10094849a87b39fbb3c230750af9
Oli Larkin <olilarkin@googlemail.com>
2021-02-09 20:44:31 +0100

    update windows IPlugWebUI projects for latest WebView prerelease

--- Examples/IPlugWebUI/IPlugWebUI.cpp
+++ Examples/IPlugWebUI/IPlugWebUI.cpp
@@ -8,8 +8,9 @@ IPlugWebUI::IPlugWebUI(const InstanceInfo& info)
 
   // Hard-coded paths must be modified!
 #ifdef OS_WIN
-  SetWebViewPaths("C:\\Users\\oli\\Dev\\iPlug2\\Examples\\IPlugWebUI\\packages\\Microsoft.Web.WebView2.1.0.721-prerelease\\build\\x64\\WebView2Loader.dll", "C:\\Users\\oli\\Dev\\iPlug2\\Examples\\IPlugWebUI\\");
+  SetWebViewPaths("C:\\Users\\oli\\Dev\\iPlug2\\Examples\\IPlugWebUI\\packages\\Microsoft.Web.WebView2.1.0.781-prerelease\\runtimes\\win-x64\\native\\WebView2Loader.dll", "C:\\Users\\oli\\Dev\\iPlug2\\Examples\\IPlugWebUI\\");
 #endif
+
 
   mEditorInitFunc = [&]() {
 #ifdef OS_WIN
