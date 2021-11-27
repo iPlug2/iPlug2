9a25f1531ebaf44934af35b6750e6c5460eff596
Alex Harker <ajharker@gmail.com>
2021-11-27 13:53:34 +0000

    Preliminary implementation of latency / parameters / state

--- IPlug/CLAP/IPlugCLAP.cpp
+++ IPlug/CLAP/IPlugCLAP.cpp
@@ -72,7 +72,125 @@ bool IPlugCLAP::SendMidiMsg(const IMidiMsg& msg)
 //  return false;
 //}
 
+// clap_plugin
+
 bool IPlugCLAP::init() noexcept
 {
   return true;
 }
+
+bool IPlugCLAP::activate(double sampleRate) noexcept
+{
+    SetSampleRate(sampleRate);
+    OnActivate(true);
+    OnReset();
+  
+  return true;
+}
+
+void IPlugCLAP::deactivate() noexcept
+{
+    OnActivate(false);
+}
+
+clap_process_status IPlugCLAP::process(const clap_process *process) noexcept
+{
+    return CLAP_PROCESS_CONTINUE;
+}
+
+// clap_plugin_state
+
+bool IPlugCLAP::stateSave(clap_ostream *stream) noexcept
+{
+  IByteChunk chunk;
+  
+  if (!SerializeState(chunk))
+    return false;
+  
+  return stream->write(stream, chunk.GetData(), chunk.Size()) == chunk.Size();
+}
+
+bool IPlugCLAP::stateLoad(clap_istream *stream) noexcept
+{
+  constexpr int blockSize = 8192;
+  
+  IByteChunk chunk;
+  IByteChunk blockChunk;
+  
+  blockChunk.Resize(blockSize);
+  
+  while (stream->read(stream, blockChunk.GetData(), blockSize) == blockSize)
+    chunk.PutChunk(&blockChunk);
+
+  if (stream->read(stream, blockChunk.GetData(), blockSize) != 0)
+    return false;
+  
+  return UnserializeState(chunk, 0) >= 0;
+}
+
+// clap_plugin_params
+
+bool IPlugCLAP::paramsInfo(int32_t paramIndex, clap_param_info *info) const noexcept
+{
+  assert(MAX_PARAM_NAME_LEN <= CLAP_NAME_SIZE && "iPlug parameter name size exceeds CLAP maximum");
+  assert(MAX_PARAM_GROUP_LEN <= CLAP_MODULE_SIZE && "iPlug group name size exceeds CLAP maximum");
+
+  const IParam *pParam = GetParam(paramIndex);
+  
+  clap_param_info_flags flags = 0;
+
+  if (pParam->GetStepped())
+    flags |= CLAP_PARAM_IS_STEPPED;
+  if (pParam->GetCanAutomate())
+    flags |= CLAP_PARAM_IS_MODULATABLE;
+  
+  info->id = paramIndex;
+  info->flags = flags;
+  info->cookie = nullptr;
+
+  strcpy(info->name, pParam->GetName());
+  strcpy(info->module, pParam->GetGroup());
+
+  // Values
+  
+  info->min_value = pParam->GetMin();
+  info->min_value = pParam->GetMax();
+  info->min_value = pParam->GetDefault();
+  
+  return true;
+}
+
+bool IPlugCLAP::paramsValue(clap_id paramId, double *value) noexcept
+{
+  const IParam *pParam = GetParam(paramId);
+  *value = pParam->Value();
+  return true;
+}
+
+bool IPlugCLAP::paramsValueToText(clap_id paramId, double value, char *display, uint32_t size) noexcept
+{
+  const IParam *pParam = GetParam(paramId);
+  WDL_String str;
+  
+  pParam->GetDisplay(value, false, str);
+  
+  if (size < str.GetLength())
+    return false;
+    
+  strcpy(display, str.Get());
+  return true;
+}
+
+bool IPlugCLAP::paramsTextToValue(clap_id paramId, const char *display, double *value) noexcept
+{
+  const IParam *pParam = GetParam(paramId);
+  *value = pParam->StringToValue(display);
+  return true;
+}
+
+void IPlugCLAP::paramsFlush(const clap_event_list *input_parameter_changes, const clap_event_list *output_parameter_changes) noexcept
+{
+  // FIX
+  // ??
+}
+
--- IPlug/CLAP/IPlugCLAP.h
+++ IPlug/CLAP/IPlugCLAP.h
@@ -53,10 +53,44 @@ public:
 //  bool SendSysEx(const ISysEx& msg) override;
 
 private:
+  
   // clap_plugin
   bool init() noexcept override;
+  bool activate(double sampleRate) noexcept override;
+  void deactivate() noexcept override;
+  //bool startProcessing() noexcept override { return true; }
+  //void stopProcessing() noexcept override {}
+  clap_process_status process(const clap_process *process) noexcept override;
   
-private:
+  //void onMainThread() noexcept override {}
+  //const void *extension(const char *id) noexcept override { return nullptr; }
+
+  //clap_plugin_latency
+  bool implementsLatency() const noexcept override { return true; }
+  uint32_t latencyGet() const noexcept override { return GetLatency(); }
+  
+  // clap_plugin_state
+  bool implementsState() const noexcept override { return true; }
+  bool stateSave(clap_ostream *stream) noexcept override;
+  bool stateLoad(clap_istream *stream) noexcept override;
+  
+  /*void stateMarkDirty() const noexcept {
+     if (canUseState())
+        _hostState->mark_dirty(_host);
+  }*/
+  
+  // clap_plugin_params
+  bool implementsParams() const noexcept override { return true; }
+  uint32_t paramsCount() const noexcept override { return NParams(); }
+  
+  bool paramsInfo(int32_t paramIndex, clap_param_info *info) const noexcept override;
+  
+  bool paramsValue(clap_id paramId, double *value) noexcept override;
+  bool paramsValueToText(clap_id paramId, double value, char *display, uint32_t size) noexcept override;
+  bool paramsTextToValue(clap_id paramId, const char *display, double *value) noexcept override;
+     
+  void paramsFlush(const clap_event_list *input_parameter_changes, const clap_event_list *output_parameter_changes) noexcept;
+  bool isValidParamId(clap_id paramId) const noexcept override { return paramId < NParams(); }
 };
 
 IPlugCLAP* MakePlug(const InstanceInfo& info);
