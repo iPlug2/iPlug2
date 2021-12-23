c694b1480a0ea1b379c8bcb3245699815aa09bed
Oli Larkin <olilarkin@googlemail.com>
2021-12-23 14:13:14 +0100

    Update IPlugEffect for AUv3 objc changes

--- Examples/IPlugEffect/scripts/prepare_resources-mac.py
+++ Examples/IPlugEffect/scripts/prepare_resources-mac.py
@@ -149,7 +149,7 @@ def main():
                                AudioComponents = [{}]),
 #                               NSExtensionServiceRoleType = "NSExtensionServiceRoleTypeEditor",
   NSExtensionPointIdentifier = NSEXTENSIONPOINTIDENTIFIER,
-  NSExtensionPrincipalClass = "IPlugAUViewController"
+  NSExtensionPrincipalClass = "IPlugAUViewController_vIPlugEffect_auv3"
                              )
   auv3['NSExtension']['NSExtensionAttributes']['AudioComponents'] = [{}]
   auv3['NSExtension']['NSExtensionAttributes']['AudioComponents'][0]['description'] = config['PLUG_NAME']
