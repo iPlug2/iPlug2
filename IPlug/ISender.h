3a080c1a33fd7b6a5301e017924e14fca1f4abfb
Oli Larkin <olilarkin@googlemail.com>
2022-06-05 03:25:34 +0200

    Add ISpectrumSender

--- IPlug/ISender.h
+++ IPlug/ISender.h
@@ -17,6 +17,7 @@
  */
 
 #include "denormal.h"
+#include "fft.h"
 
 #include "IPlugPlatform.h"
 #include "IPlugQueue.h"
@@ -72,6 +73,7 @@ public:
     mQueue.Push(d);
   }
 
+  /** This is called on the main thread and can be used to transform the data, e.g. take an FFT. */
   virtual void PrepareDataForUI(ISenderData<MAXNC, T>& d) { /* NO-OP*/ }
   
   /** Pops elements off the queue and sends messages to controls.
@@ -425,10 +427,21 @@ template <int MAXNC = 1, int QUEUE_SIZE = 64, int MAXBUF = 128>
 class IBufferSender : public ISender<MAXNC, QUEUE_SIZE, std::array<float, MAXBUF>>
 {
 public:
+  using TDataPacket = std::array<float, MAXBUF>;
+  using TSender = ISender<MAXNC, QUEUE_SIZE, TDataPacket>;
+  
   IBufferSender(double minThresholdDb = -90., int bufferSize = MAXBUF)
-  : ISender<MAXNC, QUEUE_SIZE, std::array<float, MAXBUF>>()
-  , mThreshold(static_cast<float>(DBToAmp(minThresholdDb)))
+  : TSender()
   {
+    if (minThresholdDb == -std::numeric_limits<double>::infinity())
+    {
+      mThreshold = -1.0f;
+    }
+    else
+    {
+      mThreshold = static_cast<float>(DBToAmp(minThresholdDb));
+    }
+    
     SetBufferSize(bufferSize);
   }
 
@@ -456,7 +469,7 @@ public:
           mBuffer.ctrlTag = ctrlTag;
           mBuffer.nChans = nChans;
           mBuffer.chanOffset = chanOffset;
-          ISender<MAXNC, QUEUE_SIZE, std::array<float, MAXBUF>>::PushData(mBuffer);
+          TSender::PushData(mBuffer);
         }
 
         mPreviousSum = sum;
@@ -486,12 +499,234 @@ public:
   int GetBufferSize() const { return mBufferSize; }
   
 private:
-  ISenderData<MAXNC, std::array<float, MAXBUF>> mBuffer;
+  ISenderData<MAXNC, TDataPacket> mBuffer;
   int mBufCount = 0;
   int mBufferSize = MAXBUF;
   std::array<float, MAXNC> mRunningSum {0.};
   float mPreviousSum = 1.f;
   float mThreshold = 0.01f;
+};
+
+/** ISpectrumSender is designed for sending Spectral Data from the plug-in to the UI */
+template <int MAXNC = 1, int QUEUE_SIZE = 64, int MAX_FFT_SIZE = 4096>
+class ISpectrumSender : public IBufferSender<MAXNC, QUEUE_SIZE, MAX_FFT_SIZE>
+{
+public:
+  using TDataPacket = std::array<float, MAX_FFT_SIZE>;
+  using TBufferSender = IBufferSender<MAXNC, QUEUE_SIZE, MAX_FFT_SIZE>;
+  
+  enum class EWindowType {
+    Hann = 0,
+    BlackmanHarris,
+    Hamming,
+    Flattop,
+    Rectangular
+  };
+  
+  enum class EOutputType {
+    Complex = 0,
+    MagPhase,
+  };
+  
+  ISpectrumSender(int fftSize = 1024, int overlap = 2, EWindowType window = EWindowType::Hann, EOutputType outputType = EOutputType::MagPhase)
+  : TBufferSender(-std::numeric_limits<double>::infinity(), fftSize)
+  , mWindowType(window)
+  , mOutputType(outputType)
+  {
+    WDL_fft_init();
+    SetFFTSizeAndOverlap(fftSize, overlap);
+  }
+
+  void SetFFTSize(int fftSize)
+  {
+    TBufferSender::SetBufferSize(fftSize);
+    SetFFTSize();
+    CalculateWindow();
+    CalculateScalingFactors();
+  }
+  
+  void SetFFTSizeAndOverlap(int fftSize, int overlap)
+  {
+    mOverlap = overlap;
+    TBufferSender::SetBufferSize(fftSize);
+    SetFFTSize();
+    CalculateWindow();
+    CalculateScalingFactors();
+  }
+  
+  void SetWindowType(EWindowType windowType)
+  {
+    mWindowType = windowType;
+    CalculateWindow();
+  }
+  
+  void SetOutputType(EOutputType outputType)
+  {
+    mOutputType = outputType;
+  }
+  
+  void PrepareDataForUI(ISenderData<MAXNC, TDataPacket>& d) override
+  {
+    auto fftSize = TBufferSender::GetBufferSize();
+
+    for (auto s = 0; s < fftSize; s++)
+    {
+      for (auto stftFrameIdx = 0; stftFrameIdx < mOverlap; stftFrameIdx++)
+      {
+        auto& stftFrame = mSTFTFrames[stftFrameIdx];
+        
+        for (auto ch = 0; ch < MAXNC; ch++)
+        {
+          auto windowedValue = (float) d.vals[ch][s] * mWindow[stftFrame.pos];
+          stftFrame.bins[ch][stftFrame.pos].re = windowedValue;
+          stftFrame.bins[ch][stftFrame.pos].im = 0.0f;
+        }
+        
+        stftFrame.pos++;
+
+        if (stftFrame.pos >= fftSize)
+        {
+          stftFrame.pos = 0;
+          
+          for (auto ch = 0; ch < MAXNC; ch++)
+          {
+            Permute(ch, stftFrameIdx);
+            memcpy(d.vals[ch].data(), mSTFTOutput[ch].data(), fftSize * sizeof(float));
+          }
+        }
+      }
+    }
+  }
+
+  int GetFFTSize() const
+  {
+    return TBufferSender::GetBufferSize();
+  }
+
+  int GetOverlap() const
+  {
+    return mOverlap;
+  }
+    
+private:
+  void SetFFTSize()
+  {
+    if (mSTFTFrames.size() != mOverlap)
+    {
+      mSTFTFrames.resize(mOverlap);
+    }
+    
+    for (auto&& frame : mSTFTFrames)
+    {
+      for (auto ch = 0; ch < MAXNC; ch++)
+      {
+        std::fill(frame.bins[ch].begin(), frame.bins[ch].end(), WDL_FFT_COMPLEX{0.0f, 0.0f});
+      }
+      
+      frame.pos = 0;
+    }
+    
+    for (auto ch = 0; ch < MAXNC; ch++)
+    {
+      std::fill(mSTFTOutput[ch].begin(), mSTFTOutput[ch].end(), 0.0f);
+    }
+  }
+  
+  void CalculateWindow()
+  {
+    const auto fftSize = TBufferSender::GetBufferSize();
+
+    const float M = static_cast<float>(fftSize - 1);
+    
+    switch (mWindowType)
+    {
+      case EWindowType::Hann:
+        for (auto i = 0; i < fftSize; i++) { mWindow[i] = 0.5f * (1.0f - std::cos(PI * 2.0f * i / M)); }
+        break;
+      case EWindowType::BlackmanHarris:
+        for (auto i = 0; i < fftSize; i++) {
+          mWindow[i] = 0.35875 - (0.48829f * std::cos(2.0f * PI * i / M)) +
+                                 (0.14128f * std::cos(4.0f * PI * i / M)) -
+                                 (0.01168f * std::cos(6.0f * PI * i / M));
+        }
+        break;
+      case EWindowType::Hamming:
+        for (auto i = 0; i < fftSize; i++) { mWindow[i] = 0.54f - 0.46f * std::cos(2.0f * PI * i / M); }
+        break;
+      case EWindowType::Flattop:
+        for (auto i = 0; i < fftSize; i++) {
+          mWindow[i] = 0.21557895f - 0.41663158f * std::cos(2.0f * PI * i / M) +
+                                    0.277263158f * std::cos(4.0f * PI * i / M) -
+                                    0.083578947f * std::cos(6.0f * PI * i / M) +
+                                    0.006947368f * std::cos(8.0f * PI * i / M);
+        }
+        break;
+      case EWindowType::Rectangular:
+        std::fill(mWindow.begin(), mWindow.end(), 1.0f);
+        break;
+      default:
+        break;
+    }
+  }
+  
+  void CalculateScalingFactors()
+  {
+    const auto fftSize = TBufferSender::GetBufferSize();
+    const float M = static_cast<float>(fftSize - 1);
+
+    auto scaling = 0.0f;
+    
+    for (auto i = 0; i < fftSize; i++)
+    {
+      auto v = 0.5f * (1.0f - std::cos(2.0f * PI * i / M));
+      scaling += v;
+    }
+    
+    mScalingFactor = scaling * scaling;
+  }
+  
+  void Permute(int ch, int frameIdx)
+  {
+    const auto fftSize = TBufferSender::GetBufferSize();
+    WDL_fft(mSTFTFrames[frameIdx].bins[ch].data(), fftSize, false);
+
+    if (mOutputType == EOutputType::Complex)
+    {
+      auto nBins = fftSize/2;
+      for (auto i = 0; i < nBins; ++i)
+      {
+        int sortIdx = WDL_fft_permute(fftSize, i);
+        mSTFTOutput[ch][i] = mSTFTFrames[frameIdx].bins[ch][sortIdx].re;
+        mSTFTOutput[ch][i + nBins] = mSTFTFrames[frameIdx].bins[ch][sortIdx].im;
+      }
+    }
+    else // magPhase
+    {
+      auto nBins = fftSize/2;
+      for (auto i = 0; i < nBins; ++i)
+      {
+        int sortIdx = WDL_fft_permute(fftSize, i);
+        auto re = mSTFTFrames[frameIdx].bins[ch][sortIdx].re;
+        auto im = mSTFTFrames[frameIdx].bins[ch][sortIdx].im;
+        mSTFTOutput[ch][i] = std::sqrt(2.0f * (re * re + im * im) / mScalingFactor);
+        mSTFTOutput[ch][i + nBins] = std::atan2(im, re);
+      }
+    }
+  }
+  
+  struct STFTFrame
+  {
+    int pos;
+    std::array<std::array<WDL_FFT_COMPLEX, MAX_FFT_SIZE>, MAXNC> bins;
+  };
+  
+  int mOverlap = 2;
+  EWindowType mWindowType;
+  EOutputType mOutputType;
+  std::array<float, MAX_FFT_SIZE> mWindow;
+  std::vector<STFTFrame> mSTFTFrames;
+  std::array<std::array<float, MAX_FFT_SIZE>, MAXNC> mSTFTOutput;
+  float mScalingFactor = 0.0f;
 };
 
 END_IPLUG_NAMESPACE
