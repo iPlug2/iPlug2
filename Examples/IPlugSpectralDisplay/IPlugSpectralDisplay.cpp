7d36fdc38c4be6c64afee046452eae56b09c23be
Oli Larkin <olilarkin@googlemail.com>
2022-07-12 21:12:03 +0200

    DROPME: add IPlugSpectralDisplay Example

---
+++ Examples/IPlugSpectralDisplay/IPlugSpectralDisplay.cpp
@@ -0,0 +1,35 @@
+#include "IPlugSpectralDisplay.h"
+#include "IPlug_include_in_plug_src.h"
+#include "IControls.h"
+
+IPlugSpectralDisplay::IPlugSpectralDisplay(const InstanceInfo& info)
+: Plugin(info, MakeConfig(kNumParams, kNumPresets))
+{
+  GetParam(kGain)->InitDouble("Gain", 0., 0., 100.0, 0.01, "%");
+
+#if IPLUG_EDITOR // http://bit.ly/2S64BDd
+  mMakeGraphicsFunc = [&]() {
+    return MakeGraphics(*this, PLUG_WIDTH, PLUG_HEIGHT, PLUG_FPS, GetScaleForScreen(PLUG_WIDTH, PLUG_HEIGHT));
+  };
+  
+  mLayoutFunc = [&](IGraphics* pGraphics) {
+    pGraphics->AttachCornerResizer(EUIResizerMode::Scale, false);
+    pGraphics->AttachPanelBackground(COLOR_GRAY);
+    pGraphics->LoadFont("Roboto-Regular", ROBOTO_FN);
+    const IRECT b = pGraphics->GetBounds();
+    pGraphics->AttachControl(new IVSpectrumAnalyzerControl<>(b), 0);
+  };
+#endif
+}
+
+void IPlugSpectralDisplay::OnIdle()
+{
+  mSender.TransmitData(*this);
+}
+
+#if IPLUG_DSP
+void IPlugSpectralDisplay::ProcessBlock(sample** inputs, sample** outputs, int nFrames)
+{
+  mSender.ProcessBlock(inputs, nFrames, 0);
+}
+#endif
