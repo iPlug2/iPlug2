07daefff97a36b01fca17710b70046277d354385
Oli Larkin <olilarkin@googlemail.com>
2022-07-12 21:12:03 +0200

    DROPME: Add IPlugSpectralDisplay Example

---
+++ Examples/IPlugSpectralDisplay/IPlugSpectralDisplay.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "IPlug_include_in_plug_hdr.h"
+#include "ISender.h"
+
+const int kNumPresets = 1;
+
+enum EParams
+{
+  kOctaveGain = 0,
+  kNumParams
+};
+
+using namespace iplug;
+using namespace igraphics;
+
+class IPlugSpectralDisplay final : public Plugin
+{
+public:
+  IPlugSpectralDisplay(const InstanceInfo& info);
+
+  void OnReset() override;
+
+#if IPLUG_DSP // http://bit.ly/2S64BDd
+  void OnIdle() override;
+  void ProcessBlock(sample** inputs, sample** outputs, int nFrames) override;
+  ISpectrumSender<2> mSender;
+#endif
+  void OnParentWindowResize(int width, int height) override;
+
+  bool OnMessage(int msgTag, int ctrlTag, int dataSize, const void* pData) override;
+  void OnParamChangeUI(int paramIdx, EParamSource source) override;
+};
---
+++ Examples/IPlugSpectralDisplay/README.md
@@ -0,0 +1,2 @@
+# IPlugSpectralDisplay
+A basic volume control effect plug-in with IGraphics GUI
