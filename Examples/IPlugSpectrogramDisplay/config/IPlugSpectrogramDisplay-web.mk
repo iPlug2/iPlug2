c19b62c61b8f290ebc0ec11ddbe82da14cab524a
Nicolas Dittlo <deadlab.plugins@gmail.com>
2022-07-19 13:41:46 +0200

    test plugin: IPlugSpectrogramDisplay

---
+++ Examples/IPlugSpectrogramDisplay/config/IPlugSpectrogramDisplay-web.mk
@@ -0,0 +1,19 @@
+# IPLUG2_ROOT should point to the top level IPLUG2 folder from the project folder
+# By default, that is three directories up from /Examples/IPlugSpectrogramDisplay/config
+IPLUG2_ROOT = ../../..
+
+include ../../../common-web.mk
+
+SRC += $(PROJECT_ROOT)/IPlugSpectrogramDisplay.cpp
+
+# WAM_SRC +=
+
+# WAM_CFLAGS +=
+
+WEB_CFLAGS += -DIGRAPHICS_NANOVG -DIGRAPHICS_GLES2
+
+WAM_LDFLAGS += -O3 -s EXPORT_NAME="'AudioWorkletGlobalScope.WAM.IPlugSpectrogramDisplay'" -s ASSERTIONS=0
+
+WEB_LDFLAGS += -O3 -s ASSERTIONS=0
+
+WEB_LDFLAGS += $(NANOVG_LDFLAGS)
