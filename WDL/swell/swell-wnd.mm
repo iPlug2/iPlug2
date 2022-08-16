77d4deeb9088e2b71ac1d26588e8fffa94d9ace7
Cockos Inc <nospam@cockos.com>
2022-08-16 12:41:21 -0400

    SWELL listviews: handle double-click message for listview columns that have fastclick mask set -- from c04abd19

--- WDL/swell/swell-wnd.mm
+++ WDL/swell/swell-wnd.mm
@@ -871,7 +871,8 @@ STANDARD_CONTROL_NEEDSDISPLAY_IMPL( m_lbMode ? "SysListView32_LB" : "SysListView
     m_start_item_clickmode=0;
     if (m_start_item >=0 && (m_fastClickMask&(1<<m_start_subitem)))
     {
-      NMLISTVIEW nmlv={{(HWND)self,(UINT_PTR)[self tag], NM_CLICK}, m_start_item, m_start_subitem, 0, 0, 0, {NSPOINT_TO_INTS(pt)}, };
+      int msg = [theEvent clickCount] > 1 ? NM_DBLCLK : NM_CLICK;
+      NMLISTVIEW nmlv={{(HWND)self,(UINT_PTR)[self tag], msg}, m_start_item, m_start_subitem, 0, 0, 0, {NSPOINT_TO_INTS(pt)}, };
       SWELL_ListView_Row *row=m_items->Get(nmlv.iItem);
       if (row)
         nmlv.lParam = row->m_param;
