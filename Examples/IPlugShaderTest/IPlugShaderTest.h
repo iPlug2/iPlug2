bce6700ed655b265cd04bce56ae7388f5ab590a9
Oli Larkin <olilarkin@googlemail.com>
2022-08-26 16:12:42 +0100

    DROPME: Add ShaderTest example

---
+++ Examples/IPlugShaderTest/IPlugShaderTest.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "IPlug_include_in_plug_hdr.h"
+
+const int kNumPresets = 1;
+
+enum EParams
+{
+  kGain = 0,
+  kNumParams
+};
+
+using namespace iplug;
+using namespace igraphics;
+
+class IPlugShaderTest final : public Plugin
+{
+public:
+  IPlugShaderTest(const InstanceInfo& info);
+
+#if IPLUG_DSP // http://bit.ly/2S64BDd
+  void ProcessBlock(sample** inputs, sample** outputs, int nFrames) override;
+#endif
+};
