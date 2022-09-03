36fc6b7ae7aeed4c925484b83e2161f2d13d760b
Alex Harker <ajharker@gmail.com>
2022-09-03 19:17:24 +0100

    CLAP: don't output midi beyond the current frame

--- IPlug/CLAP/IPlugCLAP.cpp
+++ IPlug/CLAP/IPlugCLAP.cpp
@@ -558,7 +558,7 @@ void IPlugCLAP::ProcessOutputEvents(const clap_output_events *outputEvents, int 
 {
   // TODO - ordering of events!!!
   // N.B. Midi events are ordered by the queue
-  // We should not output anything beyond the current frame...
+  // However, sysex messsages are not restricted in this way (is there a good solution?)
   
   SysExData data;
   
@@ -572,6 +572,11 @@ void IPlugCLAP::ProcessOutputEvents(const clap_output_events *outputEvents, int 
     {
       auto msg = mMidiToHost.Peek();
       auto status = msg.mStatus;
+      
+      // Don't move beyond the current frame
+      
+      if (msg.mOffset > nFrames)
+        break;
       
       // Construct output stream
       
