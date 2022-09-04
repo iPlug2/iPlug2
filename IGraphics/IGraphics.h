60dd152f1d0437cf623a8b96ce58cbb585379621
Oli Larkin <olilarkin@googlemail.com>
2022-09-04 21:31:40 +0200

    Improve file dialog dox

--- IGraphics/IGraphics.h
+++ IGraphics/IGraphics.h
@@ -836,19 +836,21 @@ public:
   /** Call this if you modify control tool tips at runtime. \todo explain */
   virtual void UpdateTooltips() = 0;
 
-  /** Pop up a modal platform message box dialog. NOTE: this method will block the main thread
+  /** Pop up a modal platform message box dialog.
    * @param str The text message to display in the dialogue
-   * @param caption The title of the message box window \todo check
+   * @param caption The title of the message box window
    * @param type EMsgBoxType describing the button options available \see EMsgBoxType
-   * @return \todo check */
+   * @param completionHanlder an IMsgBoxCompletionHandlerFunc that will be called when a button is pressed
+   * @return EMsgBoxResult signifying which button was pressed */
   virtual EMsgBoxResult ShowMessageBox(const char* str, const char* caption, EMsgBoxType type, IMsgBoxCompletionHandlerFunc completionHandler = nullptr) = 0;
 
-  /** Create a platform file prompt dialog to choose a file/directory path for opening/saving a file/directory. NOTE: this method will block the main thread
-   * @param fileName Non const WDL_String reference specifying the file name. Set this prior to calling the method for save dialogs, to provide a default file name. For load dialogs, on successful selection of a file this will get set to the file’s name.
+  /** Create a platform file prompt dialog to choose a path for opening/saving a single file. NOTE: this method will block the main thread on macOS, unless you speficy the completionHander, which will be called asynchronously when the dialog button is pressed. On iOS, you must supply a completionHander.
+   * @param fileName Non const WDL_String reference specifying the file name. Set this prior to calling the method for save dialogs, to provide a default file name. For file-open dialogs, on successful selection of a file this will get set to the file’s name.
    * @param path WDL_String reference where the path will be put on success or empty string on failure/user cancelled
-   * @param action Determines whether this is an open dialog or a save dialog
-   * @param extensions A comma separated CString list of file extensions to filter in the dialog (e.g. “.wav, .aif” \todo check */
-  virtual void PromptForFile(WDL_String& fileName, WDL_String& path, EFileAction action = EFileAction::Open, const char* extensions = 0, IFileDialogCompletionHandlerFunc completionHander = nullptr) = 0;
+   * @param action Determines whether this is an file-open dialog or a file-save dialog
+   * @param extensions A space separated CString list of file extensions to filter in the dialog (e.g. “.wav .aif”
+   * @param completionHandler an IFileDialogCompletionHandlerFunc that will be called when a file is selected or the dialog is cancelled */
+  virtual void PromptForFile(WDL_String& fileName, WDL_String& path, EFileAction action = EFileAction::Open, const char* extensions = 0, IFileDialogCompletionHandlerFunc completionHandler = nullptr) = 0;
 
   /** Create a platform file prompt dialog to choose a directory path for opening/saving a directory. NOTE: this method will block the main thread
    * @param dir Non const WDL_String reference specifying the directory path. Set this prior to calling the method for save dialogs, to provide a default path. For load dialogs, on successful selection of a directory this will get set to the full path. */
