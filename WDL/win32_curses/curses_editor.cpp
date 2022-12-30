1f2319fb3d62b9faa33c9ee45cc01a4ade61586d
Cockos Inc <nospam@cockos.com>
2022-12-29 20:55:20 -0500

    slightly more pleasant version of home key cleanup -- from abbc537c

--- WDL/win32_curses/curses_editor.cpp
+++ WDL/win32_curses/curses_editor.cpp
@@ -2418,22 +2418,19 @@ int WDL_CursesEditor::onChar(int c)
   break;
   case KEY_HOME:
     {
-      if (CTRL_KEY_DOWN)
-      {
-        m_curs_x=0;
-        m_curs_y=0;
-      }
-      else
+      const int old_x = m_curs_x;
+      m_curs_x=0;
+      if (!CTRL_KEY_DOWN)
       {
         const WDL_FastString *ln = m_text.Get(m_curs_y);
         if (ln)
         {
           int i = 0;
           while (ln->Get()[i] == ' ' || ln->Get()[i] == '\t') i++;
-          m_curs_x = ln->Get()[i] && i != m_curs_x ? i : 0;
+          if (ln->Get()[i] && i != old_x) m_curs_x = i;
         }
-        else m_curs_x=0;
       }
+      else m_curs_y=0;
 
       if (m_selecting) { setCursor(); m_select_x2=m_curs_x; m_select_y2=m_curs_y; draw(); }
       setCursor();
