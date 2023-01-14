b8b0082dc9a76037ae32f9b9ceddc5e36904f1b8
Oli Larkin <olilarkin@googlemail.com>
2023-01-14 13:33:23 +0100

    Update WebView examples to latest ICoreWebView2 NuGet package

--- Examples/IPlugWebUI/IPlugWebUI.cpp
+++ Examples/IPlugWebUI/IPlugWebUI.cpp
@@ -8,13 +8,14 @@ IPlugWebUI::IPlugWebUI(const InstanceInfo& info)
 
   // Hard-coded paths must be modified!
 #ifdef OS_WIN
-  SetWebViewPaths("C:\\Users\\oli\\Dev\\iPlug2\\Examples\\IPlugWebUI\\packages\\Microsoft.Web.WebView2.1.0.824-prerelease\\runtimes\\win-x64\\native\\WebView2Loader.dll", "C:\\Users\\oli\\Dev\\iPlug2\\Examples\\IPlugWebUI\\");
+  SetWebViewPaths(R"(C:\Users\oli\Dev\iPlug2\Examples\IPlugWebUI\packages\Microsoft.Web.WebView2.1.0.1462.37\build\native\x64\WebView2Loader.dll)",
+    R"(C:\Users\oli\Dev\iPlug2\Examples\IPlugWebUI)");
 #endif
 
 
   mEditorInitFunc = [&]() {
 #ifdef OS_WIN
-    LoadFile("C:\\Users\\oli\\Dev\\iPlug2\\Examples\\IPlugWebUI\\resources\\web\\index.html", nullptr);
+    LoadFile(R"(C:\Users\oli\Dev\iPlug2\Examples\IPlugWebUI\resources\web\index.html)", nullptr);
 #else
     LoadFile("index.html", GetBundleID());
 #endif
