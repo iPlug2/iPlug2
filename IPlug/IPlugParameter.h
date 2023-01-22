iPlug2/iPlug2

commit 318c32f96ea4b4f19fc7af2e48d55da2f6b1dc34

Fix uninitialized variables

IPlug/IPlugParameter.h
@@ -98,7 +98,7 @@ public:
     double NormalizedToValue(double value, const IParam& param) const override;
     double ValueToNormalized(double value, const IParam& param) const override;
   
-    double mShape;
+    double mShape = 0.0;
   };
   
   /** PowCurve parameter shaping */

IGraphics/IGraphicsStructs.h
@@ -1719,8 +1719,8 @@ struct IMouseMod
 /** Used to group mouse coordinates with mouse modifier information */
 struct IMouseInfo
 {
-  float x, y;
-  float dX, dY;
+  float x = 0.0, y = 0.0;
+  float dX = 0.0, dY = 0.0;
   IMouseMod ms;
 };
 

commit 97ee27e1640b870a206df61c347c76872fdc2a45

Fix issue introduced in 318c32f96ea4b4f19fc7af2e48d55da2f6b1dc34

IPlug/IPlugParameter.h
@@ -98,7 +98,7 @@ public:
     double NormalizedToValue(double value, const IParam& param) const override;
     double ValueToNormalized(double value, const IParam& param) const override;
   
-    double mShape = 0.0;
+    double mShape;
   };
   
   /** PowCurve parameter shaping */