31400788980a0b9309438d97246806ccceb6a685
Oli Larkin <olilarkin@googlemail.com>
2023-04-13 14:51:42 +0200

    Fix ASAN error

--- IPlug/APP/IPlugAPP_main.cpp
+++ IPlug/APP/IPlugAPP_main.cpp
@@ -157,7 +157,7 @@ int main(int argc, char *argv[])
   //if invoked with an argument registerauv3 use plug-in kit to explicitly register auv3 app extension (doesn't happen from debugger)
   if(strcmp(argv[2], "registerauv3"))
   {
-    WDL_String appexPath(argv[0]);
+    WDL_String appexPath;
     appexPath.SetFormatted(1024, "pluginkit -a %s%s%s.appex", argv[0], "/../../Plugins/", appexPath.get_filepart());
     if(system(appexPath.Get()) > -1)
       NSLog(@"Registered audiounit app extension\n");
