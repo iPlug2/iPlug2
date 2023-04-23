5cccba01f8602653fdb8b3ebdca8a5d07e6f372e
Oli Larkin <olilarkin@googlemail.com>
2023-04-23 16:21:25 +0200

    IParam: Add display_type to json

--- IPlug/IPlugParameter.cpp
+++ IPlug/IPlugParameter.cpp
@@ -410,6 +410,7 @@ void IParam::GetJSON(WDL_String& json, int idx) const
   json.AppendFormatted(8192, "\"min\":%f, ", GetMin());
   json.AppendFormatted(8192, "\"max\":%f, ", GetMax());
   json.AppendFormatted(8192, "\"default\":%f, ", GetDefault());
+  json.AppendFormatted(8192, "\"display_type\":%i, ", mShape->GetDisplayType());
   json.AppendFormatted(8192, "\"rate\":\"control\"");
   json.AppendFormatted(8192, "}");
 }
