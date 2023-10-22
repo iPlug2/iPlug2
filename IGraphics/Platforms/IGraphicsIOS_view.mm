420625a9c2d738726ba2da726e09cf0b0530feb7
Oli Larkin <olilarkin@googlemail.com>
2023-10-22 17:18:20 +0200

    fixup! IOS make sure that reused cells display correctly enabled

--- IGraphics/Platforms/IGraphicsIOS_view.mm
+++ IGraphics/Platforms/IGraphicsIOS_view.mm
@@ -123,16 +123,7 @@ extern StaticStorage<CoreTextFontDescriptor> sFontDescriptorCache;
   else
     cell.accessoryType = pItem->GetSubmenu() ? UITableViewCellAccessoryDisclosureIndicator : UITableViewCellAccessoryNone;
 
-  if (!pItem->GetEnabled())
-  {
-    cell.userInteractionEnabled = NO;
-    cell.textLabel.enabled = NO;
-  }
-  else
-  {
-    cell.userInteractionEnabled = YES;
-    cell.textLabel.enabled = YES;
-  }
+  cell.textLabel.enabled = cell.userInteractionEnabled = pItem->GetEnabled();
   
   return cell;
 }
