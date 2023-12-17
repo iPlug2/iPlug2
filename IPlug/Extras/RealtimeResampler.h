116ac8573f5adb706ad6ccd8842dbb10cfd8174a
Oli Larkin <olilarkin@googlemail.com>
2023-12-17 23:38:44 +0100

    Rework RealtimeResampler
    Clean up and clarify
    Remove cubic interpolation
    Resampling type is only set at compile time
    Fix added latency (thanks steve!)
    Latency reporting adjusted for null-signal

--- IPlug/Extras/RealtimeResampler.h
+++ IPlug/Extras/RealtimeResampler.h
@@ -14,146 +14,153 @@
 #include <cmath>
 
 #include "IPlugPlatform.h"
+#include "LanczosResampler.h"
 
 #include "heapbuf.h"
 #include "ptrlist.h"
 
-#include "LanczosResampler.h"
-
 BEGIN_IPLUG_NAMESPACE
 
-enum ESRCMode
-{
-  kLinearInterpolation = 0,
-  kCubicInterpolation,
-  kLancsoz,
-  kNumResamplingModes
-};
+/** A multi-channel real-time resampling container that can be used to resample
+ * audio processing to a specified sample rate for the situation where you have
+ * some arbitary DSP code that requires a specific rate
 
-/** A multichannel realtime-resampler that can be used to resample audio processing
- * to a specified sample rate for the situation where you have some arbitary DSP
- * code that requires a specific sample rate.
-
- * Three modes are supported:
+ * Two modes are supported:
  * - Linear interpolation: simple linear interpolation between samples
- * - Cubic interpolation: cubic interpolation between samples
- * - Lanczos: Lanczos resampling uses an approximation of the sinc function to
+ * - Lanczos resampling uses an approximation of the sinc function to
  *   interpolate between samples. This is the highest quality resampling mode.
  * 
  * The Lanczos resampler has a configurable filter size (A) that affects the 
- * latency of the resampler. It can also optionally use SIMD instructions to
+ * latency of the resampler. It can also optionally use SIMD instructions
  * when T==float.
  *
- *
- * @tparam T the sampletype
+ * @tparam T the sampletype float or double
  * @tparam NCHANS the number of channels
  * @tparam A The Lanczos filter size for the LanczosResampler resampler mode
-   A higher value makes the filter closer to an 
-   ideal stop-band that rejects high-frequency content (anti-aliasing), 
-   but at the expense of higher latency
+ * A higher value makes the filter closer to an ideal stop-band that rejects high-frequency
+ * content (anti-aliasing), but at the expense of higher latency
  */
 template<typename T = double, int NCHANS=2, size_t A=12>
 class RealtimeResampler
 {
+  static_assert(std::is_same<T, float>::value || std::is_same<T, double>::value, "T must be float or double");
+
 public:
+  enum class ESRCMode
+  {
+    kLinearInterpolation = 0,
+    kLancsoz,
+    kNumResamplingModes
+  };
+
   using BlockProcessFunc = std::function<void(T**, T**, int)>;
   using LanczosResampler = LanczosResampler<T, NCHANS, A>;
 
-  RealtimeResampler(double renderingSampleRate, ESRCMode mode = ESRCMode::kLinearInterpolation)
+  /** Constructor
+   * @param innerSampleRate The sample rate that the provided DSP block will process at
+   * @param mode The sample rate conversion mode
+   */
+  RealtimeResampler(double innerSampleRate, ESRCMode mode = ESRCMode::kLancsoz)
   : mResamplingMode(mode)
-  , mRenderingSampleRate(renderingSampleRate)
+  , mInnerSampleRate(innerSampleRate)
   {
   }
   
   RealtimeResampler(const RealtimeResampler&) = delete;
   RealtimeResampler& operator=(const RealtimeResampler&) = delete;
 
-  void SetResamplingMode(ESRCMode mode)
+  /** Reset the underlying DSP (when the samplerate or max expected block size changes)
+   *  @param inputSampleRate: The external sample rate interacting with this object.
+   *  @param maxBlockSize: The largest block size expected to be passed via nFrames in processBlock
+   */
+  void Reset(double inputSampleRate, int maxBlockSize = DEFAULT_BLOCK_SIZE)
   {
-    mResamplingMode = mode;
-    Reset(mInputSampleRate);
-  }
-  
-  void Reset(double inputSampleRate, int blockSize = DEFAULT_BLOCK_SIZE)
-  {
-    if (mInputSampleRate == inputSampleRate 
-        && mMaxBlockSize == (2 * blockSize))
-    {
-      ClearBuffers();
-      return;
-    }
+    mOuterSampleRate = inputSampleRate;
+    mInRatio = mOuterSampleRate / mInnerSampleRate;
+    mOutRatio = mInnerSampleRate / mOuterSampleRate;
+    mMaxOuterLength = maxBlockSize;
+    mMaxInnerLength = CalculateMaxInnerLength(mMaxOuterLength);
 
-    mMaxBlockSize = blockSize * 2;
-    mInputSampleRate = inputSampleRate;
-    mUpRatio = mInputSampleRate / mRenderingSampleRate;
-    mDownRatio = mRenderingSampleRate / mInputSampleRate;
-    mResampledData.Resize(mMaxBlockSize * NCHANS);
-    mScratchPtrs.Empty();
-
+    mInputData.Resize(mMaxInnerLength * NCHANS);
+    mOutputData.Resize(mMaxInnerLength * NCHANS);
+    mInputPtrs.Empty();
+    mOutputPtrs.Empty();
+    
     for (auto chan=0; chan<NCHANS; chan++)
     {
-      mScratchPtrs.Add(mResampledData.Get() + (chan * mMaxBlockSize));
-    }
-
-    if (mResamplingMode == ESRCMode::kLancsoz)
-    {
-      mResamplerUp = std::make_unique<LanczosResampler>(mInputSampleRate, mRenderingSampleRate);
-      mResamplerDown = std::make_unique<LanczosResampler>(mRenderingSampleRate, mInputSampleRate);
-
-      /* Prepopulate the upsampler with silence so it can run ahead */
-      mLatency = int(mResamplerUp->GetNumSamplesRequiredFor(1) * 2);
-      mResamplerUp->PushBlock(mScratchPtrs.GetList(), mLatency);
-    }
-    else
-    {
-      mResamplerUp = nullptr;
-      mResamplerDown = nullptr;
-      mLatency = 0;
+      mInputPtrs.Add(mInputData.Get() + (chan * mMaxInnerLength));
+      mOutputPtrs.Add(mOutputData.Get() + (chan * mMaxInnerLength));
     }
 
     ClearBuffers();
+  
+    if (mResamplingMode == ESRCMode::kLancsoz)
+    {
+      mInResampler = std::make_unique<LanczosResampler>(mOuterSampleRate, mInnerSampleRate);
+      mOutResampler = std::make_unique<LanczosResampler>(mInnerSampleRate, mOuterSampleRate);
+      
+      // Warm up the resamplers with enough silence that the first real buffer can yield the required number of output samples.
+      const auto outSamplesRequired = mOutResampler->GetNumSamplesRequiredFor(1);
+      const auto inSamplesRequired = mInResampler->GetNumSamplesRequiredFor(outSamplesRequired);
+      mInResampler->PushBlock(mInputPtrs.GetList(), inSamplesRequired);
+      const auto populated = mInResampler->PopBlock(mInputPtrs.GetList(), outSamplesRequired);
+      assert(populated >= outSamplesRequired && "Didn't get enough samples required for warm up!");
+      mOutResampler->PushBlock(mOutputPtrs.GetList(), populated);
+      
+      // magic number that we seem to need to align when compensating for latency
+      constexpr auto addedLatency = 2;
+      mLatency = static_cast<int>(inSamplesRequired + addedLatency);
+    }
+    else
+    {
+      mLatency = 0;
+    }
   }
 
-  /** Resample an input block with a per-block function (up sample input -> process with function -> down sample)
+  /** Resample an input block with a per-block function (resample input -> process with function -> resample back to external sample rate)
    * @param inputs Two-dimensional array containing the non-interleaved input buffers of audio samples for all channels
    * @param outputs Two-dimensional array for audio output (non-interleaved).
    * @param nFrames The block size for this block: number of samples per channel.
-   * @param func The function that processes the audio sample at the higher sampling rate. NOTE: std::function can call malloc if you pass in captures */
+   * @param func The function that processes the audio sample at the higher sampling rate. 
+   * NOTE: std::function can call malloc if you pass in captures */
   void ProcessBlock(T** inputs, T** outputs, int nFrames, BlockProcessFunc func)
   {
-    switch (mResamplingMode) 
+    if (mInnerSampleRate == mOuterSampleRate) // nothing to do!
+    {
+      func(inputs, outputs, nFrames);
+      return;
+    }
+
+    switch (mResamplingMode)
     {
       case ESRCMode::kLinearInterpolation:
       {
-        const auto nNewFrames = LinearInterpolate(inputs, mScratchPtrs.GetList(), nFrames, mUpRatio, mMaxBlockSize);
-        func(mScratchPtrs.GetList(), mScratchPtrs.GetList(), nNewFrames);
-        LinearInterpolate(mScratchPtrs.GetList(), outputs, nNewFrames, mDownRatio, nFrames);
-        break;
-      }
-      case ESRCMode::kCubicInterpolation:
-      {
-        const auto nNewFrames = CubicInterpolate(inputs, mScratchPtrs.GetList(), nFrames, mUpRatio, mMaxBlockSize);
-        func(mScratchPtrs.GetList(), mScratchPtrs.GetList(), nNewFrames);
-        CubicInterpolate(mScratchPtrs.GetList(), outputs, nNewFrames, mDownRatio, nFrames);
+        const auto nNewFrames = LinearInterpolate(inputs, mInputPtrs.GetList(), nFrames, mInRatio, mMaxInnerLength);
+        func(mInputPtrs.GetList(), mOutputPtrs.GetList(), nNewFrames);
+        LinearInterpolate(mOutputPtrs.GetList(), outputs, nNewFrames, mOutRatio, nFrames);
         break;
       }
       case ESRCMode::kLancsoz:
       {
-        mResamplerUp->PushBlock(inputs, nFrames);
-        
-        const auto outputLen = static_cast<int>(std::ceil(static_cast<double>(nFrames) / mUpRatio));
+        mInResampler->PushBlock(inputs, nFrames);
+        const auto maxInnerLength = CalculateMaxInnerLength(nFrames);
 
-        while (mResamplerUp->GetNumSamplesRequiredFor(outputLen) == 0)
+        while (mInResampler->GetNumSamplesRequiredFor(1) == 0) // i.e. there's signal still available to pop
         {
-          mResamplerUp->PopBlock(mScratchPtrs.GetList(), outputLen);
-          func(mScratchPtrs.GetList(), mScratchPtrs.GetList(), outputLen);
-          
-          mResamplerDown->PushBlock(mScratchPtrs.GetList(), outputLen);
+          const auto populated = mInResampler->PopBlock(mInputPtrs.GetList(), maxInnerLength);
+          assert(populated <= maxInnerLength && "Received more samples than the encapsulated DSP is able to handle!");
+          func(mInputPtrs.GetList(), mOutputPtrs.GetList(), static_cast<int>(populated));
+          mOutResampler->PushBlock(mOutputPtrs.GetList(), populated);
         }
         
-        mResamplerDown->PopBlock(outputs, nFrames);
-        mResamplerUp->RenormalizePhases();
-        mResamplerDown->RenormalizePhases();
+#ifdef _DEBUG
+        const auto populated =
+#endif
+        mOutResampler->PopBlock(outputs, nFrames);
+        assert(populated >= nFrames && "Did not yield enough samples to provide the required output buffer!");
+        
+        mInResampler->RenormalizePhases();
+        mOutResampler->RenormalizePhases();
         break;
       }
       default:
@@ -161,93 +168,63 @@ public:
     }
   }
   
+  /** Get the latency of the resampling, not including any latency of the encapsulated DSP */
   int GetLatency() const { return mLatency; }
 
 private:
-  static inline int LinearInterpolate(T** inputs, T** outputs, int inputLen, double ratio, int maxOutputLen)
+  /** Interpolate the signal across the block with a specific resampling ratio */
+  static inline int LinearInterpolate(T** inputs, T** outputs, int inputLength, double ratio, int maxOutputLength)
   {
-    const auto outputLen =
-      std::min(static_cast<int>(std::ceil(static_cast<double>(inputLen) / ratio)), maxOutputLen);
+    const auto outputLength =
+      std::min(static_cast<int>(std::ceil(static_cast<double>(inputLength) / ratio)), maxOutputLength);
 
-    for (auto writePos = 0; writePos < outputLen; writePos++)
+    for (auto writePos=0; writePos<outputLength; writePos++)
     {
       const auto readPos = ratio * static_cast<double>(writePos);
       const auto readPostionTrunc = std::floor(readPos);
       const auto readPosInt = static_cast<int>(readPostionTrunc);
 
-      if (readPosInt < inputLen)
+      if (readPosInt < inputLength)
       {
         const auto y = readPos - readPostionTrunc;
 
         for (auto chan=0; chan<NCHANS; chan++)
         {
           const auto x0 = inputs[chan][readPosInt];
-          const auto x1 = ((readPosInt + 1) < inputLen) ? inputs[chan][readPosInt + 1] : inputs[chan][readPosInt-1];
+          const auto x1 = ((readPosInt + 1) < inputLength) ? inputs[chan][readPosInt + 1] 
+                                                           : inputs[chan][readPosInt - 1];
           outputs[chan][writePos] = (1.0 - y) * x0 + y * x1;
         }
       }
     }
 
-    return outputLen;
+    return outputLength;
   }
-  
-  static inline int CubicInterpolate(T** inputs, T** outputs, int inputLen, double ratio, int maxOutputLen)
-  {
-    const auto outputLen =
-      std::min(static_cast<int>(std::ceil(static_cast<double>(inputLen) / ratio)), maxOutputLen);
 
-    for (auto writePos = 0; writePos < outputLen; writePos++)
-    {
-      const auto readPos = ratio * static_cast<double>(writePos);
-      const auto readPostionTrunc = std::floor(readPos);
-      const auto readPosInt = static_cast<int>(readPostionTrunc);
-
-      if (readPosInt < inputLen)
-      {
-        const auto y = readPos - readPostionTrunc;
-        
-        for (auto chan=0; chan<NCHANS; chan++)
-        {
-          const auto xm1 = ((readPosInt - 1) > 0) ? inputs[chan][readPosInt - 1] : 0.0f;
-          const auto x0 = ((readPosInt) < inputLen) ? inputs[chan][readPosInt] : inputs[chan][readPosInt-1];
-          const auto x1 = ((readPosInt + 1) < inputLen) ? inputs[chan][readPosInt + 1] : inputs[chan][readPosInt-1];
-          const auto x2 = ((readPosInt + 2) < inputLen) ? inputs[chan][readPosInt + 2] : inputs[chan][readPosInt-1];
-          
-          const auto  c = (x1 - xm1) * 0.5;
-          const auto  v = x0 - x1;
-          const auto  w = c + v;
-          const auto  a = w + v + (x2 - x0) * 0.5;
-          const auto  b = w + a;
-          
-          outputs[chan][writePos] = ((((a * y) -b) * y + c) * y + x0);
-        }
-      }
-    }
-    
-    return outputLen;
-  }
-  
+  /** Zero the memory in the scratch buffers */
   void ClearBuffers()
   {
-    memset(mResampledData.Get(), 0.0f, mMaxBlockSize * NCHANS * sizeof(T));
-    
-    if (mResamplingMode == ESRCMode::kLancsoz)
-    {
-      mResamplerUp->ClearBuffer();
-      mResamplerDown->ClearBuffer();
-    }
+    const auto nBytes = mMaxInnerLength * NCHANS * sizeof(T);
+    memset(mInputData.Get(), 0, nBytes);
+    memset(mOutputData.Get(), 0, nBytes);
   }
-
-  WDL_TypedBuf<T> mResampledData;
-  WDL_PtrList<T> mScratchPtrs;
-  double mUpRatio = 0.0, mDownRatio = 0.0;
-  double mInputSampleRate = 0.0;
-  int mMaxBlockSize = 0;
-  int mLatency = 0;
-  const double mRenderingSampleRate;
-  ESRCMode mResamplingMode;
   
-  std::unique_ptr<LanczosResampler> mResamplerUp, mResamplerDown;
+  /** Calculate the signal length in samples required for the encapsulated processing */
+  int CalculateMaxInnerLength(const int outerLength) const
+  {
+    return static_cast<int>(std::ceil(double(outerLength) / mInRatio));
+  }
+  
+  WDL_TypedBuf<T> mInputData, mOutputData;
+  WDL_PtrList<T> mInputPtrs, mOutputPtrs;
+  double mInRatio = 0.0, mOutRatio = 0.0;
+  double mOuterSampleRate = DEFAULT_SAMPLE_RATE;
+  const double mInnerSampleRate;
+  int mMaxOuterLength = 0; // The maximum outer block size expected
+  int mMaxInnerLength = 0; // The computed maximum inner block size
+  int mLatency = 0;
+  const ESRCMode mResamplingMode;
+  std::unique_ptr<LanczosResampler> mInResampler, mOutResampler;
 };
 
 END_IPLUG_NAMESPACE
