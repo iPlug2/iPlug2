e7851ebfc8a847459a5d075b112040339f6f0060
Oli Larkin <olilarkin@googlemail.com>
2024-01-04 00:54:34 +0100

    WIP

--- Examples/IPlugEffect/IPlugEffect.cpp
+++ Examples/IPlugEffect/IPlugEffect.cpp
@@ -2,6 +2,62 @@
 #include "IPlug_include_in_plug_src.h"
 #include "IControls.h"
 
+class ScrollerControl : public IContainerBase
+{
+public:
+  ScrollerControl(const IRECT& bounds)
+  : IContainerBase(bounds)
+  {}
+  
+  void Draw(IGraphics& g) override
+  {
+    g.FillRect(COLOR_LIGHT_GRAY, mRECT);
+  }
+  
+  void OnAttached() override
+  {
+    auto sliderStyle = DEFAULT_STYLE.WithColor(kX1, COLOR_TRANSPARENT).WithDrawShadows(false).WithRoundness(1);
+
+    AddChildControl(new IVSliderControl(IRECT(), [this](IControl* pCaller) {
+      mYOffset = pCaller->GetValue();
+      this->GetChild(1)->SetTargetAndDrawRECTs(mRECT.GetReducedFromRight(20));
+      SetDirty(false);
+    }, "", sliderStyle, false, EDirection::Vertical, DEFAULT_GEARING, 8.f, 8.f, true));
+    
+    auto buttonStyle = DEFAULT_STYLE.WithDrawFrame(false).WithColor(kSH, COLOR_BLACK).WithShadowOffset(1).WithLabelText({25.f, EVAlign::Middle});
+
+    AddChildControl(new IContainerBase(IRECT(), [&](IContainerBase* pCaller, const IRECT& r){
+      for (auto i = 0; i < (this->mNumRows*2); i++)
+      {
+        pCaller->AddChildControl(new IVButtonControl(IRECT(), SplashClickActionFunc, "Hello World", buttonStyle, true))->SetIgnoreMouse(true);
+      }
+    }, [&](IContainerBase* pCaller, const IRECT& r){
+      for (auto i = 0; i < (this->mNumRows*2); i++)
+      {
+        pCaller->GetChild(i)->SetTargetAndDrawRECTs(r.SubRectVertical(mNumRows, i).GetVShifted(-r.H() + (mYOffset * r.H())));
+      }
+    }));
+    
+    OnResize();
+  }
+
+  void OnResize() override
+  {
+    if (NChildren())
+    {
+      GetChild(0)->SetTargetAndDrawRECTs(mRECT);
+      GetChild(1)->SetTargetAndDrawRECTs(mRECT.GetReducedFromRight(20));
+    }
+  }
+  
+private:
+  float mYOffset = 0.0;
+  const int mNumRows = 10;
+  //void OnMouseDown(float x, float y, const IMouseMod& mod) override {}
+  //void OnMouseUp(float x, float y, const IMouseMod& mod) override {}
+  //void OnMouseDrag(float x, float y, float dX, float dY, const IMouseMod& mod) override {}
+};
+
 IPlugEffect::IPlugEffect(const InstanceInfo& info)
 : iplug::Plugin(info, MakeConfig(kNumParams, kNumPresets))
 {
@@ -17,8 +73,7 @@ IPlugEffect::IPlugEffect(const InstanceInfo& info)
     pGraphics->AttachPanelBackground(COLOR_GRAY);
     pGraphics->LoadFont("Roboto-Regular", ROBOTO_FN);
     const IRECT b = pGraphics->GetBounds();
-    pGraphics->AttachControl(new ITextControl(b.GetMidVPadded(50), "Hello iPlug 2!", IText(50)));
-    pGraphics->AttachControl(new IVKnobControl(b.GetCentredInside(100).GetVShifted(-100), kGain));
+    pGraphics->AttachControl(new ScrollerControl(b.GetCentredInside(300)));
   };
 #endif
 }
