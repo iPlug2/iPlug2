2797fb295cedccb514d23b3ecb08306eaaea1ac4
Oli Larkin <olilarkin@googlemail.com>
2024-06-16 17:25:03 +0200

    [WIP] Breaking: adapt IGraphics::ShowMessageBox()
    for prompting for arbitary text entry using an alert

--- IGraphics/Platforms/IGraphicsWin.h
+++ IGraphics/Platforms/IGraphicsWin.h
@@ -51,7 +51,7 @@ public:
   
   void GetMouseLocation(float& x, float&y) const override;
 
-  EMsgBoxResult ShowMessageBox(const char* str, const char* title, EMsgBoxType type, IMsgBoxCompletionHandlerFunc completionHandler) override;
+  EMsgBoxResult ShowMessageBox(const char* str, const char* title, EMsgBoxType type, IMsgBoxCompletionHandlerFunc completionHandler, bool withTextEntry = false) override;
 
   void* OpenWindow(void* pParent) override;
   void CloseWindow() override;
