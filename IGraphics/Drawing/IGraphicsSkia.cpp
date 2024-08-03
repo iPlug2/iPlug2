617f9694a93a46f10b2eee7583b3a659347cab0c
Oli Larkin <olilarkin@googlemail.com>
2024-08-03 22:13:51 +0200

    [DROP] Squash of skia-multiline-text
    define IGRAPHICS_NO_SKIA_SKPARAGRAPH if you don't need it
    
    (cherry picked from commit 316968f1012e5236ea1cbf50175526b3dd30b964)

--- IGraphics/Drawing/IGraphicsSkia.cpp
+++ IGraphics/Drawing/IGraphicsSkia.cpp
@@ -13,6 +13,12 @@
 #include "SkTypeface.h"
 #include "SkVertices.h"
 #include "SkSwizzle.h"
+#if !defined IGRAPHICS_NO_SKIA_SKPARAGRAPH
+#include "modules/skparagraph/include/Paragraph.h"
+#include "modules/skparagraph/include/ParagraphBuilder.h"
+#include "modules/skparagraph/include/ParagraphStyle.h"
+#include "modules/skparagraph/include/TextStyle.h"
+#endif
 #pragma warning( pop )
 
 #if defined OS_MAC || defined OS_IOS
@@ -952,6 +958,68 @@ void IGraphicsSkia::DrawFastDropShadow(const IRECT& innerBounds, const IRECT& ou
   
   paint.setMaskFilter(SkMaskFilter::MakeBlur(kSolid_SkBlurStyle, blur * 0.5)); // 0.5 seems to match nanovg
   mCanvas->drawRoundRect(r, roundness, roundness, paint);
+}
+
+void IGraphicsSkia::DrawMultiLineText(const IText& text, const char* str, IRECT& bounds, const IBlend* pBlend)
+{
+#if !defined IGRAPHICS_NO_SKIA_SKPARAGRAPH
+  using namespace skia::textlayout;
+  
+  auto fontCollection = sk_make_sp<FontCollection>();
+  fontCollection->setDefaultFontManager(SkFontMgr::RefDefault());
+
+  auto ConvertTextAlign = [](EAlign align) {
+    switch (align)
+    {
+      case EAlign::Near: return TextAlign::kLeft;
+      case EAlign::Center: return TextAlign::kCenter;
+      case EAlign::Far: return TextAlign::kRight;
+      default: return TextAlign::kLeft;
+    }
+  };
+
+  ParagraphStyle paragraphStyle;
+  paragraphStyle.setTextAlign(ConvertTextAlign(text.mAlign));
+  
+  auto pBuilder = ParagraphBuilder::make(paragraphStyle, fontCollection);
+  
+  TextStyle textStyle;
+  textStyle.setColor(SkiaColor(text.mFGColor, pBlend));
+  textStyle.setFontFamilies({SkString(text.mFont)});
+  textStyle.setFontSize(text.mSize);
+  textStyle.setFontStyle(SkFontStyle(SkFontStyle::kNormal_Weight,
+                                     SkFontStyle::kNormal_Width,
+                                     SkFontStyle::kUpright_Slant));
+  
+  pBuilder->pushStyle(textStyle);
+  pBuilder->addText(str);
+  pBuilder->pop();
+  
+  auto pParagraph = pBuilder->Build();
+  
+  pParagraph->layout(bounds.W());
+  
+  float yOffset = 0;
+  switch (text.mVAlign)
+  {
+    case EVAlign::Top:
+      yOffset = 0;
+      break;
+    case EVAlign::Middle:
+      yOffset = (bounds.H() - pParagraph->getHeight()) / 2;
+      break;
+    case EVAlign::Bottom:
+      yOffset = bounds.H() - pParagraph->getHeight();
+      break;
+  }
+    
+  mCanvas->save();
+  mCanvas->translate(bounds.L, bounds.T + yOffset);
+  pParagraph->paint(mCanvas, 0, 0);
+  mCanvas->restore();
+#else
+  DrawText(text, str, bounds, pBlend);
+#endif
 }
 
 const char* IGraphicsSkia::GetDrawingAPIStr()
