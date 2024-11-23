iPlug2/iPlug2

commit b145123193790eaf50a7357780832824ce8f345f

Refactor/Update IPlugWebview
This is a large update which reworks the IPlugWebView class using the Pimpl idiom. It also renames methods and adds functionality and splits out the WkWebView implementation classes into multiple files.

New functionality:

- File downloading
- Custom URL scheme serving

IPlug/Extras/WebView/IPlugWKWebViewDelegate.h
@@ -1,0 +1,46 @@
+#pragma once
+
+#import <WebKit/WebKit.h>
+
+namespace iplug {
+class IWebView;
+}
+
+@interface IPLUG_WKWEBVIEW_DELEGATE : NSObject <WKNavigationDelegate, WKDownloadDelegate>
+{
+  iplug::IWebView* _Nonnull mIWebView;
+  NSURL* _Nullable downloadDestinationURL;
+}
+
+- (id _Nonnull)initWithIWebView:(iplug::IWebView* _Nonnull)webView;
+
+- (void)dealloc;
+
+- (void)webView:(WKWebView * _Nonnull)webView 
+decidePolicyForNavigationAction:(WKNavigationAction * _Nonnull)navigationAction 
+decisionHandler:(void (^ _Nonnull)(WKNavigationActionPolicy))decisionHandler;
+
+- (void)webView:(WKWebView * _Nonnull)webView 
+decidePolicyForNavigationResponse:(WKNavigationResponse * _Nonnull)navigationResponse 
+decisionHandler:(void (^ _Nonnull)(WKNavigationResponsePolicy))decisionHandler;
+
+#pragma mark WKDownloadDelegate
+
+- (void)webView:(WKWebView * _Nonnull)webView 
+navigationResponse:(WKNavigationResponse * _Nonnull)navigationResponse 
+didBecomeDownload:(WKDownload * _Nonnull)download API_AVAILABLE(macos(11.3), ios(14.5));
+
+- (void)download:(WKDownload * _Nonnull)download 
+decideDestinationUsingResponse:(NSURLResponse * _Nonnull)response 
+suggestedFilename:(NSString * _Nonnull)filename 
+completionHandler:(void (^ _Nonnull)(NSURL * _Nullable))completionHandler API_AVAILABLE(macos(11.3), ios(14.5));
+
+- (void)download:(WKDownload * _Nonnull)download 
+didReceiveData:(int64_t)length API_AVAILABLE(macos(11.3), ios(14.5));
+
+- (void)download:(WKDownload * _Nonnull)download 
+didFailWithError:(NSError * _Nonnull)error API_AVAILABLE(macos(11.3), ios(14.5));
+
+- (void)downloadDidFinish:(WKDownload * _Nonnull)download API_AVAILABLE(macos(11.3), ios(14.5));
+
+@end

IPlug/Extras/WebView/IPlugWKWebViewScriptMessageHandler.h
@@ -1,0 +1,37 @@
+#pragma once
+
+#import <WebKit/WebKit.h>
+
+namespace iplug {
+class IWebView;
+}
+
+@class IPLUG_WKWEBVIEW;
+
+@interface IPLUG_WKSCRIPTMESSAGEHANDLER : NSObject <WKScriptMessageHandler, WKURLSchemeHandler>
+{
+  iplug::IWebView* _Nonnull mIWebView;
+  NSURL* _Nullable downloadDestinationURL;
+}
+
+- (void)dealloc;
+
+- (id _Nonnull)initWithIWebView:(iplug::IWebView* _Nonnull)webView;
+
+- (void)userContentController:(nonnull WKUserContentController*)userContentController 
+      didReceiveScriptMessage:(nonnull WKScriptMessage*)message;
+
+#pragma mark - WKURLSchemeHandler
+
+- (void)webView:(IPLUG_WKWEBVIEW* _Nonnull)webView didFinishNavigation:(WKNavigation* _Nullable)navigation;
+
+- (NSURL* _Nullable)changeURLScheme:(NSURL* _Nonnull)url toScheme:(NSString* _Nonnull)newScheme;
+
+- (void)webView:(nonnull WKWebView *)webView 
+startURLSchemeTask:(nonnull id<WKURLSchemeTask>)urlSchemeTask API_AVAILABLE(macos(10.13));
+
+- (void)webView:(nonnull WKWebView *)webView 
+stopURLSchemeTask:(nonnull id<WKURLSchemeTask>)urlSchemeTask API_AVAILABLE(macos(10.13));
+
+@end
+

IGraphics/Controls/IWebViewControl.h
@@ -64,7 +64,7 @@ public:
   void OnAttached() override
   {
     IGraphics* pGraphics = GetUI();
-    mPlatformView = OpenWebView(pGraphics->GetWindow(), mRECT.L, mRECT.T, mRECT.W(), mRECT.H(), pGraphics->GetDrawScale(), mEnableDevTools);
+    mPlatformView = OpenWebView(pGraphics->GetWindow(), mRECT.L, mRECT.T, mRECT.W(), mRECT.H(), pGraphics->GetDrawScale());
     pGraphics->AttachPlatformView(mRECT, mPlatformView);
     EnableScroll(mEnableScroll);
   }

IPlug/Extras/WebView/IPlugWKWebView.h
@@ -1,0 +1,19 @@
+#pragma once
+
+#import <WebKit/WebKit.h>
+
+@interface IPLUG_WKWEBVIEW : WKWebView
+{
+  bool mEnableInteraction;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame configuration:(WKWebViewConfiguration *)configuration;
+- (void)setEnableInteraction:(bool)enable;
+
+#ifdef OS_MAC
+- (NSView *)hitTest:(NSPoint)point;
+- (void)willOpenMenu:(NSMenu *)menu withEvent:(NSEvent *)event;
+#endif
+
+@end
+

IPlug/Extras/WebView/IPlugWKWebView.mm
@@ -1,0 +1,85 @@
+#import "IPlugWKWebView.h"
+
+#include "IPlugPlatform.h"
+
+@implementation IPLUG_WKWEBVIEW
+
+- (instancetype)initWithFrame:(CGRect)frame configuration:(WKWebViewConfiguration *)configuration
+{
+  self = [super initWithFrame:frame configuration:configuration];
+  
+  if (self)
+  {
+    mEnableInteraction = true;
+  }
+  return self;
+}
+
+#ifdef OS_MAC
+- (NSView *)hitTest:(NSPoint)point
+{
+  if (!mEnableInteraction)
+  {
+    return nil;
+  }
+  else
+    return [super hitTest:point];
+}
+
+- (void)willOpenMenu:(NSMenu *)menu withEvent:(NSEvent *)event
+{
+  [super willOpenMenu:menu withEvent:event];
+  
+  NSArray<NSString *> *WKStrings = @[
+   @"WKMenuItemIdentifierCopy",
+   @"WKMenuItemIdentifierCopyImage",
+   @"WKMenuItemIdentifierCopyLink",
+   @"WKMenuItemIdentifierDownloadImage",
+   @"WKMenuItemIdentifierDownloadLinkedFile",
+   @"WKMenuItemIdentifierGoBack",
+   @"WKMenuItemIdentifierGoForward",
+//   @"WKMenuItemIdentifierInspectElement",
+   @"WKMenuItemIdentifierLookUp",
+   @"WKMenuItemIdentifierOpenFrameInNewWindow",
+   @"WKMenuItemIdentifierOpenImageInNewWindow",
+   @"WKMenuItemIdentifierOpenLink",
+   @"WKMenuItemIdentifierOpenLinkInNewWindow",
+   @"WKMenuItemIdentifierPaste",
+//   @"WKMenuItemIdentifierReload",
+   @"WKMenuItemIdentifierSearchWeb",
+   @"WKMenuItemIdentifierShowHideMediaControls",
+   @"WKMenuItemIdentifierToggleFullScreen",
+   @"WKMenuItemIdentifierTranslate",
+   @"WKMenuItemIdentifierShareMenu",
+   @"WKMenuItemIdentifierSpeechMenu"
+  ];
+  
+  for (NSInteger itemIndex = 0; itemIndex < menu.itemArray.count; itemIndex++)
+  {
+    if ([WKStrings containsObject:menu.itemArray[itemIndex].identifier])
+    {
+      [menu removeItemAtIndex:itemIndex];
+    }
+  }
+}
+
+#endif
+
+- (void)setEnableInteraction:(bool)enable
+{
+  mEnableInteraction = enable;
+  
+#ifdef OS_MAC
+  if (!mEnableInteraction)
+  {
+    for (NSTrackingArea* trackingArea in self.trackingAreas)
+    {
+      [self removeTrackingArea:trackingArea];
+    }
+  }
+#else
+  self.userInteractionEnabled = mEnableInteraction;
+#endif
+}
+
+@end

IPlug/Extras/WebView/IPlugWKWebViewDelegate.mm
@@ -1,0 +1,111 @@
+#import "IPlugWKWebViewDelegate.h"
+
+#include "IPlugWebView.h"
+
+#if !__has_feature(objc_arc)
+#error This file must be compiled with Arc. Use -fobjc-arc flag
+#endif
+
+using namespace iplug;
+
+@implementation IPLUG_WKWEBVIEW_DELEGATE
+
+- (id _Nonnull)initWithIWebView:(iplug::IWebView* _Nonnull)webView;
+{
+  self = [super init];
+  if (self)
+  {
+    mIWebView = webView;
+  }
+  return self;
+}
+
+- (void)dealloc
+{
+  mIWebView = nullptr;
+}
+
+- (void)webView:(WKWebView * _Nonnull)webView 
+navigationResponse:(WKNavigationResponse * _Nonnull)navigationResponse 
+didBecomeDownload:(WKDownload * _Nonnull)download API_AVAILABLE(macos(11.3), ios(14.5))
+{
+  download.delegate = self;
+}
+
+- (void)webView:(WKWebView * _Nonnull)webView 
+decidePolicyForNavigationAction:(WKNavigationAction * _Nonnull)navigationAction 
+decisionHandler:(void (^ _Nonnull)(WKNavigationActionPolicy))decisionHandler
+{
+  bool allow = mIWebView->OnCanNavigateToURL([[[[navigationAction request] URL] absoluteString] UTF8String]);
+  
+  if (allow)
+  {
+    decisionHandler(WKNavigationActionPolicyAllow);
+  }
+  else
+  {
+    decisionHandler(WKNavigationActionPolicyCancel);
+  }
+}
+
+- (void)webView:(WKWebView * _Nonnull)webView 
+decidePolicyForNavigationResponse:(WKNavigationResponse * _Nonnull)navigationResponse 
+decisionHandler:(void (^ _Nonnull)(WKNavigationResponsePolicy))decisionHandler
+{
+  bool allow = mIWebView->OnCanNavigateToURL([[[[navigationResponse response] URL] absoluteString] UTF8String]);
+  
+  if (!allow)
+  {
+    decisionHandler(WKNavigationResponsePolicyCancel);
+  }
+  
+  if (@available(macOS 11.3, iOS 14.5, *))
+  {
+    bool allowMimeType = mIWebView->OnCanDownloadMIMEType([navigationResponse.response.MIMEType UTF8String]);
+
+    if (allowMimeType)
+    {
+      decisionHandler(WKNavigationResponsePolicyDownload);
+    }
+    else
+    {
+      decisionHandler(WKNavigationResponsePolicyAllow);
+    }
+  }
+  else
+  {
+    decisionHandler(WKNavigationResponsePolicyAllow);
+  }
+}
+
+- (void)download:(WKDownload * _Nonnull)download 
+decideDestinationUsingResponse:(NSURLResponse * _Nonnull)response 
+suggestedFilename:(NSString * _Nonnull)filename 
+completionHandler:(void (^ _Nonnull)(NSURL * _Nullable))completionHandler API_AVAILABLE(macos(11.3), ios(14.5))
+{
+  WDL_String localPath;
+  mIWebView->OnGetLocalDownloadPathForFile([filename UTF8String], localPath);
+  downloadDestinationURL = [NSURL fileURLWithPath:[NSString stringWithUTF8String:localPath.Get()]];
+  completionHandler(downloadDestinationURL);
+}
+
+- (void)download:(WKDownload * _Nonnull)download 
+didReceiveData:(int64_t)length API_AVAILABLE(macos(11.3), ios(14.5))
+{
+  mIWebView->OnReceivedData(length, 0);
+}
+
+- (void)download:(WKDownload * _Nonnull)download 
+didFailWithError:(NSError * _Nonnull)error API_AVAILABLE(macos(11.3), ios(14.5))
+{
+  mIWebView->OnFailedToDownloadFile([[downloadDestinationURL absoluteString] UTF8String]);
+  downloadDestinationURL = nil;
+}
+
+- (void)downloadDidFinish:(WKDownload * _Nonnull)download API_AVAILABLE(macos(11.3), ios(14.5))
+{
+  mIWebView->OnDownloadedFile([[downloadDestinationURL path] UTF8String]);
+  downloadDestinationURL = nil;
+}
+
+@end

IPlug/Extras/WebView/IPlugWKWebViewScriptMessageHandler.mm
@@ -1,0 +1,99 @@
+#import "IPlugWKWebViewScriptMessageHandler.h"
+#include "IPlugWebView.h"
+
+#if !__has_feature(objc_arc)
+#error This file must be compiled with Arc. Use -fobjc-arc flag
+#endif
+
+using namespace iplug;
+
+@implementation IPLUG_WKSCRIPTMESSAGEHANDLER
+
+-(id) initWithIWebView:(IWebView*) webView
+{
+  self = [super init];
+  
+  if (self)
+    mIWebView = webView;
+  
+  return self;
+}
+
+- (void)dealloc
+{
+  mIWebView = nullptr;
+}
+
+- (void) userContentController:(nonnull WKUserContentController*) userContentController didReceiveScriptMessage:(nonnull WKScriptMessage*) message
+{
+  if ([[message name] isEqualToString:@"callback"])
+  {
+    NSDictionary* dict = (NSDictionary*) message.body;
+    NSData* data = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:nil];
+    NSString* jsonString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    mIWebView->OnMessageFromWebView([jsonString UTF8String]);
+  }
+}
+
+- (void) webView:(IPLUG_WKWEBVIEW*) webView didFinishNavigation:(WKNavigation*) navigation
+{
+  mIWebView->OnWebContentLoaded();
+}
+
+- (NSURL*) changeURLScheme:(NSURL*) url toScheme:(NSString*) newScheme
+{
+  NSURLComponents* components = [NSURLComponents componentsWithURL:url resolvingAgainstBaseURL:YES];
+  components.scheme = newScheme;
+  return components.URL;
+}
+
+- (void) webView:(nonnull WKWebView *)webView startURLSchemeTask:(nonnull id<WKURLSchemeTask>)urlSchemeTask  API_AVAILABLE(macos(10.13)){
+
+  NSString* customUrlScheme = [NSString stringWithUTF8String:mIWebView->GetCustomUrlScheme()];
+  const BOOL useCustomUrlScheme = [customUrlScheme length];
+  
+  NSString* urlScheme = @"file:";
+  
+  if (useCustomUrlScheme)
+  {
+    urlScheme = [urlScheme stringByReplacingOccurrencesOfString:@"file" withString:customUrlScheme];
+  }
+  
+  if (useCustomUrlScheme && [urlSchemeTask.request.URL.absoluteString containsString: urlScheme])
+  {
+    NSURL* customFileURL = urlSchemeTask.request.URL;
+    NSURL* fileURL = [self changeURLScheme:customFileURL toScheme:@"file"];
+    NSHTTPURLResponse* response = NULL;
+    
+    if ([[NSFileManager defaultManager] fileExistsAtPath:fileURL.path])
+    {
+      NSData* data = [[NSData alloc] initWithContentsOfURL:fileURL];
+      NSString* contentLengthStr = [[NSString alloc] initWithFormat:@"%lu", [data length]];
+      NSString* contentTypeStr = @"text/plain";
+      NSString* extStr = [[fileURL path] pathExtension];
+      if ([extStr isEqualToString:@"html"]) contentTypeStr = @"text/html";
+      else if ([extStr isEqualToString:@"css"]) contentTypeStr = @"text/css";
+      else if ([extStr isEqualToString:@"js"]) contentTypeStr = @"text/javascript";
+      else if ([extStr isEqualToString:@"jpg"]) contentTypeStr = @"image/jpeg";
+      else if ([extStr isEqualToString:@"jpeg"]) contentTypeStr = @"image/jpeg";
+      else if ([extStr isEqualToString:@"svg"]) contentTypeStr = @"image/svg+xml";
+
+      NSDictionary* headerFields = [NSDictionary dictionaryWithObjects:@[contentLengthStr, contentTypeStr] forKeys:@[@"Content-Length", @"Content-Type"]];
+      response = [[NSHTTPURLResponse alloc] initWithURL:customFileURL statusCode:200 HTTPVersion:@"HTTP/1.1" headerFields:headerFields];
+      [urlSchemeTask didReceiveResponse:response];
+      [urlSchemeTask didReceiveData:data];
+    }
+    else
+    {
+      response = [[NSHTTPURLResponse alloc] initWithURL:customFileURL statusCode:404 HTTPVersion:@"HTTP/1.1" headerFields:nil];
+      [urlSchemeTask didReceiveResponse:response];
+    }
+    [urlSchemeTask didFinish];
+  }
+}
+
+- (void) webView:(nonnull WKWebView *)webView stopURLSchemeTask:(nonnull id<WKURLSchemeTask>)urlSchemeTask  API_AVAILABLE(macos(10.13)){
+  
+}
+
+@end

IPlug/Extras/WebView/IPlugWKWebViewUIDelegate.h
@@ -1,0 +1,34 @@
+#pragma once
+
+#ifdef __OBJC__
+#import <WebKit/WebKit.h>
+#if defined __APPLE__
+  #include "TargetConditionals.h"
+  #if TARGET_OS_IOS
+    #import <UIKit/UIKit.h>
+  #endif
+#endif
+
+namespace iplug {
+class IWebView;
+}
+
+@interface IPLUG_WKWEBVIEW_UI_DELEGATE : NSObject <WKUIDelegate>
+{
+  iplug::IWebView* _Nonnull mIWebView;
+}
+
+- (id _Nonnull)initWithIWebView:(iplug::IWebView* _Nonnull)webView;
+
+- (void)webView:(WKWebView *_Nonnull)webView runOpenPanelWithParameters:(WKOpenPanelParameters *_Nullable)parameters 
+  initiatedByFrame:(WKFrameInfo *_Nonnull)frame completionHandler:(void (^_Nullable)(NSArray<NSURL *> * _Nullable URLs))completionHandler;
+
+@end
+
+#if TARGET_OS_IOS
+@interface IPLUG_WKWEBVIEW_UI_DELEGATE () <UIDocumentPickerDelegate>
+@property (nonatomic, copy) void (^ _Nullable filePickerCompletionHandler)(NSArray<NSURL *> * _Nullable URLs);
+@end
+#endif
+
+#endif // __OBJC__

commit 739089e7c6a35ea3c5814838aa68304cb95cdcf5

WKWebView: Fix a bug introduced in b145123, continued

IPlug/Extras/WebView/IPlugWKWebViewDelegate.h
@@ -45,30 +45,21 @@ class IWebView;
 
 - (void)dealloc;
 
-- (void)webView:(WKWebView * _Nonnull)webView 
-decidePolicyForNavigationAction:(WKNavigationAction * _Nonnull)navigationAction 
-decisionHandler:(void (^ _Nonnull)(WKNavigationActionPolicy))decisionHandler;
+- (void)webView:(WKWebView * _Nonnull)webView decidePolicyForNavigationAction:(WKNavigationAction * _Nonnull)navigationAction decisionHandler:(void (^ _Nonnull)(WKNavigationActionPolicy))decisionHandler;
 
-- (void)webView:(WKWebView * _Nonnull)webView 
-decidePolicyForNavigationResponse:(WKNavigationResponse * _Nonnull)navigationResponse 
-decisionHandler:(void (^ _Nonnull)(WKNavigationResponsePolicy))decisionHandler;
+- (void)webView:(WKWebView * _Nonnull)webView decidePolicyForNavigationResponse:(WKNavigationResponse * _Nonnull)navigationResponse decisionHandler:(void (^ _Nonnull)(WKNavigationResponsePolicy))decisionHandler;
+
+- (void)webView:(IPLUG_WKWEBVIEW* _Nonnull)webView didFinishNavigation:(null_unspecified WKNavigation*)navigation;
 
 #pragma mark WKDownloadDelegate
 
-- (void)webView:(WKWebView * _Nonnull)webView 
-navigationResponse:(WKNavigationResponse * _Nonnull)navigationResponse 
-didBecomeDownload:(WKDownload * _Nonnull)download API_AVAILABLE(macos(11.3), ios(14.5));
+- (void)webView:(WKWebView * _Nonnull)webView navigationResponse:(WKNavigationResponse * _Nonnull)navigationResponse didBecomeDownload:(WKDownload * _Nonnull)download API_AVAILABLE(macos(11.3), ios(14.5));
 
-- (void)download:(WKDownload * _Nonnull)download 
-decideDestinationUsingResponse:(NSURLResponse * _Nonnull)response 
-suggestedFilename:(NSString * _Nonnull)filename 
-completionHandler:(void (^ _Nonnull)(NSURL * _Nullable))completionHandler API_AVAILABLE(macos(11.3), ios(14.5));
+- (void)download:(WKDownload * _Nonnull)download decideDestinationUsingResponse:(NSURLResponse * _Nonnull)response suggestedFilename:(NSString * _Nonnull)filename completionHandler:(void (^ _Nonnull)(NSURL * _Nullable))completionHandler API_AVAILABLE(macos(11.3), ios(14.5));
 
-- (void)download:(WKDownload * _Nonnull)download 
-didReceiveData:(int64_t)length API_AVAILABLE(macos(11.3), ios(14.5));
+- (void)download:(WKDownload * _Nonnull)download didReceiveData:(int64_t)length API_AVAILABLE(macos(11.3), ios(14.5));
 
-- (void)download:(WKDownload * _Nonnull)download 
-didFailWithError:(NSError * _Nonnull)error API_AVAILABLE(macos(11.3), ios(14.5));
+- (void)download:(WKDownload * _Nonnull)download didFailWithError:(NSError * _Nonnull)error API_AVAILABLE(macos(11.3), ios(14.5));
 
 - (void)downloadDidFinish:(WKDownload * _Nonnull)download API_AVAILABLE(macos(11.3), ios(14.5));
 

IPlug/Extras/WebView/IPlugWKWebViewScriptMessageHandler.h
@@ -52,8 +52,6 @@ class IWebView;
 
 #pragma mark - WKURLSchemeHandler
 
-- (void)webView:(IPLUG_WKWEBVIEW* _Nonnull)webView didFinishNavigation:(WKNavigation* _Nullable)navigation;
-
 - (NSURL* _Nullable)changeURLScheme:(NSURL* _Nonnull)url toScheme:(NSString* _Nonnull)newScheme;
 
 - (void)webView:(nonnull WKWebView *)webView 