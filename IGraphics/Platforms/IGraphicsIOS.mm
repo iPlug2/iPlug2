63321400a645e8f9fafa6dc7860698be8cc61623
Oli Larkin <olilarkin@googlemail.com>
2024-12-20 13:17:25 +0100

    Remove unused variables

--- IGraphics/Platforms/IGraphicsIOS.mm
+++ IGraphics/Platforms/IGraphicsIOS.mm
@@ -235,9 +235,7 @@ void IGraphicsIOS::PromptForDirectory(WDL_String& path, IFileDialogCompletionHan
 {
   assert(completionHandler != nullptr && "You must provide a completion handler on iOS");
   
-  NSString* pDefaultFileName = nil;
   NSString* pDefaultPath = nil;
-  NSMutableArray* pFileTypes = [[NSMutableArray alloc] init];
 
   if (path.GetLength())
     pDefaultPath = [NSString stringWithUTF8String:path.Get()];
