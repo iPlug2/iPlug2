1e9e660c68874eba623f9bebc2bed9ed214feac9
Oli Larkin <olilarkin@googlemail.com>
2024-12-24 16:41:21 +0000

    Draw into the window with skia

--- Examples/IPlugAppExperiment/main_dialog_proc.cpp
+++ Examples/IPlugAppExperiment/main_dialog_proc.cpp
@@ -2,6 +2,10 @@
 #include "resource.h"
 #include "wdlstring.h"
 #include "render_dialog_proc.hpp"
+#include "include/core/SkCanvas.h"
+#include "include/core/SkSurface.h"
+#include "include/core/SkBitmap.h"
+#include "include/core/SkColorSpace.h"
 
 #ifndef _WIN32
 #define PostQuitMessage SWELL_PostQuitMessage
@@ -10,25 +14,54 @@
 HWND gHWND;
 //extern HMENU SWELL_app_stocksysmenu;
 
+HDC memDC;
+sk_sp<SkSurface> surface;
+
+sk_sp<SkSurface> CreateSurface(HDC drawContext, int w, int h)
+{
+  SkImageInfo info = SkImageInfo::Make(w, h, kBGRA_8888_SkColorType, kPremul_SkAlphaType, nullptr);
+  void* pixels = SWELL_GetCtxFrameBuffer(drawContext);
+  sk_sp<SkSurface> surface = SkSurfaces::WrapPixels(info, pixels, sizeof(uint32_t) * w);
+  return surface;
+}
+
 // Dialog Procedure for the main app window (To understand: why not a Window Proc?)
 WDL_DLGRET MainDlgProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
   switch (uMsg)
   {
     case WM_INITDIALOG:
+    {
       gHWND = hwndDlg;
+      RECT r;
+      GetClientRect(hwndDlg, &r);
+      auto w = r.right - r.left;
+      auto h = r.bottom - r.top;
       SWELL_EnableMetal(gHWND, 1);
+      memDC = SWELL_CreateMemContext(nullptr, w, h);
+      surface = CreateSurface(memDC, w, h);
       ShowWindow(hwndDlg, SW_SHOW);
       return 0;
+    }
     case WM_PAINT:
     {
       PAINTSTRUCT ps;
-      /*HDC dc = */BeginPaint(hwndDlg, &ps);
+      HDC dc = BeginPaint(hwndDlg, &ps);
       RECT r;
       GetClientRect(hwndDlg, &r);
-      HBRUSH brs = CreateSolidBrush(RGB(255, 0, 0));
-      FillRect(ps.hdc, &r, brs);
-      DeleteObject(brs);
+      
+      auto w = r.right - r.left;
+      auto h = r.bottom - r.top;
+
+      SkCanvas* canvas = surface->getCanvas();
+      canvas->clear(SK_ColorCYAN);
+      SkPaint paint;
+      paint.setColor(SK_ColorBLUE);
+      paint.setAntiAlias(true);
+      canvas->drawCircle(100, 100, 50, paint);
+      
+      BitBlt(dc, 0, 0, w, h, memDC, 0, 0, 0);
+      
       EndPaint(hwndDlg, &ps);
       return 0;
     }
@@ -37,6 +70,13 @@ WDL_DLGRET MainDlgProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
       RECT r;
       GetClientRect(hwndDlg, &r);
       InvalidateRect(hwndDlg, &r, 0);
+      
+      auto w = r.right - r.left;
+      auto h = r.bottom - r.top;
+      
+      SWELL_DeleteGfxContext(memDC);
+      memDC = SWELL_CreateMemContext(nullptr, w, h);
+      surface = CreateSurface(memDC, w, h);
       return 0;;
     }
     case WM_DESTROY:
