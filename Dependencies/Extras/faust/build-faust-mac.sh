e5605eff1aef396d2a7b9636895c50656f1f3036
Oli Larkin <olilarkin@googlemail.com>
2025-03-30 12:57:22 +0200

    WIP modify faust build scripts for interpreter

--- Dependencies/Extras/faust/build-faust-mac.sh
+++ Dependencies/Extras/faust/build-faust-mac.sh
@@ -16,14 +16,8 @@ then
 else
   git clone --recursive https://github.com/grame-cncm/faust.git $FAUST_REPO_DIR
 fi
-mkdir $FAUST_CMAKE_BUILD_DIR
-cd $FAUST_CMAKE_BUILD_DIR
-cmake -C $DEPS_DIR/iplug-backends.cmake -C $DEPS_DIR/iplug-targets-mac.cmake -DINCLUDE_STATIC=on -DINCLUDE_DYNAMIC=on -DINCLUDE_OSC=off -DINCLUDE_HTTP=off -DUNIVERSAL=off ../faust/build
-cmake -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR ../faust/build
-cmake --build $FAUST_CMAKE_BUILD_DIR --config Release -- -j 6
-make install
-rm $INSTALL_DIR/lib/ios-libsndfile.a
-rm $INSTALL_DIR/lib/libfaust.dylib
-rm $INSTALL_DIR/lib/libfaust.2.dylib
-mv $INSTALL_DIR/lib/*.dylib $INSTALL_DIR/lib/libfaust.dylib
-install_name_tool -id @loader_path/../Frameworks/libfaust.dylib $INSTALL_DIR/lib/libfaust.dylib
+cd $FAUST_REPO_DIR
+# cmake -C $DEPS_DIR/iplug-backends.cmake -C $DEPS_DIR/iplug-targets-mac.cmake -DINCLUDE_STATIC=on -DINCLUDE_DYNAMIC=off -DINCLUDE_OSC=off -DINCLUDE_HTTP=off -DUNIVERSAL=off -DLLVM_SUPPORT=OFF  ../faust/build
+# cmake -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR ../faust/build
+# cmake --build $FAUST_CMAKE_BUILD_DIR --config Release -- -j 6
+make interp -j6
