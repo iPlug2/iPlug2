2a075281899d2ec65961b2e3975aca150232f7fa
Oli Larkin <olilarkin@googlemail.com>
2025-12-08 17:28:21 +0100

    Fix duplicate symbol in IPlugSwiftUI example

--- Examples/IPlugSwiftUI/IPlugSwiftUI-Shared.h
+++ Examples/IPlugSwiftUI/IPlugSwiftUI-Shared.h
@@ -1,19 +1,19 @@
 #ifndef IPlugSwiftUI_Shared_h
 #define IPlugSwiftUI_Shared_h
 
-const int kNumPresets = 1;
+static const int kNumPresets = 1;
 
-const NSInteger kParamGain = 0;
-const NSInteger kNumParams = 1;
+static const NSInteger kParamGain = 0;
+static const NSInteger kNumParams = 1;
 
-const NSInteger kCtrlTagScope = 1;
-const NSInteger kCtrlTagButton = 2;
+static const NSInteger kCtrlTagScope = 1;
+static const NSInteger kCtrlTagButton = 2;
 
-const NSInteger kUpdateMessage = 0; // must match ISender::kUpdateMessage
-const NSInteger kMsgTagHello = 1;
-const NSInteger kMsgTagRestorePreset = 2;
+static const NSInteger kUpdateMessage = 0; // must match ISender::kUpdateMessage
+static const NSInteger kMsgTagHello = 1;
+static const NSInteger kMsgTagRestorePreset = 2;
 
-const NSInteger kDataPacketSize = 1024;
+static const NSInteger kDataPacketSize = 1024;
 extern const NSInteger kScopeBufferSize;
 
 #endif /* IPlugSwiftUI_Shared_h */
