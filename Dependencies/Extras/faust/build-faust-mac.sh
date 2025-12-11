aecd6c85dfe1df1da4ab304d9779c5af961ad0c8
Oli Larkin <olilarkin@googlemail.com>
2025-12-11 17:49:09 +0100

    fixup! Add FAUST build configuration and dependency scripts

--- Dependencies/Extras/faust/build-faust-mac.sh
+++ Dependencies/Extras/faust/build-faust-mac.sh
@@ -7,17 +7,37 @@ BUILD_DIR="$PWD/../../Build"
 SRC_DIR="$BUILD_DIR/src"
 TMP_DIR="$BUILD_DIR/tmp"
 FAUST_REPO_DIR="$TMP_DIR/faust"
-FAUST_CMAKE_BUILD_DIR="$TMP_DIR/faust-cmake"
+FAUST_CMAKE_BUILD_DIR="$FAUST_REPO_DIR/build/faustdir"
 INSTALL_DIR="$BUILD_DIR/mac"
 
 if [ -d "$FAUST_REPO_DIR" ]
 then
-  echo faust repo exists
+  echo "faust repo exists"
 else
-  git clone --recursive https://github.com/grame-cncm/faust.git $FAUST_REPO_DIR
+  git clone --recursive https://github.com/grame-cncm/faust.git "$FAUST_REPO_DIR"
 fi
-cd $FAUST_REPO_DIR
-# cmake -C $DEPS_DIR/iplug-backends.cmake -C $DEPS_DIR/iplug-targets-mac.cmake -DINCLUDE_STATIC=on -DINCLUDE_DYNAMIC=off -DINCLUDE_OSC=off -DINCLUDE_HTTP=off -DUNIVERSAL=off -DLLVM_SUPPORT=OFF  ../faust/build
-# cmake -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR ../faust/build
-# cmake --build $FAUST_CMAKE_BUILD_DIR --config Release -- -j 6
-make interp -j6
+
+# Build using FAUST's Makefile wrapper with iPlug2 config files
+cd "$FAUST_REPO_DIR"
+
+# Copy iPlug2 backend/target configs to FAUST's config directories
+cp "$DEPS_DIR/iplug-backends.cmake" build/backends/iplug.cmake
+cp "$DEPS_DIR/iplug-targets-mac.cmake" build/targets/iplug.cmake
+
+# Configure and build static library with interpreter backend
+cd build
+make cmake BACKENDS=iplug.cmake TARGETS=iplug.cmake
+make staticlib -j6
+cd ..
+
+# Copy built library to install location
+mkdir -p "$INSTALL_DIR/lib"
+mkdir -p "$INSTALL_DIR/include/faust/dsp"
+mkdir -p "$INSTALL_DIR/include/faust/gui"
+
+cp "$FAUST_REPO_DIR/build/lib/libfaust.a" "$INSTALL_DIR/lib/"
+cp "$FAUST_REPO_DIR/architecture/faust/"*.h "$INSTALL_DIR/include/faust/"
+cp -r "$FAUST_REPO_DIR/architecture/faust/dsp/"* "$INSTALL_DIR/include/faust/dsp/"
+cp -r "$FAUST_REPO_DIR/architecture/faust/gui/"* "$INSTALL_DIR/include/faust/gui/"
+
+echo "FAUST built successfully to $INSTALL_DIR"
--- Dependencies/Extras/faust/iplug-targets-mac.cmake
+++ Dependencies/Extras/faust/iplug-targets-mac.cmake
@@ -1,9 +1,9 @@
 # this file may be used to select different target
-# values are among ON or OFF 
+# values are among ON or OFF
 
-set ( INCLUDE_EXECUTABLE  ON  CACHE STRING "Include Faust compiler" FORCE )
+set ( INCLUDE_EXECUTABLE  OFF CACHE STRING "Include Faust compiler" FORCE )
 set ( INCLUDE_STATIC      ON  CACHE STRING "Include static Faust library" FORCE )
-set ( INCLUDE_DYNAMIC     ON  CACHE STRING "Include dynamic Faust library" FORCE )
+set ( INCLUDE_DYNAMIC     OFF CACHE STRING "Include dynamic Faust library" FORCE )
 set ( INCLUDE_OSC         OFF CACHE STRING "Include Faust OSC static library" FORCE )
 set ( OSCDYNAMIC          OFF CACHE STRING "Include Faust OSC dynamic library" FORCE )
 set ( INCLUDE_HTTP        OFF CACHE STRING "Include Faust HTTPD static library" FORCE )
